f01b9405322c42543b33c61ddb1ea0bb00f2fcfe
Nicolas Guillaume <nicolas@themesandco.com>
2014-11-20 20:04:42 +0100

    fix issues with WordPress coding standard rules custom page

--- custom-page.php
+++ custom-page.php
@@ -4,7 +4,7 @@ Template Name: Custom Page Example
 */
 ?>
 <?php do_action( '__before_main_wrapper' ); ##hook of the header with get_header ?>
-<div id="main-wrapper" class="<?php echo tc__f( 'tc_main_wrapper_classes' , 'container' ) ?>">
+<div id="main-wrapper" class="<?php echo esc_attr( tc__f( 'tc_main_wrapper_classes' , 'container' ) ) ?>">
 
     <?php do_action( '__before_main_container' ); ##hook of the featured page (priority 10) and breadcrumb (priority 20)...and whatever you need! ?>
     
@@ -13,30 +13,31 @@ Template Name: Custom Page Example
 
             <?php do_action( '__before_article_container' ); ##hook of left sidebar?>
                 
-                <div id="content" class="<?php echo tc__f( '__screen_layout' , tc__f ( '__ID' ) , 'class' ) ?> article-container">
+                <div id="content" class="<?php echo esc_attr( tc__f( '__screen_layout' , tc__f( '__ID' ) , 'class' ) ) ?> article-container">
                     
-                    <?php do_action ( '__before_loop' );##hooks the header of the list of post : archive, search... ?>
+                    <?php do_action( '__before_loop' );##hooks the header of the list of post : archive, search... ?>
 
-                        <?php if ( have_posts() ) : ?>
+                        <?php if ( have_posts() ) : ?>  
+
                             <?php while ( have_posts() ) : ##all other cases for single and lists: post, custom post type, page, archives, search, 404 ?>
                                 
                                 <?php the_post(); ?>
 
-                                <?php do_action ( '__before_article' ) ?>
-                                    <article <?php tc__f('__article_selectors' ) ?>>
+                                <?php do_action( '__before_article' ) ?>
+                                    <article <?php tc__f( '__article_selectors' ) ?>>
                                         <?php do_action( '__loop' ); ?>
                                     </article>
-                                <?php do_action ( '__after_article' ) ?>
+                                <?php do_action( '__after_article' ) ?>
 
                             <?php endwhile; ?>
 
                         <?php endif; ##end if have posts ?>
 
-                    <?php do_action ('__after_loop');##hook of the comments and the posts navigation with priorities 10 and 20 ?>
+                    <?php do_action( '__after_loop' );##hook of the comments and the posts navigation with priorities 10 and 20 ?>
 
                 </div><!--.article-container -->
 
-           <?php do_action( '__after_article_container'); ##hook of left sidebar ?>
+           <?php do_action( '__after_article_container' ); ##hook of left sidebar ?>
 
         </div><!--.row -->
     </div><!-- .container role: main -->
