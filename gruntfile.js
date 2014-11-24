f753782ca3000806cb5914f5086b17fbb3072765
Nicolas Guillaume <nicolas@themesandco.com>
2014-11-24 14:24:00 +0100

    added to do notice in gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,5 @@
 //@todo add a grunt log task before build : git log --pretty=format:"%h %s" --graph --since="2014-11-15"
+//@todo save description in package json and copy in different place on each builds
 module.exports = function(grunt) {
 	console.log('OPTIONS : ', grunt.option('src'), grunt.option('dest'));
 	var path = require('path');
