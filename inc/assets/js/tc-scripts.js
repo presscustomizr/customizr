4561a74d4fa053db51e955d55e51d11d60ed7535
Nicolas Guillaume <nicolas@themesandco.com>
2015-01-20 20:31:09 +0100

    v3.2.11 beta build

--- inc/assets/js/tc-scripts.js
+++ inc/assets/js/tc-scripts.js
@@ -1660,7 +1660,7 @@ var TCParams = TCParams || {};
       dimension = this.dimension()
       scroll = $.camelCase(['scroll', dimension].join('-'))
       actives = this.$parent && this.$parent.find('> .accordion-group > .in')
-      
+
       if (actives && actives.length) {
         hasData = actives.data('collapse')
         if (hasData && hasData.transitioning) return
@@ -1674,13 +1674,13 @@ var TCParams = TCParams || {};
 
       //@tc adddon
       //give the revealed sub menu the height of the visible viewport
-      if ( 1 == TCParams.dropdowntoViewport ) 
+      if ( 1 == TCParams.dropdowntoViewport )
       {
         var tcVisible = $('body').hasClass('sticky-enabled') ? $(window).height() : ($(window).height() - $('.navbar-wrapper').offset().top);
         tcVisible = ( tcVisible - 90 ) > 80 ? tcVisible - 90 : 300;
         this.$element.css('max-height' , tcVisible + 'px');
-      } 
-      else if ( 1 != TCParams.dropdowntoViewport && 1 == TCParams.stickyHeader ) 
+      }
+      else if ( 1 != TCParams.dropdowntoViewport && 1 == TCParams.stickyHeader )
       {
         //trigger click on back to top if sticky enabled
         if ( 0 != $('.back-to-top').length ) {
@@ -1703,7 +1703,7 @@ var TCParams = TCParams || {};
       this.reset(this.$element[dimension]())
       this.transition('removeClass', $.Event('hide'), 'hidden')
       this.$element[dimension](0)
-      
+
       //@tc adddon
       if ( 1 != TCParams.dropdowntoViewport && 1 == TCParams.stickyHeader ) {
         $('body').addClass('tc-sticky-header');
