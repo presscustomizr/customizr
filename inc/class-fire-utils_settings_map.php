c500e1b1e7a373423a751637d8c32638ffe04d88
Nicolas Guillaume <nicolas@themesandco.com>
2015-02-13 10:49:38 +0100

    Disable some title icons by default for new users

--- inc/class-fire-utils_settings_map.php
+++ inc/class-fire-utils_settings_map.php
@@ -1529,8 +1529,8 @@ if ( ! class_exists( 'TC_utils_settings_map' ) ) :
                                 'transport'   => 'postMessage'
               ),
               'tc_theme_options[tc_show_page_title_icon]'  =>  array(
-                                'default'       => 1,
-                                'control'     => 'TC_controls' ,
+                                'default'       => TC_utils::$instance -> tc_user_started_before_version( '3.2.18' ) ? 1 : 0,
+                                'control'       => 'TC_controls' ,
                                 'label'         => __( "Display a page icon next to the page title" , "customizr" ),
                                 'section'       => 'tc_titles_icons_settings' ,
                                 'type'          => 'checkbox',
@@ -1538,7 +1538,7 @@ if ( ! class_exists( 'TC_utils_settings_map' ) ) :
                                 'transport'   => 'postMessage'
               ),
               'tc_theme_options[tc_show_post_title_icon]'  =>  array(
-                                'default'       => 1,
+                                'default'       => TC_utils::$instance -> tc_user_started_before_version( '3.2.18' ) ? 1 : 0,
                                 'control'     => 'TC_controls' ,
                                 'label'         => __( "Display a post icon next to the single post title" , "customizr" ),
                                 'section'       => 'tc_titles_icons_settings' ,
@@ -1557,7 +1557,7 @@ if ( ! class_exists( 'TC_utils_settings_map' ) ) :
                                 'transport'   => 'postMessage'
               ),
               'tc_theme_options[tc_show_post_list_title_icon]'  =>  array(
-                                'default'       => 1,
+                                'default'       => TC_utils::$instance -> tc_user_started_before_version( '3.2.18' ) ? 1 : 0,
                                 'control'     => 'TC_controls' ,
                                 'label'         => __( "Display an icon next to each post title in an archive page" , "customizr" ),
                                 'section'       => 'tc_titles_icons_settings' ,
