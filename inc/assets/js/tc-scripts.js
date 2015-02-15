6ffb2d1e48acc8840af01af7e0150f2d32c6105d
Nicolas Guillaume <nicolas@themesandco.com>
2015-02-15 11:20:42 +0100

    Fix: dropcap skips also ul,ol tags

--- inc/assets/js/tc-scripts.js
+++ inc/assets/js/tc-scripts.js
@@ -23,7 +23,7 @@ var TCParams = TCParams || {
   dropcapWhere:{ post : 0, page : 1 },
   dropcapMinWords:50,
   skipSelectors: {
-              tags : ['IMG' , 'IFRAME', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'BLOCKQUOTE'],
+              tags : ['IMG' , 'IFRAME', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'BLOCKQUOTE', 'UL', 'OL'],
               classes : [],
               ids : []
             },
@@ -2527,6 +2527,9 @@ var TCParams = TCParams || {};
  * Example of gif 1px x 1px placeholder :
  * 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
  *
+ * inspired by the work of Luís Almeida
+ * http://luis-almeida.github.com/unveil
+ *
  * =================================================== */
 ;(function ( $, window, document, undefined ) {
   //defaults
@@ -2607,17 +2610,17 @@ var TCParams = TCParams || {};
   */
   Plugin.prototype._is_visible = function( _img, _evt ) {
     var $_img       = $(_img),
-        wind_to_top = $(window).scrollTop(),
-        wind_to_bot = wind_to_top + $(window).height(),
-        img_to_top  = $_img.offset().top,
-        img_to_bot  = img_to_top + $_img.height(),
-        cust_thresh = this.options.threshold;
+        wt = $(window).scrollTop(),
+        wb = wt + $(window).height(),
+        it  = $_img.offset().top,
+        ib  = it + $_img.height(),
+        th = this.options.threshold;
 
     //force all images to visible if first scroll option enabled
     if ( _evt && 'scroll' == _evt.type && this.options.load_all_images_on_first_scroll )
       return true;
 
-    return img_to_bot >= wind_to_top - cust_thresh && img_to_top <= wind_to_bot + cust_thresh;
+    return ib >= wt - th && it <= wb + th;
   };
 
 
--- inc/class-fire-resources.php
+++ inc/class-fire-resources.php
@@ -182,7 +182,7 @@ if ( ! class_exists( 'TC_resources' ) ) :
               'dropcapEnabled'      => esc_attr( tc__f( '__get_option' , 'tc_enable_dropcap' ) ),
               'dropcapWhere'      => array( 'post' => esc_attr( tc__f( '__get_option' , 'tc_post_dropcap' ) ) , 'page' => esc_attr( tc__f( '__get_option' , 'tc_page_dropcap' ) ) ),
               'dropcapMinWords'     => esc_attr( tc__f( '__get_option' , 'tc_dropcap_minwords' ) ),
-              'skipSelectors'       => apply_filters( 'tc_dropcap_skip_selectors' , array( 'tags' => array('IMG' , 'IFRAME', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'BLOCKQUOTE' ), 'classes' => array() , 'id' => array() ) ),
+              'skipSelectors'       => apply_filters( 'tc_dropcap_skip_selectors' , array( 'tags' => array('IMG' , 'IFRAME', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'BLOCKQUOTE', 'UL', 'OL'), 'classes' => array() , 'id' => array() ) ),
               'imgSmartLoadEnabled' => esc_attr( tc__f( '__get_option' , 'tc_img_smart_load' ) ),
               'imgSmartLoadOpts'    => apply_filters( 'tc_img_smart_load_options' , array() )
 	        	),
