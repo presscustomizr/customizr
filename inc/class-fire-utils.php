e0d5f89d296d12aa66581de878495621868a9580
Nicolas Guillaume <nicolas@themesandco.com>
2015-02-28 11:54:47 +0100

    replaced 'tc_theme_options' by TC___::$tc_option_group

--- inc/class-fire-utils.php
+++ inc/class-fire-utils.php
@@ -289,7 +289,7 @@ if ( ! class_exists( 'TC_utils' ) ) :
       */
       function tc_opt( $option_name , $option_group = null ) {
           //do we have to look in a specific group of option (plugin?)
-          $option_group       = is_null($option_group) ? 'tc_theme_options' : $option_group;
+          $option_group       = is_null($option_group) ? TC___::$tc_option_group : $option_group;
           if ( $this -> tc_is_customizing() || is_admin() )
             $_db_options = (array) get_option( $option_group );
           else
