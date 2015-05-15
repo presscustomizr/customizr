0765878b50d376cb6e713cbca11619f17a512533
Nicolas Guillaume <nicolas@themesandco.com>
2015-05-15 18:54:37 +0200

    added : less files updated with new rtl vars and conditional statements

--- grunt-tasks-config/less.js
+++ grunt-tasks-config/less.js
@@ -17,11 +17,20 @@ module.exports = {
 			modifyVars: {
 				lnkCol : '<%= skin_color %>',
 				is_rtl: false,
+        floatDirection : 'left',
+        textAlign: 'left'
 			}
 		},
 		files: {"<%= paths.front_css %><%= skin_name %>.css": "<%= paths.less %>tc_skin_gen.less"}
 	},
   prod_common : {
+    options: {
+      modifyVars: {
+        is_rtl: false,
+        floatDirection : 'left',
+        textAlign: 'left'
+      }
+    },
     files: [
       {src: '<%= paths.less %>tc_common_gen.less', dest: '<%= paths.front_css %>tc_common.css'}
     ]
@@ -30,6 +39,8 @@ module.exports = {
     options: {
       modifyVars: {
         is_rtl: true,
+        floatDirection : 'right',
+        textAlign: 'right'
       }
     },
     files: [
@@ -41,6 +52,8 @@ module.exports = {
       modifyVars: {
         lnkCol : '<%= skin_color %>',
         is_rtl: false,
+        floatDirection : 'left',
+        textAlign: 'left'
       }
     },
     files: [
