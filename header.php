622f102cd91fb06cda4451f4632750669c4c341b
Nicolas Guillaume <nicolas@themesandco.com>
2015-05-25 09:37:33 +0200

    added : a div#tc-page-wrap including header, content and footer

--- header.php
+++ header.php
@@ -25,16 +25,20 @@
 
 	<body <?php body_class(); ?> <?php echo apply_filters('tc_body_attributes' , 'itemscope itemtype="http://schema.org/WebPage"') ?>>
 
-		<?php do_action( '__before_header' ); ?>
+    <?php do_action( '__before_page_wrapper' ); ?>
 
-	   	<header class="<?php echo implode( " ", apply_filters('tc_header_classes', array('tc-header' ,'clearfix', 'row-fluid') ) ) ?>" role="banner">
-			<?php
-				// The '__header' hook is used with the following callback functions (ordered by priorities) :
-				//TC_header_main::$instance->tc_logo_title_display(), TC_header_main::$instance->tc_tagline_display(), TC_header_main::$instance->tc_navbar_display()
-				do_action( '__header' );
-			?>
-		</header>
-		<?php
-		 	//This hook is used for the slider : TC_slider::$instance->tc_slider_display()
-			do_action ( '__after_header' )
-		?>
+    <div id="tc-page-wrap" class="<?php echo implode( " ", apply_filters('tc_page_wrap_class', array() ) ) ?>">
+
+  		<?php do_action( '__before_header' ); ?>
+
+  	   	<header class="<?php echo implode( " ", apply_filters('tc_header_classes', array('tc-header' ,'clearfix', 'row-fluid') ) ) ?>" role="banner">
+  			<?php
+  				// The '__header' hook is used with the following callback functions (ordered by priorities) :
+  				//TC_header_main::$instance->tc_logo_title_display(), TC_header_main::$instance->tc_tagline_display(), TC_header_main::$instance->tc_navbar_display()
+  				do_action( '__header' );
+  			?>
+  		</header>
+  		<?php
+  		 	//This hook is used for the slider : TC_slider::$instance->tc_slider_display()
+  			do_action ( '__after_header' )
+  		?>
