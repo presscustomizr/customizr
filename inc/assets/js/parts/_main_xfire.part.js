d6159d7247094adb8e6c81721c7152b93c7b17a4
Rocco Aliberti <raliberti84@gmail.com>
2015-06-25 14:43:53 +0200

    Fix: re-introduce btt-arrow handling in new front js

--- inc/assets/js/parts/_main_xfire.part.js
+++ inc/assets/js/parts/_main_xfire.part.js
@@ -8,7 +8,7 @@ jQuery(function ($) {
     BrowserDetect : [],
     Czr_Plugins : ['centerImagesWithDelay', 'imgSmartLoad' , 'dropCaps', 'extLinks' , 'fancyBox'],
     Czr_Slider : ['fireSliders', 'manageHoverClass', 'centerSliderArrows', 'addSwipeSupport', 'sliderTriggerSimpleLoad'],
-    Czr_UserExperience : ['anchorSmoothScroll', 'backToTop', 'widgetsHoverActions', 'attachmentsFadeEffect', 'clickableCommentButton', 'dynSidebarReorder', 'dropdownMenuEventsHandler' ],
+    Czr_UserExperience : ['eventListener','anchorSmoothScroll', 'backToTop', 'widgetsHoverActions', 'attachmentsFadeEffect', 'clickableCommentButton', 'dynSidebarReorder', 'dropdownMenuEventsHandler' ],
     Czr_StickyHeader : ['stickyHeaderEventListener', 'triggerStickyHeaderLoad' ]
   };
   czrapp.cacheProp().loadCzr(toLoad);
