4f518cccdc384b9a6f98c9fd18cc387e3dfde62f
nikeo <nicolas@presscustomizr.com>
2015-07-21 16:28:19 +0200

    fix : jump to post list button action

--- inc/admin/js/theme-customizer-control.js
+++ inc/admin/js/theme-customizer-control.js
@@ -923,7 +923,7 @@ jQuery(function ($) {
 
     /* BUTTON JUMP TO POST LIST */
     $('.tc-navigate-to-post-list').click( function() {
-      $('#accordion-section-tc_post_list_settings > .accordion-section-title').trigger('click');
+      $('#accordion-section-post_lists_sec > .accordion-section-title').trigger('click');
     });
 
     /* RECENTER CURRENT SECTIONS */
