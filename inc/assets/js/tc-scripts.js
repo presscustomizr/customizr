9d1e28c9724f9da74363afc06250852df323b3d1
nikeo <nicolas@presscustomizr.com>
2015-07-28 10:15:08 +0200

    Fix: anchors smooth scroll - exclude ultimate members anchor links

--- inc/assets/js/tc-scripts.js
+++ inc/assets/js/tc-scripts.js
@@ -16,7 +16,7 @@ var TCParams = TCParams || {
   centerSliderImg : 1,
 	SmoothScroll: { Enabled : 1 , Options : {} },
 	anchorSmoothScroll: "linear",
-  anchorSmoothScrollExclude : ['[class*=edd]', '.tc-carousel-control', '.carousel-control', '[data-toggle="modal"]', '[data-toggle="dropdown"]', '[data-toggle="tooltip"]', '[data-toggle="popover"]', '[data-toggle="collapse"]', '[data-toggle="tab"]', '[class*=upme]'],
+  anchorSmoothScrollExclude : ['[class*=edd]', '.tc-carousel-control', '.carousel-control', '[data-toggle="modal"]', '[data-toggle="dropdown"]', '[data-toggle="tooltip"]', '[data-toggle="popover"]', '[data-toggle="collapse"]', '[data-toggle="tab"]', '[class*=upme]', '[class*=um-]'],
 	stickyCustomOffset: { _initial : 0, _scrolling : 0, options : { _static : true, _element : "" } },
 	stickyHeader: 1,
 	dropdowntoViewport: 1,
@@ -41,7 +41,8 @@ var TCParams = TCParams || {
   gridGoldenRatioLimit : 350,
   isSecondMenuEnabled : 0,
   secondMenuRespSet : 'in-sn-before'
-}// addEventListener Polyfill ie9- http://stackoverflow.com/a/27790212
+}
+// addEventListener Polyfill ie9- http://stackoverflow.com/a/27790212
 window.addEventListener = window.addEventListener || function (e, f) { window.attachEvent('on' + e, f); };
 
 
