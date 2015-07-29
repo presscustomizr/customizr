f52b97274946839796b3db062ca6f48b122e757a
Rocco Aliberti <raliberti84@gmail.com>
2015-07-29 11:29:11 +0200

    Fix: sidenav - in mobiles force a slide on top opening effect

--- inc/parts/class-header-menu.php
+++ inc/parts/class-header-menu.php
@@ -621,28 +621,53 @@ if ( ! class_exists( 'TC_menu' ) ) :
     function tc_set_sidenav_style( $_css ) {
       $sidenav_width = apply_filters( 'tc_sidenav_width', 330 );
 
+      $_sidenav_mobile_css = '
+          #tc-sn { width: %1$spx;}
+          nav#tc-sn { z-index: 999; }
+          [class*=sn-left].sn-close #tc-sn, [class*=sn-left] #tc-sn{
+            -webkit-transform: translate3d( -100%%, 0, 0 );
+            -moz-transform: translate3d( -100%%, 0, 0 );
+            transform: translate3d(-100%%, 0, 0 ); 
+          }
+          [class*=sn-right].sn-close #tc-sn,[class*=sn-right] #tc-sn {
+            -webkit-transform: translate3d( 100%%, 0, 0 );
+            -moz-transform: translate3d( 100%%, 0, 0 );
+            transform: translate3d( 100%%, 0, 0 ); 
+          }
+         .animating #tc-page-wrap, .sn-open #tc-sn, .tc-sn-visible:not(.sn-close) #tc-sn{
+            -webkit-transform: translate3d( 0, 0, 0 );
+            -moz-transform: translate3d( 0, 0, 0 );
+            transform: translate3d(0,0,0) !important;
+          }
+      ';
+      $_sidenav_desktop_css = '
+          #tc-sn { width: %1$spx;}
+          .tc-sn-visible[class*=sn-left] #tc-page-wrap { left: %1$spx; }
+          .tc-sn-visible[class*=sn-right] #tc-page-wrap { right: %1$spx; }
+          [class*=sn-right].sn-close #tc-page-wrap, [class*=sn-left].sn-open #tc-page-wrap {
+            -webkit-transform: translate3d( %1$spx, 0, 0 );
+            -moz-transform: translate3d( %1$spx, 0, 0 );
+            transform: translate3d( %1$spx, 0, 0 );
+          }
+          [class*=sn-right].sn-open #tc-page-wrap, [class*=sn-left].sn-close #tc-page-wrap {
+            -webkit-transform: translate3d( -%1$spx, 0, 0 );
+            -moz-transform: translate3d( -%1$spx, 0, 0 );
+             transform: translate3d( -%1$spx, 0, 0 );
+          }
+          /* stick the sticky header to the left/right of the page wrapper */
+          .tc-sticky-header.tc-sn-visible:not(.animating)[class*=sn-left] .tc-header { left: %1$spx; }
+          .tc-sticky-header.tc-sn-visible:not(.animating)[class*=sn-right] .tc-header { right: %1$spx; }
+          /* ie<9 breaks using :not */
+          .no-csstransforms3d .tc-sticky-header.tc-sn-visible[class*=sn-left] .tc-header { left: %1$spx; }
+          .no-csstransforms3d .tc-sticky-header.tc-sn-visible[class*=sn-right] .tc-header { right: %1$spx; }
+      ';
+
       return sprintf("%s\n%s",
         $_css,
         sprintf(
-            '#tc-sn { width: %1$spx;}
-             .tc-sn-visible[class*=sn-left] #tc-page-wrap { left: %1$spx; }
-             .tc-sn-visible[class*=sn-right] #tc-page-wrap { right: %1$spx; }
-             [class*=sn-right].sn-close #tc-page-wrap, [class*=sn-left].sn-open #tc-page-wrap {
-               -webkit-transform: translate3d( %1$spx, 0, 0 );
-               -moz-transform: translate3d( %1$spx, 0, 0 );
-               transform: translate3d( %1$spx, 0, 0 );
-             }
-             [class*=sn-right].sn-open #tc-page-wrap, [class*=sn-left].sn-close #tc-page-wrap {
-               -webkit-transform: translate3d( -%1$spx, 0, 0 );
-               -moz-transform: translate3d( -%1$spx, 0, 0 );
-               transform: translate3d( -%1$spx, 0, 0 );
-             }
-             /* stick the sticky header to the left/right of the page wrapper*/
-             .tc-sticky-header.tc-sn-visible:not(.animating)[class*=sn-left] .tc-header { left: %1$spx; }
-             .tc-sticky-header.tc-sn-visible:not(.animating)[class*=sn-right] .tc-header { right: %1$spx; }
-             /* ie<9 breaks using :not */
-             .no-csstransforms3d .tc-sticky-header.tc-sn-visible[class*=sn-left] .tc-header { left: %1$spx; }
-             .no-csstransforms3d .tc-sticky-header.tc-sn-visible[class*=sn-right] .tc-header { right: %1$spx; }',
+            apply_filters('tc_sidenav_inline_css', 
+              apply_filters( 'tc_sidenav_slide_mobile', wp_is_mobile() ) ? $_sidenav_mobile_css : $_sidenav_desktop_css
+            ),
             $sidenav_width
         )
       );
