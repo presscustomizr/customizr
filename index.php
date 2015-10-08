4c8693b88a921b3559c59d7159e127b595055739
nikeo <nicolas@presscustomizr.com>
2015-10-08 16:24:20 +0200

    changed : in index, turn main loop into a dynamic, easier to contextualize loop the $wp_query becomes a parameter all hooks like __loop, __before_loop have now dynamic names => the hook __loop is just a particular case we use by default added a new base class named loop_base whose job are mainly to : 1) agnostically renders a view where we ask it to 2) according to the context (is_single()) instanciates sub classes (like TC_post) which add styles, scripts, and content,

--- index.php
+++ index.php
@@ -14,41 +14,7 @@
 
     <div class="container" role="main">
         <div class="<?php echo implode(' ', apply_filters( 'tc_column_content_wrapper_classes' , array('row' ,'column-content-wrapper') ) ) ?>">
-
-            <?php do_action( '__before_article_container'); ##hook of left sidebar?>
-
-                <div id="content" class="<?php echo implode(' ', apply_filters( 'tc_article_container_class' , array( TC_utils::tc_get_layout( TC_utils::tc_id() , 'class' ) , 'article-container' ) ) ) ?>">
-
-                    <?php do_action ('__before_loop');##hooks the heading of the list of post : archive, search... ?>
-
-                        <?php if ( tc__f('__is_no_results') || is_404() ) : ##no search results or 404 cases ?>
-
-                            <article <?php tc__f('__article_selectors') ?>>
-                                <?php do_action( '__loop' ); ?>
-                            </article>
-
-                        <?php endif; ?>
-
-                        <?php if ( have_posts() && ! is_404() ) : ?>
-                            <?php while ( have_posts() ) : ##all other cases for single and lists: post, custom post type, page, archives, search, 404 ?>
-                                <?php the_post(); ?>
-
-                                <?php do_action ('__before_article') ?>
-                                    <article <?php tc__f('__article_selectors') ?>>
-                                        <?php do_action( '__loop' ); ?>
-                                    </article>
-                                <?php do_action ('__after_article') ?>
-
-                            <?php endwhile; ?>
-
-                        <?php endif; ##end if have posts ?>
-
-                    <?php do_action ('__after_loop');##hook of the comments and the posts navigation with priorities 10 and 20 ?>
-
-                </div><!--.article-container -->
-
-           <?php do_action( '__after_article_container'); ##hook of left sidebar ?>
-
+          <?php do_action('__daloop'); ?>
         </div><!--.row -->
     </div><!-- .container role: main -->
 
