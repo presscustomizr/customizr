e2c6b9a38c513713a736a08599b4e368bf5c8c14
nikeo <nicolas@presscustomizr.com>
2015-10-25 17:26:04 +0100

    pass view instance to template with the global wp_query

--- inc/views/templates/custom.php
+++ inc/views/templates/custom.php
@@ -1,5 +1,5 @@
 <div class="row-fluid">
   <div class="span12">
-    <h1>I AM THE CUSTOM TEMPLATE</h1>
+    <h1>I AM THE CUSTOM TEMPLATE OF THE VIEW <span style="color:yellow"><?php echo $view -> id ?></span></h1>
   </div>
 </div>
