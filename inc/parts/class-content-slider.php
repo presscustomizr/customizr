9af1313649f11dc084ebd0c8a483130a1aae4089
Rocco Aliberti <raliberti84@gmail.com>
2015-10-26 10:04:41 +0100

    Fix: amend broken doc links in the slider class

--- inc/parts/class-content-slider.php
+++ inc/parts/class-content-slider.php
@@ -1017,13 +1017,13 @@ class TC_slider {
     switch ( $id ) {
       case 1 :
         $data['title']        = __( 'Discover how to replace or remove this demo slider.', 'customizr' );
-        $data['link_url']     = implode('/', array('http:/','docs.presscustomizr.com' , 'customizr/content-options/#slider-options') );
+        $data['link_url']     = implode('/', array('http:/','docs.presscustomizr.com' , 'article', '102-customizr-theme-options-front-page' ) ); //do we need an anchor in the doc?
         $data['button_text']  = __( 'Check the front page slider doc &raquo;' , 'customizr');
       break;
 
       case 2 :
         $data['title']        = __( 'Easily create sliders and add them in any posts or pages.', 'customizr' );
-        $data['link_url']     = implode('/', array('http:/','docs.presscustomizr.com' , 'customizr/creating-sliders/') );
+        $data['link_url']     = implode('/', array('http:/','docs.presscustomizr.com' , 'article', '3-creating-a-slider-with-customizr-wordpress-theme' ) );
         $data['button_text']  = __( 'Check the slider doc now &raquo;' , 'customizr');
       break;
     };
