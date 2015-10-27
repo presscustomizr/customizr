1aa0fa651f22b9a9d6d7dbfe43c54c6327fae4e5
nikeo <nicolas@presscustomizr.com>
2015-10-27 19:50:18 +0100

    View : each front end part of the Customizr theme is rendered through a view instance. //Each view inherits its properties from a model instance. //The model decides if it has to instanciate the view or not, according to the context
    //Once properly instanciated, a view does not think to anything else than rendering what and where (hook) we ask it to.
    //A view may add specific properties to its instance, and doing so, extending its underlying model.
    //This class (and children) jobs are :
    //- assign its rendering callback to the specified hook
    //- render either html, WordPress template or more complex content, depending on the its model settings.
    //- when renders on a template, the view must pass its model to the WordPress template through the $wp_query global

---
+++ inc/class-view.php
@@ -0,0 +1,124 @@
+<?php
+//This is the view class. Each front end part of the Customizr theme is rendered through a view instance.
+//Each view inherits its properties from a model instance.
+//The model decides if it has to instanciate the view or not, according to the context
+
+//Once properly instanciated, a view does not think to anything else than rendering what and where (hook) we ask it to.
+//A view may add specific properties to its instance, and doing so, extending its underlying model.
+//This class (and children) jobs are :
+//- assign its rendering callback to the specified hook
+//- render either html, WordPress template or more complex content, depending on the its model settings.
+//- when renders on a template, the view must pass its model to the WordPress template through the $wp_query global
+
+
+if ( ! class_exists( 'TC_View' ) ) :
+  class TC_View extends TC_model {
+    function __construct( $model = array() ) {
+      $keys = array_keys( get_object_vars( $this ) );
+
+      foreach ( $keys as $key ) {
+        if ( isset( $model[ $key ] ) ) {
+          $this->$key = $model[ $key ];
+        }
+      }
+
+
+      //Execute various actions before actually hooking the view to front end
+      //=> takes 2 params
+      //$id
+      //$instance
+      $this -> tc_setup_view();
+
+      //emit event on view instanciation
+      do_action( "view_instanciated", $this -> id );
+
+      //listens to a view pre-render => and fire the tc_apply_registered_changes_to_instance
+      // => a change might have been registered
+      //view id
+      //view params
+      add_action( 'pre_render_view'             , array( $this, 'tc_apply_registered_changes_to_instance' ), 10, 1 );
+    }
+
+
+
+
+    /**********************************************************************************
+    * RENDERS
+    ***********************************************************************************/
+    //hook : $model['hook']
+    //NOTE : the $this here can be the child class $this.
+    public function tc_maybe_render() {
+      //this event is used to check for late deletion or change before actually rendering
+      //will fire tc_apply_registered_changes_to_instance
+      do_action( 'pre_render_view', $this -> id );
+
+      if ( ! apply_filters( "tc_do_render_view_{$this -> id}", true ) )
+        return;
+
+      do_action( "before_render_view_{$this -> id}" );
+        $this -> tc_render();
+      do_action( "after_render_view_{$this -> id}" );
+    }
+
+
+    //might be overriden in the child view if any
+    public function tc_render() {
+      if ( ! empty( $this -> html ) )
+        echo $this -> html;
+
+      if ( ! empty( $this -> template ) ) {
+        //add the view to the wp_query wp global
+        set_query_var( "{$this -> template}_model", $this );
+        get_template_part( "inc/views/templates/{$this -> template}" );
+      }
+      // $path = '';
+      // $part = '';
+      // get_template_part( $path , $part );
+
+      if ( ! empty( $this -> callback ) )
+        CZR() -> helpers -> tc_fire_cb( $this -> callback, $this -> cb_params );
+    }
+
+
+    //updates the view properties with the requested args
+    //stores the clean and updated args in a view property.
+    //@return void()
+    //called directly sometimes
+    //fired on 'pre_render_view'
+    //fired on tc_change if view is instanciated
+    public function tc_apply_registered_changes_to_instance( $id, $new_params = array() ) {
+      if ( ! $this -> tc_has_registered_change( $id ) )
+        return;
+
+      $new_params = empty($new_params) ? tc_get_registered_changes( $id ) : $new_params;
+
+      $this -> tc_update_model_instance( $id, $new_params );
+
+      //This event will trigger a removal of the change from the change list
+      //=> tc_deregister_change
+      do_action('registered_changed_applied' , $id);
+    }
+
+
+    //at this stage, the view is instanciated
+    //@return void()
+    private function tc_update_model_instance( $id, $new_params ) {
+      //get current params
+      $current_params = $this -> tc_get_view($id);
+      if ( ! $current_params || ! is_array($current_params) )
+        return;
+      //pre-process new params
+      $new_params = wp_parse_args( $new_params, $current_params );
+
+      //update the modified view properties
+      //=> will automatically trigger the collection update
+      foreach ($new_params as $property => $value) {
+        if ( $value != $current_params[$property] )
+          $current_params['_instance'] -> tc_set_property( $property, $value );
+      }
+    }
+
+
+
+  }//class
+endif;
