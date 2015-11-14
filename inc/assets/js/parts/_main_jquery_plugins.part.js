presscustomizr/customizr

commit a5417cac09f9326d4432db563174a88405eeddd0

Fix: better handling of the simple-load event triggered on holders fixes #377

inc/assets/js/parts/_main_jquery_plugins.part.js
@@ -22,7 +22,7 @@ var czrapp = czrapp || {};
           $_to_center      = $( _where ).find('img');
 
       if ( smartLoadEnabled ) {
-        $_to_center      = _.filter( $( _where ).find('img') , function ( img ) {
+        $_to_center      = _.filter( $_to_center , function ( img ) {
           return $(img).is(TCParams.imgSmartLoadOpts.opts.excludeImg.join());
         } );//filter
       }//if
@@ -31,8 +31,16 @@ var czrapp = czrapp || {};
         $( _where ).imgSmartLoad(
           _.size( TCParams.imgSmartLoadOpts.opts ) > 0 ? TCParams.imgSmartLoadOpts.opts : {}
         );
-
-      this.triggerSimpleLoad($_to_center);
+    
+      //simple-load event on holders needs to be needs to be triggered with a certain delay otherwise holders will be misplaced (centering)
+      if ( 1 == TCParams.centerAllImg ) {
+        self = this;
+        setTimeout( function(){ 
+            self.triggerSimpleLoad( _.filter( $_to_center, function( img ) { 
+                return $(img).hasClass('tc-holder-img'); 
+            } ) ); }, 
+            300 );
+      }
     },
 
 

commit ac44cd110a55cd3db508f7b71e7aca95c2b7bfd0

Fix: typo in front-js: fix referring to the global variable self use local scoped instead fixes #404 ( Bug introduced in a5417cac09f9326d4432db563174a88405eeddd0 )

inc/assets/js/parts/_main_jquery_plugins.part.js
@@ -34,7 +34,7 @@ var czrapp = czrapp || {};
     
       //simple-load event on holders needs to be needs to be triggered with a certain delay otherwise holders will be misplaced (centering)
       if ( 1 == TCParams.centerAllImg ) {
-        self = this;
+        var self = this;
         setTimeout( function(){ 
             self.triggerSimpleLoad( _.filter( $_to_center, function( img ) { 
                 return $(img).hasClass('tc-holder-img'); 