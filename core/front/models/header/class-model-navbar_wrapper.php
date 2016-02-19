ae54c36636606cad20fc503cc1d32eff357bee13
Rocco Aliberti <raliberti84@gmail.com>
2016-02-19 16:47:18 +0100

    Fix: navbar_wrapper css classes depending on some user options

--- core/front/models/header/class-model-navbar_wrapper.php
+++ core/front/models/header/class-model-navbar_wrapper.php
@@ -9,8 +9,27 @@ class TC_navbar_wrapper_model_class extends TC_Model {
   * return model params array() 
   */
   function tc_extend_params( $model = array() ) {
-    $model[ 'class' ] = implode( " ", apply_filters( 'tc_navbar_wrapper_class', array('navbar-wrapper', 'clearfix', 'span9') ) );
+    $_class = $this -> get_navbar_classes();  
+    $model[ 'class' ] = implode( " ", apply_filters( 'tc_navbar_wrapper_class', $_class ) );
     
     return $model;
+  }
+
+  function get_navbar_classes() {
+    $_classes = array('navbar-wrapper', 'clearfix', 'span9');
+
+    $_classes = ( ! wp_is_mobile() && 0 != esc_attr( TC_utils::$inst->tc_opt( 'tc_menu_submenu_fade_effect') ) ) ? array_merge( $_classes, array( 'tc-submenu-fade' ) ) : $_classes;
+    
+    $_classes = ( 0 != esc_attr( TC_utils::$inst->tc_opt( 'tc_menu_submenu_item_move_effect') ) ) ? array_merge( $_classes, array( 'tc-submenu-move' ) ) : $_classes;
+    
+    $_classes = ( ! wp_is_mobile() && 'hover' == esc_attr( TC_utils::$inst->tc_opt( 'tc_menu_type' ) ) ) ? array_merge( $_classes, array( 'tc-open-on-hover' ) ) : array_merge( $_classes, array( 'tc-open-on-click' ) );
+  //Navbar menus positions (not sidenav)
+  //CASE 1 : regular menu (sidenav not enabled), controled by option 'tc_menu_position'
+  //CASE 2 : second menu ( is_secondary_menu_enabled ?), controled by option 'tc_second_menu_position'
+    if ( apply_filters( 'tc_is_sidenav_enabled', 'aside' == esc_attr( TC_utils::$inst->tc_opt( 'tc_menu_style' ) ) ) )
+      array_push( $_classes , esc_attr( TC_utils::$inst->tc_opt( 'tc_menu_position') ) );
+    if ( apply_filters( 'tc_is_second_menu_enabled', (bool)esc_attr( TC_utils::$inst->tc_opt( 'tc_display_second_menu' ) ) ) )
+      array_push( $_classes , esc_attr( TC_utils::$inst->tc_opt( 'tc_second_menu_position') ) );
+    return $_classes;    
   }
 }
