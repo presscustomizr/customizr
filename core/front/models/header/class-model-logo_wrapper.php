d1b639b2dcaa2fac2317e818a5051ad3688628f4
Rocco Aliberti <raliberti84@gmail.com>
2016-02-19 16:59:37 +0100

    Fix: logo and title wrappers position depending on the header layout

--- core/front/models/header/class-model-logo_wrapper.php
+++ core/front/models/header/class-model-logo_wrapper.php
@@ -13,7 +13,8 @@ class TC_logo_wrapper_model_class extends TC_Model {
   * return model params array() 
   */
   function tc_extend_params( $model = array() ) {
-    $model[ 'logo_wrapper_class' ] = implode( ' ', apply_filters( 'tc_logo_class', array( 'brand', 'span3', 'pull-left'), $model ) );
+    $_class                        = $this -> get_logo_wrapper_class();  
+    $model[ 'logo_wrapper_class' ] = implode( ' ', apply_filters( 'tc_logo_class', $_class, $model ) );
     $model[ 'link_class' ]         = 'site-logo';
     $model[ 'link_title' ]         = apply_filters( 'tc_site_title_link_title', sprintf( '%1$s | %2$s' ,
                                              __( esc_attr( get_bloginfo( 'name' ) ) ), 
@@ -24,5 +25,13 @@ class TC_logo_wrapper_model_class extends TC_Model {
     $model[ 'link_url'   ]         = apply_filters( 'tc_logo_link_url', esc_url( home_url( '/' ) ), $model );
 
     return $model;
+  }
+
+  /* the same in the title wrapper class, some kind of unification will be needed IMHO */
+  function get_logo_wrapper_class() {
+    $_class     = array( 'brand', 'span3' );  
+    $_layout    = esc_attr( TC_utils::$inst->tc_opt( 'tc_header_layout') );
+    $_class[] = 'right' == $_layout ? 'pull-right' : 'pull-left';
+    return $_class;
   }
 }
--- core/front/models/header/class-model-title_wrapper.php
+++ core/front/models/header/class-model-title_wrapper.php
@@ -13,7 +13,9 @@ class TC_title_wrapper_model_class extends TC_Model {
   * return model params array() 
   */
   function tc_extend_params( $model = array() ) {
-    $model[ 'title_wrapper_class' ] = implode( ' ', apply_filters( 'tc_logo_class',  array( 'brand', 'span3', 'pull-left'), $model ) );
+    $_class                         = $this -> get_title_wrapper_class();  
+
+    $model[ 'title_wrapper_class' ] = implode( ' ', apply_filters( 'tc_logo_class', $_class, $model ) );
     $model[ 'tag'        ]          = apply_filters( 'tc_site_title_tag', 'h1', $model);
     $model[ 'link_class' ]          = 'site-title';
     $model[ 'link_title' ]          = apply_filters( 'tc_site_title_link_title', sprintf( '%1$s | %2$s' ,
@@ -25,5 +27,13 @@ class TC_title_wrapper_model_class extends TC_Model {
     $model[ 'link_url'   ]          = apply_filters( 'tc_logo_link_url', esc_url( home_url( '/' ) ), $model );
 
     return $model;
+  }
+
+  /* the same in the logo wrapper class, some kind of unification will be needed IMHO */
+  function get_title_wrapper_class() {
+    $_class     = array( 'brand', 'span3' );  
+    $_layout    = esc_attr( TC_utils::$inst->tc_opt( 'tc_header_layout') );
+    $_class[] = 'right' == $_layout ? 'pull-right' : 'pull-left';
+    return $_class;
   }
 }
