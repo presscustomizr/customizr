d835b6498a846b30b2de353f8b736f1e704671bb
Rocco Aliberti <raliberti84@gmail.com>
2016-02-23 10:29:14 +0100

    Add: introduce footer model

--- templates/footer/footer.php
+++ templates/footer/footer.php
@@ -1,7 +1,3 @@
-<footer>
-  <h1>I AM THE TEMPLATE OF THE VIEW <span style="color:blue"><?php echo $footer_model -> id ?></span></h1>
-  <p style="text-align:center"><strong style="font-size:50px;font-family:arial">FOOTER</strong></p>
-
+<footer id="footer" class="<?php echo $footer_model -> class ?>">
   <?php do_action( '__footer__'); ?>
-
 </footer>
