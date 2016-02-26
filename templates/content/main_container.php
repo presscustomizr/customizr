9e8754df7134d97fdfca0809f662399ee1502e99
Rocco Aliberti <raliberti84@gmail.com>
2016-02-26 15:12:32 +0100

    Fix: add column content wrapper in the main container

--- templates/content/main_container.php
+++ templates/content/main_container.php
@@ -1,3 +1,5 @@
-<div class="container" role="main">
-  <?php do_action( '__main_container__') ?>
+<div class="<?php echo $main_container_model -> class ?>" role="main">
+  <div class="<?php echo $main_container_model -> column_content_class ?>">
+    <?php do_action( '__main_container__') ?>
+  </div>
 </div>
