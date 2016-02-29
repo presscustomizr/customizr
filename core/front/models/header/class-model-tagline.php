f0a89117b2241056169783adf3259540a74e252a
nikeo <nicolas@presscustomizr.com>
2016-02-29 10:44:51 +0100

    local changes

--- core/front/models/header/class-model-tagline.php
+++ core/front/models/header/class-model-tagline.php
@@ -9,8 +9,8 @@ class TC_tagline_model_class extends TC_Model {
   /*
   * @override
   * fired before the model properties are parsed
-  * 
-  * return model params array() 
+  *
+  * return model params array()
   */
   function tc_extend_params( $model = array() ) {
     $model[ 'content' ]    = apply_filters( 'tc_tagline_text', __( esc_attr( get_bloginfo( 'description' ) ) ), $model );
@@ -31,21 +31,22 @@ class TC_tagline_model_class extends TC_Model {
   /**
   * @override
   * Allow filtering of the header class by registering to its pre view rendering hook
-  */ 
+  */
   function tc_maybe_filter_views_model() {
     parent::tc_maybe_filter_views_model();
     add_action( 'pre_rendering_view_header', array( $this, 'pre_rendering_view_header_cb' ) );
   }
+
   /**
   * parse this model properties for rendering
-  */ 
+  */
   function pre_rendering_my_view_cb( $model ) {
     $model -> class = join( ' ', array_unique( $model -> class ) );
   }
 
   /**
   * parse header model before rendering to add 'destkop'&&'sticky' tagline visibility class
-  */ 
+  */
   function pre_rendering_view_header_cb( $header_model ) {
     if ( 'mobile' != $this -> type && esc_attr( TC_utils::$inst->tc_opt( "tc_sticky_header") || TC___::$instance -> tc_is_customizing() ) ) {
       $_class =        0 != esc_attr( TC_utils::$inst->tc_opt( 'tc_sticky_show_tagline') ) ? 'tc-tagline-on' : 'tc-tagline-off';
