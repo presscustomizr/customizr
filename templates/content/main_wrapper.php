bf384f8ff5e83649c36ffd0875433714d65796f0
Rocco Aliberti <raliberti84@gmail.com>
2016-02-29 14:44:45 +0100

    Fix: fix main-wrapper div id

--- templates/content/main_wrapper.php
+++ templates/content/main_wrapper.php
@@ -1,3 +1,3 @@
-<div id="main_wrapper" class="<?php echo $main_wrapper_model -> class ?>">
+<div id="main-wrapper" class="<?php echo $main_wrapper_model -> class ?>">
   <?php do_action( '__main_wrapper__') ?>
 </div>
