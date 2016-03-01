f7252977c44872437ef741872835f80f5966ee8e
Rocco Aliberti <raliberti84@gmail.com>
2016-03-01 16:00:07 +0100

    Imp:better rendering of the rating block in the customizer + small fixes - do not overflow the collapsing arrow - for the upcoming wp 4.5 allow coexistence with the devices   (responsiveness) buttons   -- for this purpose an override of the api.Section has been needed in   order to re-set the height of the section content dynamically - fine tune of the customizer elements when in subpanel when the customizer settings panel width becomes 100% (ww<641px ) - set the height of the donate block to 0 when in subpanels so it   doesn't hurt the panel top offset (hence its opened section ) as the   panel top offset isn't dynamically calculated when resizing the window

--- inc/admin/js/parts/_control.js
+++ inc/admin/js/parts/_control.js
@@ -34,6 +34,33 @@
     }
   };
 
+  /* mokey patch for the content height set */
+  // backup the original function
+  var _original_section_initialize = api.Section.prototype.initialize;
+  api.Section.prototype.initialize = function( id, options ) {
+    //call the original constructor
+    _original_section_initialize.apply( this, [id, options] );
+    var section = this;
+    
+    this.expanded.callbacks.add( function( _expanded ) {
+      if ( ! _expanded )
+        return;
+
+ 	  var container = section.container.closest( '.wp-full-overlay-sidebar-content' ),
+          content = section.container.find( '.accordion-section-content' ); 
+      //content resizing to the container height
+      _resizeContentHeight = function() {
+        console.log('pippo');  
+        content.css( 'height', container.innerHeight() );
+	  };
+      _resizeContentHeight();
+      //this is set to off in the original expand callback if 'expanded' is false
+      $( window ).on( 'resize.customizer-section', _.debounce( _resizeContentHeight, 110 ) );
+    });
+  };
+  /* end monkey patch */
+
+
   /* Multiple Picker */
   /**
    * @constructor
