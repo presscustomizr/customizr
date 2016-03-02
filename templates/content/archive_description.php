c8ad86cba42b45a90697f2b8f4a2c48e615f4a39
Rocco Aliberti <raliberti84@gmail.com>
2016-03-02 16:32:12 +0100

    Imp: initial merging of all convenient lists of posts in the same model

--- templates/content/archive_description.php
+++
@@ -1,1 +0,0 @@
-<div class="archive-meta"><?php echo $archive_description_model -> description ?></div>
--- templates/content/archive_title.php
+++
@@ -1,1 +0,0 @@
-<h1 class="entry-title <?php echo $archive_title_model -> class ?>"><?php echo $archive_title_model -> pre_title ?><?php echo $archive_title_model -> title ?></h1>
