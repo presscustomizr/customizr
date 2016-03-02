ba23c5a14ab874223677d5c1d47f005a6542f24e
Rocco Aliberti <raliberti84@gmail.com>
2016-03-02 18:42:42 +0100

    Imp: start post list description modelling in this case the author post list description (showing the bio) should have its own model + it's own templatel, this is making me think about the overall idea of merging titles models in one model as in that case too the controllers will create redundancy or will be too much dumb

--- templates/content/posts_list_title.php
+++ templates/content/posts_list_title.php
@@ -1,1 +1,1 @@
-<h1 class="entry-title <?php echo $posts_list_title_model -> class ?>"><?php echo $posts_list_title_model -> pre_title ?><?php echo $posts_list_title_model -> title ?></h1>
+<h1 class="entry-title <?php echo $posts_list_title_model -> class ?>"><?php echo $posts_list_title_model -> pre_title ?> <?php echo $posts_list_title_model -> title ?></h1>
