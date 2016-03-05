a419bfc7e73f5dbeccabacd2a9f7b7f3c1b76ff7
Rocco Aliberti <raliberti84@gmail.com>
2016-03-05 12:52:02 +0100

    Fix: with the new colophon layout system we can get rid of the paragraph back to top link wrapper

--- core/front/models/footer/class-model-footer_btt.php
+++ core/front/models/footer/class-model-footer_btt.php
@@ -12,8 +12,7 @@ class TC_footer_btt_model_class extends TC_Model {
   * return model params array() 
   */
   function tc_extend_params( $model = array() ) {
-    $model[ 'element_class' ]             = array('backtop');
-    $model[ 'inner_class' ]               = array( is_rtl() ? 'pull-left' : 'pull-right' );
+    $model[ 'element_class' ]             = array( 'backtop', is_rtl() ? 'pull-left' : 'pull-right' );
     $model[ 'link_class' ]                = array( 'back-to-top' );
     $model[ 'text' ]                      = __( 'Back to top', 'customizr' );
 
@@ -24,7 +23,6 @@ class TC_footer_btt_model_class extends TC_Model {
   * parse this model properties for rendering
   */ 
   function pre_rendering_my_view_cb( $model ) {
-    $model -> inner_class   = join( ' ', $model -> inner_class );    
     $model -> element_class = join( ' ', $model -> element_class );    
     $model -> link_class    = join( ' ', $model -> link_class );    
   }
