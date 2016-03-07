26686f48a67186d926459714fd7adc9c0aaad18e
Rocco Aliberti <raliberti84@gmail.com>
2016-03-07 15:04:06 +0100

    Fix: better headings context based

--- core/front/models/content/class-model-post_page_headings.php
+++ core/front/models/content/class-model-post_page_headings.php
@@ -1,6 +1,19 @@
 <?php
 class TC_post_page_headings_model_class extends TC_headings_model_class {
   public $type = 'content';
+  private static $post_formats_with_no_heading   = array( 'aside' , 'status' , 'link' , 'quote' );
+
+  function __construct( $model = array() ) {
+    parent::__construct( $model );
+    
+    //render this?
+    add_filter( "tc_do_render_view_{$this -> id}",  array( $this, 'tc_post_has_headings') );
+  }
+
+  function tc_post_has_headings() {
+    return ! ( in_array( get_post_format(), apply_filters( 'tc_post_formats_with_no_heading', self::$post_formats_with_no_heading ) ) );
+  }
+
   /**
   * @override
   * fired before the model properties are parsed
@@ -10,4 +23,5 @@ class TC_post_page_headings_model_class extends TC_headings_model_class {
   function tc_get_class( $model = array() ) {
     return 'entry-header';  
   }
+
 }
--- templates/content/post_page_title.php
+++ templates/content/post_page_title.php
@@ -1,1 +1,1 @@
-<?php call_user_func( $post_page_title_model -> title_cb ); ?>
+<a href="<?php the_permalink() ?>"><?php call_user_func( $post_page_title_model -> title_cb ); ?></a>
