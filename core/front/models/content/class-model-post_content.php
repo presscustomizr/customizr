c5cc91b44c1b1c0e0209e878b151ab4ae83f4ed0
Rocco Aliberti <raliberti84@gmail.com>
2016-03-07 11:41:20 +0100

    Imp: initial post/page content implementation

---
+++ core/front/models/content/class-model-post_content.php
@@ -0,0 +1,17 @@
+<?php
+class TC_post_content_model_class extends TC_post_page_content_model_class {
+   
+  /**
+  * @override
+  * fired before the model properties are parsed
+  * 
+  * return model params array() 
+  */  
+  function tc_extend_params( $model = array() ) {
+    $icon_class             = in_array( get_post_format(), array(  'quote' , 'aside' , 'status' , 'link' ) ) ? apply_filters( 'tc_post_format_icon', 'format-icon' ) :'' ;
+
+    $model['element_class'] = array( $this -> element_class, $icon_class );
+
+    return $model;
+  }
+}
---
+++ templates/content/article.php
@@ -0,0 +1,3 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class( $article_model -> element_class ); ?>>
+  <?php do_action( "__article__" ) ?>
+</article>
