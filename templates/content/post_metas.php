6daa24fe73714205f25b58d0b79204bbcaaef515
Rocco Aliberti <raliberti84@gmail.com>
2016-03-08 13:38:33 +0100

    Imp: post metas initial mapping

--- templates/content/post_metas.php
+++ templates/content/post_metas.php
@@ -1,3 +1,16 @@
 POST METAS:
-CATS : <?php echo $post_metas_model -> tc_get_cat_list(); ?>
-TAGS : <?php echo $post_metas_model -> tc_get_tag_list(); ?>
+<?php if ( $post_metas_model -> tc_get_cat_list() ) : ?>
+PUB : <?php echo $post_metas_model -> tc_get_publication_date(); ?>
+<?php endif; ?>
+<?php if ( $post_metas_model -> tc_get_cat_list() ) : ?>
+ CATS : <?php echo $post_metas_model -> tc_get_cat_list(); ?>
+<?php endif; ?>
+<?php if ( $post_metas_model -> tc_get_tag_list() ) : ?>
+ TAGS : <?php echo $post_metas_model -> tc_get_tag_list(); ?>
+<?php endif; ?>
+<?php if ( $post_metas_model -> tc_get_author() ) : ?>
+ AUTHOR : <?php echo $post_metas_model -> tc_get_author(); ?>
+<?php endif; ?>
+<?php if ( $post_metas_model -> tc_get_cat_list() ) : ?>
+ UPDATE : <?php echo $post_metas_model -> tc_get_update_date(); ?>
+<?php endif; ?>
