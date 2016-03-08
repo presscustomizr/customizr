c363b04e61318e00013e4ef72a7435f6a885c045
Rocco Aliberti <raliberti84@gmail.com>
2016-03-08 11:15:36 +0100

    Fix: posts with no headings are actually posts with no titles the header tag is needed to display metas

--- core/front/models/content/class-model-post_page_title.php
+++ core/front/models/content/class-model-post_page_title.php
@@ -1,6 +1,20 @@
 <?php
 class TC_post_page_title_model_class extends TC_Model {
   public $title_cb;
+  private static $post_formats_with_no_heading   = array( 'aside' , 'status' , 'link' , 'quote' );
+
+  function __construct( $model = array() ) {
+    parent::__construct( $model );
+    
+    //render this?
+    add_filter( "tc_do_render_view_{$this -> id}",  array( $this, 'tc_post_has_headings') );
+  }
+
+  function tc_post_has_headings() {
+    return ! ( in_array( get_post_format(), apply_filters( 'tc_post_formats_with_no_heading', self::$post_formats_with_no_heading ) ) );
+  }
+
+
   /**
   * @override
   * fired before the model properties are parsed
