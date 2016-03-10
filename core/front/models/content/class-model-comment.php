726dcbeac89420b1e531b40f399b0faaa91956b8
Rocco Aliberti <raliberti84@gmail.com>
2016-03-10 14:59:50 +0100

    Imp: complete the single comment template/module

--- core/front/models/content/class-model-comment.php
+++ core/front/models/content/class-model-comment.php
@@ -1,8 +1,21 @@
 <?php
 class TC_comment_model_class extends TC_Model {
-    public $args;
-    public $comment;
-    public $depth;
+  public $args;
+  public $comment;
+  public $comment_text;
+  public $depth;
+  public $comment_wrapper_class;
+  public $comment_avatar_class;
+  public $comment_content_class;
+  public $comment_reply_btn_class;
+  public $comment_avatar_size;
+
+  public $comment_reply_link_args;
+
+  //bools
+  public $is_current_post_author;
+  public $has_edit_button;
+  public $is_awaiting_moderation;
 
   function __construct( $model = array() ) {
     parent::__construct( $model );
@@ -10,11 +23,40 @@ class TC_comment_model_class extends TC_Model {
     //parse the comment callback params so to set this propertied
     add_filter( 'tc_comment_callback_params', array( $this, 'tc_set_comment_properties'), 10, 3 );
     //render this?
-    add_filter( 'tc_do_render_view_{$this -> id}',  'have_comments' ); 
+    add_filter( "tc_do_render_view_{$this -> id}", array( $this, 'tc_maybe_render_comment' ) ); 
+  }
+  /**
+  * @override
+  * fired before the model properties are parsed
+  * 
+  * return model params array() 
+  */
+  function tc_extend_params( $model = array() ) {
+    $model[ 'comment_wrapper_class' ]   = apply_filters( 'tc_comment_wrapper_class', array('row-fluid') );
+    $model[ 'comment_avatar_class'  ]   = apply_filters( 'tc_comment_avatar_class', array('comment-avatar', 'span2') );
+    $model[ 'comment_content_class' ]   = apply_filters( 'tc_comment_content_class', array('span10') );
+    $model[ 'comment_reply_btn_class' ] = apply_filters( 'tc_comment_reply_btn_class', array('reply btn btn-small' ) );
+    $model[ 'comment_avatar_size' ]     = apply_filters( 'tc_comment_avatar_size', 80 );
+
+    return $model;
+  }
+
+  function tc_maybe_render_comment() {
+    return $this -> visibility;     
   }
 
   /**
-   * Template for comments and pingbacks.
+  * @override
+  * parse this model properties for rendering
+  */
+  function pre_rendering_my_view_cb( $model ) {
+    parent::pre_rendering_my_view_cb( $model );
+    foreach ( array('wrapper', 'content', 'avatar', 'reply_btn' ) as $property )
+      $model -> {"comment_{$property}_class"} = $this -> tc_stringify_model_property( "comment_{$property}_class" );
+  }
+
+  /**
+   * Template for comments
    *
    *
    * Used as a callback by wp_list_comments() for displaying the comments.
@@ -22,9 +64,28 @@ class TC_comment_model_class extends TC_Model {
    * @package Customizr
    * @since Customizr 1.0
   */
-    function tc_set_comment_properties( $comment, $args, $depth ) {
-      $this -> comment = $comment;
-      $this -> args = $args;
-      $this -> depth = $depth;  
+  function tc_set_comment_properties( $comment, $args, $depth ) {
+    global $post;
+
+    if ( ! ( $tc_show_comment_content = 1 == get_option( 'thread_comments' ) && ($depth < $args['max_depth']) && comments_open() ) ){
+      $this -> visibility = false;
+      return;
+    }
+    $this -> comment                 = $comment;
+    $this -> comment_text            = apply_filters( 'comment_text', get_comment_text( $comment->comment_ID , $args ), $comment, $args );
+    
+    $this -> comment_reply_link_args = array_merge( $args,
+        array(
+          'reply_text' => __( 'Reply' , 'customizr' ).' <span>&darr;</span>',
+          'depth' => $depth,
+          'max_depth' => $args['max_depth'] ,
+          'add_below' => apply_filters( 'tc_comment_reply_below' , 'comment' )
+        )
+    );
+
+    $this -> is_current_post_author     = ( $comment->user_id === $post->post_author );
+    $this -> has_edit_button            = ! TC___::$instance -> tc_is_customizing();
+
+    $this -> is_awaiting_moderation     = '0' == $comment->comment_approved;
   }
 }
