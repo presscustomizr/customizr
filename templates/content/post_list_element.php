36fd5bea6e3108e32db78395a173a0d79e813f14
Rocco Aliberti <raliberti84@gmail.com>
2016-03-14 11:50:31 +0100

    Imp: implement post list content retrieval/rendering

--- templates/content/post_list_element.php
+++ templates/content/post_list_element.php
@@ -1,1 +1,3 @@
-<?php call_user_func( $post_list_element_model -> content_cb ) ?>
+<section class="<?php echo $post_list_element_model -> content_class ?>">
+<?php call_user_func($post_list_element_model -> content_cb ) ?>
+</section>
