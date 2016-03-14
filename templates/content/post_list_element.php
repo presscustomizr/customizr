6c7cd0b80f74912be6308747fbfddce72cae4fad
Rocco Aliberti <raliberti84@gmail.com>
2016-03-14 12:00:59 +0100

    Fix: dynamic tag for the post list element section for the content div for the thumb

--- templates/content/post_list_element.php
+++ templates/content/post_list_element.php
@@ -1,3 +1,3 @@
-<section class="<?php echo $post_list_element_model -> content_class ?>">
+<<?php echo $post_list_element_model -> content_tag ?> class="<?php echo $post_list_element_model -> content_class ?>">
 <?php call_user_func($post_list_element_model -> content_cb ) ?>
-</section>
+</<?php echo $post_list_element_model -> content_tag ?>>
