8b5756ed1deff3f3d247fc3f3010999e2ba20d6f
Rocco Aliberti <raliberti84@gmail.com>
2016-03-18 11:03:12 +0100

    Fix: add post list rectangular inline style

--- core/front/models/content/class-model-post_list_rectangular_thumbnail.php
+++ core/front/models/content/class-model-post_list_rectangular_thumbnail.php
@@ -18,4 +18,23 @@ class TC_post_list_rectangular_thumbnail_model_class extends TC_post_list_thumbn
     $_position = esc_attr( TC_utils::$inst->tc_opt( 'tc_post_list_thumb_position' ) );  
     return ( 'top' == $_position || 'bottom' == $_position ) ? 'tc_rectangular_size' : $_default_size;
   }
+
+  /**
+  * hook : tc_user_options_style
+  * @return css string
+  *
+  * @package Customizr
+  * @since Customizr 3.2.6
+  */
+  function tc_user_options_style_cb( $_css ) {
+    $_list_thumb_height     = esc_attr( TC_utils::$inst->tc_opt( 'tc_post_list_thumb_height' ) );
+    $_list_thumb_height     = (! $_list_thumb_height || ! is_numeric($_list_thumb_height) ) ? 250 : $_list_thumb_height;
+    return sprintf("%s\n%s",
+      $_css,
+      ".tc-rectangular-thumb {
+        max-height: {$_list_thumb_height}px;
+        height :{$_list_thumb_height}px
+      }\n"
+    );
+  }
 }
