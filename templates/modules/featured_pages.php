db103ed3b30561d39cffe03ca593f69de58a67d4
Rocco Aliberti <raliberti84@gmail.com>
2016-03-19 12:59:10 +0100

    Imp: add featured pages featurette divider As the wrapper is not in the template the featurette divider will be displayed inside the .marketing.container, while in Customizr it was displayed just below. This, in ui terms, makes no difference as it always grows as the .marketing.container grows

--- templates/modules/featured_pages.php
+++ templates/modules/featured_pages.php
@@ -1,6 +1,7 @@
 <?php 
   foreach ( $featured_pages_model -> featured_pages as $index => $featured_page ) {
     do_action( "in_featured_pages_{$featured_pages_model -> id}", $index, $featured_page );
-    do_action( "__featured_page__");
+    do_action( "__featured_page__" );
   }
-
+?>
+<hr class="featurette-divider <?php echo $featured_pages_model -> hook ?>">
