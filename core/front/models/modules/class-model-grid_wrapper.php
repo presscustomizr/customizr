c34726388d913bbdcd7ab9fd3c9e790602b7479f
Rocco Aliberti <raliberti84@gmail.com>
2016-03-21 12:13:16 +0100

    Imp: very start of the grid porting: still a mess, needs a lot of refinements

--- core/front/models/modules/class-model-grid_wrapper.php
+++ core/front/models/modules/class-model-grid_wrapper.php
@@ -2,7 +2,14 @@
 class TC_grid_wrapper_model_class extends TC_article_model_class {
   public $is_first_of_row;
   public $is_last_of_row;
+
+  public $is_loop_start;
+  public $is_loop_end;
+
   public $section_cols;
+
+  public $figure_class;
+  public $has_heading_in_caption;
 //  public $text;
 
   private $expanded_sticky = false;
@@ -20,37 +27,89 @@ class TC_grid_wrapper_model_class extends TC_article_model_class {
     add_action( $this -> hook, array( $this, 'set_this_properties' ), 0 );
   }
 
-  function set_this_properties() {
-    //section properties which refers to the section wrapper
-    $section_wrapper = $this -> tc_get_section_wrapper_properties();
 
-    $this -> tc_update( $section_wrapper );
+  /**
+  * @override
+  * fired before the model properties are parsed
+  * 
+  * return model params array() 
+  */
+  function tc_extend_params( $model = array() ) {
+    $element_class = array();  
+    
+    //wrapper classes based on the user options
+    if ( esc_attr( TC_utils::$inst->tc_opt( 'tc_grid_shadow') ) )
+      array_push( $element_class, 'tc-grid-shadow' );
+    if ( esc_attr( TC_utils::$inst->tc_opt( 'tc_grid_bottom_border') ) )
+      array_push( $element_class, 'tc-grid-border' );
+
+    $model[ 'element_class' ] = $element_class;
+
+    return $model;
   }
 
+
+
+  function set_this_properties() {
+    $element_wrapper        = $this -> tc_get_element_wrapper_properties();
+    //figure class
+    $figure_properties      = $this -> tc_get_figure_properties();
+
+    $has_heading_in_caption = array(true);
+    //section properties which refers to the section row wrapper
+    $section_row_wrapper    = $this -> tc_get_section_row_wrapper_properties();
+    $this -> tc_update( array_merge( $element_wrapper, $section_row_wrapper, $figure_properties, $has_heading_in_caption ) );
+
+    //hack
+    set_query_var( 'section_cols', $section_row_wrapper['section_cols'] );
+  }
+
+  /*
+  *
+  */
+  function tc_get_figure_properties() {
+    $has_thumb           = $this -> tc_grid_show_thumb();
+    $figure_class        = array( $has_thumb ? 'has-thumb' : 'no-thumb' );
+
+    //if 1 col layout or current post is the expanded => golden ratio should be disabled
+    if ( ( '1' == $this -> tc_get_grid_cols() || $this -> tc_force_current_post_expansion() ) && ! wp_is_mobile() )
+      array_push( $figure_class, 'no-gold-ratio' );
+    return compact( 'figure_class', 'has_thumb' );
+  }
+
+  /*
+  * post list wrapper
+  */
+  function tc_get_element_wrapper_properties() {
+    global $wp_query;
+    $is_loop_start = 0 == $wp_query -> current_post;
+    $is_loop_end   = $wp_query -> current_post == $wp_query -> post_count ;
+
+    return compact( 'is_loop_start', 'is_loop_end' );
+  }
 
   /*
   * Wrap articles in a grid section
   */
-  function tc_get_section_wrapper_properties() {
+  function tc_get_section_row_wrapper_properties() {
     global $wp_query;
     
     $current_post      = $wp_query -> current_post;
     $start_post        = $this -> expanded_sticky ? 1 : 0;
-    $cols              = $this -> tc_get_grid_section_cols();
+    $section_cols      = $this -> tc_get_grid_section_cols();
 
     $is_first_of_row = false;
     $is_last_of_row  = false;
-    $section_cols    = '';
 
-    if ( $start_post == $current_post || 0 == ( $current_post - $start_post ) % $cols ) {
+    if ( $start_post == $current_post || 0 == ( $current_post - $start_post ) % $section_cols ) 
       $is_first_of_row = true;
-      $section_cols    = $cols;
-    }
-    if ( $wp_query->post_count == ( $current_post + 1 ) || 0 == ( ( $current_post - $start_post + 1 ) % $cols ) )
+   
+    if ( $wp_query->post_count == ( $current_post + 1 ) || 0 == ( ( $current_post - $start_post + 1 ) % $section_cols ) )
       $is_last_of_row  = true;
 
     return compact( 'is_first_of_row', 'is_last_of_row', 'section_cols' );
   }
+
 
   /* retrieves number of cols option, and wrap it into a filter */
   private function tc_get_grid_cols() {
@@ -76,14 +135,6 @@ class TC_grid_wrapper_model_class extends TC_article_model_class {
     return ( $this -> expanded_sticky && 0 == $wp_query -> current_post );
   }
 
-  /**
-  * @override 
-  * Apply proper class to articles selectors to control articles width
-  * hook : tc_post_list_selectors
-  */
-  function tc_grid_set_article_selectors( $selectors ){
-    return str_replace( 'row-fluid', $_class, $selectors );
-  }
 
 
   /**
@@ -105,4 +156,20 @@ class TC_grid_wrapper_model_class extends TC_article_model_class {
     return 'class="' . join( ' ', get_post_class( $_class ) ) . '"';
   }
 
+  /**
+  * @return  boolean
+  */
+  private function tc_grid_show_thumb() {
+    return TC_utils_thumbnails::$instance -> tc_has_thumb() && 0 != esc_attr( TC_utils::$inst->tc_opt( 'tc_post_list_show_thumb' ) );
+  }
+
+
+  /**
+  * parse this model properties for rendering
+  */ 
+  function pre_rendering_my_view_cb( $model ) {
+    parent::pre_rendering_my_view_cb( $model );  
+    foreach ( array('figure') as $property )
+      $model -> {"{$property}_class"} = $this -> tc_stringify_model_property( "{$property}_class" );
+  }
 }
---
+++ templates/modules/grid_thumbnail.php
@@ -0,0 +1,2 @@
+<div class="tc-grid-icon format-icon" <?php echo $grid_thumbnail_model -> icon_attributes ?>></div>
+<?php echo $grid_thumbnail_model -> thumb_img ?>
