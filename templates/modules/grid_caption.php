6d5ab0fb43cffb1025d3de813547a1980b150765
Rocco Aliberti <raliberti84@gmail.com>
2016-03-21 17:05:09 +0100

    Fix: partial rendering of the grid elements

--- templates/modules/grid_caption.php
+++ templates/modules/grid_caption.php
@@ -1,8 +1,8 @@
 <figcaption class="tc-grid-excerpt">
   <div class="entry-summary">
     <div class="tc-g-cont"><?php the_excerpt() ?></div>
-    <?php if ( isset( $grid_content_model -> has_heading_in_caption ) ) : ?>
-      <?php do_action( '__grid_heading__' ) ?>
+    <?php if ( isset( $grid_caption_model -> has_heading_in_caption ) ) : ?>
+      <h2 class="entry-title"><?php the_title() ?></h2>
     <?php endif ?>
   </div>
   <a class="tc-grid-bg-link" href="<?php the_permalink() ?>" title="<?php esc_attr( strip_tags( get_the_title() ) ) ?>"></a>
