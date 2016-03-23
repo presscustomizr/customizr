fd7e27aa9e163b30d765dc56511047267b748b76
Rocco Aliberti <raliberti84@gmail.com>
2016-03-23 15:59:46 +0100

    Fix: get rid of sidenav inner class property, it's static

--- templates/header/sidenav.php
+++ templates/header/sidenav.php
@@ -1,5 +1,5 @@
 <nav id="tc-sn" class="tc-sn navbar">
-  <div class="<?php tc_echo( 'inner_class') ?>">
+  <div class="tc-sn-inner nav-collapse">
     <?php do_action( '__sidenav__' ); /*hook of social, tagline, menu, ordered by priorities 10, 20, 20*/ ?>
   </div><!-- /.tc-sn-inner  -->
 </nav>
