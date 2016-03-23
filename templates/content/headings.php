5eac1d8752dc1e36923512fc3fd7ace5b38dee7a
Rocco Aliberti <raliberti84@gmail.com>
2016-03-23 10:53:34 +0100

    Imp: add contextual headings featurette divider

--- templates/content/headings.php
+++ templates/content/headings.php
@@ -2,4 +2,7 @@
 <?php if ( 'content' == tc_get( 'type' ) ) : ?>
   <?php do_action( '__post_page_title__' ) ?>
   <?php do_action( '__post_metas__' ) ?>
-<?php endif;
+<?php endif; ?>
+<?php if ( 'content' != tc_get( 'type' ) || is_singular() ) : ?>
+<hr class="featurette-divider headings">
+<?php endif ;
