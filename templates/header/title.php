7249298647e5a92a0c261b19eccff697cf1c9245
Rocco Aliberti <raliberti84@gmail.com>
2016-03-24 09:49:01 +0100

    Fix: fix site title wrapper

--- templates/header/title.php
+++ templates/header/title.php
@@ -1,5 +1,7 @@
-<h1 class="<?php tc_echo('element_class') ?>">
-  <a class="<?php tc_echo( 'link_class' ) ?>" href="<?php tc_echo( 'link_url' ) ?>" title="<?php tc_echo( 'link_title' ) ?>" >
-    <?php _e( esc_attr( get_bloginfo( 'name' ) ) ); ?>
-  </a>
-</h1>
+<div class="<?php tc_echo('element_class') ?>">
+  <h1>  
+    <a class="<?php tc_echo( 'link_class' ) ?>" href="<?php tc_echo( 'link_url' ) ?>" title="<?php tc_echo( 'link_title' ) ?>" >
+      <?php _e( esc_attr( get_bloginfo( 'name' ) ) ); ?>
+    </a>
+  </h1>
+</div>
