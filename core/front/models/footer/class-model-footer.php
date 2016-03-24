60495feb1919bd6e6574dc8836a7770f7f0208a1
Rocco Aliberti <raliberti84@gmail.com>
2016-03-24 14:59:25 +0100

    Fix: footer model class - remove useless code/add todo

--- core/front/models/footer/class-model-footer.php
+++ core/front/models/footer/class-model-footer.php
@@ -1,15 +1,4 @@
 <?php
 class TC_footer_model_class extends TC_Model {
-  public $element_id = 'footer';
-
-  /**
-  * @override
-  * fired before the model properties are parsed
-  * 
-  * return model params array() 
-  */
-  function tc_extend_params( $model = array() ) {
-    $model[ 'element_class' ] = apply_filters('tc_footer_classes', array(), $model );
-    return $model;
-  }
+  /* TODO: children registration */
 }//end of class
