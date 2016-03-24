b93498e4fa5d46ccd73571a752dfad22a1b2eeb8
Rocco Aliberti <raliberti84@gmail.com>
2016-03-24 16:26:52 +0100

    Fix: footer credits - fix typo in gettext usage

--- templates/footer/footer_credits.php
+++ templates/footer/footer_credits.php
@@ -2,6 +2,6 @@
   <p>
     &middot; <span class="tc-copyright-text">&copy; <?php echo esc_attr( date('Y') ) ?></span> <a href="<?php echo esc_url( home_url() ) ?>" title="<?php echo esc_attr( get_bloginfo() ) ?>"><?php echo esc_attr( get_bloginfo() ) ?></a>
     &middot; <span class="tc-credits-text">Designed by</span> <a href="<?php echo TC_WEBSITE ?>">Press Customizr</a>
-    &middot; <span class="tc-wp-powered-text"><?php _e('Powered by', 'customizr') ?></span> <a class="icon-wordpress" target="_blank" href="https://wordpress.org" title="<?php _('Powered by Wordpress', 'customizr') ?>"></a> &middot;
+    &middot; <span class="tc-wp-powered-text"><?php _e('Powered by', 'customizr') ?></span> <a class="icon-wordpress" target="_blank" href="https://wordpress.org" title="<?php _e('Powered by Wordpress', 'customizr') ?>"></a> &middot;
   </p>
 </div> 
