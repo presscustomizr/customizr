8a967c66cc73d6d55d41d410914dadeae4f52d0b
Rocco Aliberti <raliberti84@gmail.com>
2016-03-29 14:09:47 +0200

    Imp: add edit button for the single slide, use the same edit button template (conditional check inside) use an extended edit button model class to set-up data/visibility

--- core/front/models/modules/class-model-edit_button.php
+++ core/front/models/modules/class-model-edit_button.php
@@ -1,15 +1,12 @@
 <?php
 class TC_edit_button_model_class extends TC_Model {
+  public $context = 'post';
 
   /*
   * @override
   */
   function tc_maybe_render_this_model_view () {
     if ( ! $this -> visibility )
-      return;
-
-    //Must be in the loop
-    if ( ! in_the_loop() )
       return;
 
     if ( ! apply_filters( 'tc_edit_in_title', $this -> tc_is_edit_enabled() ) )
@@ -25,8 +22,11 @@ class TC_edit_button_model_class extends TC_Model {
   * @since Customizr 3.3+
   */
   public function tc_is_edit_enabled() {
+    if ( ! in_the_loop() )
+      return;
+
     //when are we displaying the edit link?
-    $edit_enabled = ( is_page() && current_user_can('edit_pages') ) ? true : false;
-    return ( 0 !== get_the_ID() && current_user_can('edit_post' , get_the_ID() ) && ! is_page() ) ? true : $edit_enabled;
+    $edit_enabled = ( is_page() && current_user_can( 'edit_pages' ) ) ? true : false;
+    return ( 0 !== get_the_ID() && current_user_can( 'edit_post' , get_the_ID() ) && ! is_page() ) ? true : $edit_enabled;
   }
 }
--- templates/modules/slide.php
+++ templates/modules/slide.php
@@ -1,3 +1,12 @@
+<?php
+/**
+ * The template for displaying a single slide item
+ *
+ * @package WordPress
+ * @subpackage Customizr
+ * @since Customizr 3.5.0
+ */
+?>
 <div class="item <?php tc_echo( 'element_class' ) ?>" <?php tc_echo('element_attributes') ?>>
 
   <div class="<?php tc_echo( 'img_wrapper_class' ) ?>">
@@ -5,18 +14,24 @@
     <a class="tc-slide-link" href="<?php tc_echo( 'link_url' ) ?>" target="<?php tc_echo( 'link_target' ) ?>" title=<?php _e( 'Go to', 'customizr' ) ?>>
   <?php endif ?>
     <?php
-        do_action('__before_all_slides');
+        do_action('__before_all_slides_background__');
  //     do_action_ref_array ("__before_slide_{$id}" , array( $data['slide_background'], $data['link_url'], $id, $slider_name_id, $data ) );
           tc_echo( 'slide_background' );
 /*do_action_ref_array ("__after_slide_{$id}" , array( $data['slide_background'], $data['link_url'], $id, $slider_name_id, $data ) );*/
-        do_action('__after_all_slides');
+        do_action('__after_all_slides_background__');
     ?>
   <?php if ( tc_get( 'link_whole_slide' ) ) : ?>
     </a>
   <?php endif; ?>
   </div> <!-- .carousel-image -->
 
-  <?php if ( tc_get( 'has_caption' ) ) : ?>
+  <?php 
+    
+  if ( tc_get( 'has_caption' ) ) : 
+
+  do_action('__before_all_slides_caption__');
+
+  ?>
   <div class="<?php tc_echo( 'caption_class' ) ?>">
     <?php if ( tc_get( 'title' ) ): ?>
     <!-- TITLE -->
@@ -31,5 +46,11 @@
       <a class="<?php tc_echo( 'button_class' ) ?>" href="<?php tc_echo( 'button_link' ) ?>" target="<?php tc_echo( 'link_target' ) ?>"><?php tc_echo( 'button_text' ) ?></a>
     <?php endif; ?>
   </div>
-  <?php endif; ?>
+  <?php 
+
+  do_action('__after_all_slides_caption__');
+  /* endif caption*/
+  endif; 
+
+  ?>
 </div><! -- /.item -->
