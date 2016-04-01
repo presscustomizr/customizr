6646b6767217fa9baff381179bb53183f1052ac1
nikeo <nicolas@presscustomizr.com>
2016-04-01 14:54:11 +0200

    removed the __root__ template

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php
-do_action('__rooot__');
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?> <?php tc_echo('element_attributes') ?>>
+<!--<![endif]-->
+  <?php do_action( '__html__' ); ?>
+</html>
