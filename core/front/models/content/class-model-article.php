d63630687f1872067a5da0297e84ed2f9a89087f
Rocco Aliberti <raliberti84@gmail.com>
2016-04-07 16:21:41 +0200

    Fix: singular article selectors retrieving

--- core/front/models/content/class-model-article.php
+++ core/front/models/content/class-model-article.php
@@ -28,21 +28,21 @@ class TC_article_model_class extends TC_Model {
   */
   function tc_get_the_article_selectors() {
     if ( 'singular_article' == $this -> id ) {
-      $this -> tc_get_singular_article_selectors();
+     $selectors = $this -> tc_get_singular_article_selectors();
+    }else {
+      //gets global vars
+      global $post;
+      global $wp_query;
+
+      //declares selector var
+      $selectors                  = '';
+
+      $post_class                 = CZR() -> helpers -> tc_get_the_post_class( $this -> post_class );
+
+      // POST LIST
+      $post_list_selector_bool    = ( isset($post) && !is_singular() && !is_404() && !tc__f( '__is_home_empty') ) || ( is_search() && 0 != $wp_query -> post_count );
+      $selectors                  = $post_list_selector_bool ? apply_filters( 'tc_post_list_selectors' , 'id="post-'.get_the_ID().'" '. $post_class ) : $selectors;
     }
-
-    //gets global vars
-    global $post;
-    global $wp_query;
-
-    //declares selector var
-    $selectors                  = '';
-
-    $post_class                 = CZR() -> helpers -> tc_get_the_post_class( $this -> post_class );
-
-    // POST LIST
-    $post_list_selector_bool    = ( isset($post) && !is_singular() && !is_404() && !tc__f( '__is_home_empty') ) || ( is_search() && 0 != $wp_query -> post_count );
-    $selectors                  = $post_list_selector_bool ? apply_filters( 'tc_post_list_selectors' , 'id="post-'.get_the_ID().'" '. $post_class ) : $selectors;
 
     $selectors = apply_filters( 'tc_article_selectors', $selectors );
 
