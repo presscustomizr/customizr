172fd02bd8b40b4f9f5d056f2e00159df9cbcdd9
Rocco Aliberti <raliberti84@gmail.com>
2016-04-07 17:25:58 +0200

    Fix: fix attachment post metas moved from content/singles to content/post-metas as they can be displayed also in post lists, if attachments allowed in search results

--- templates/content/singles/post_page_headings.php
+++ templates/content/singles/post_page_headings.php
@@ -13,9 +13,9 @@
   <?php
     if ( tc_has('post_page_title') ) { tc_render_template('content/singles/post_page_title'); }
 
-    if ( tc_has('post_metas_button') ) { tc_render_template('post_metas_button'); }
-    elseif ( tc_has('post_metas_text') ) { tc_render_template('post_metas_text'); }
-    elseif ( tc_has('post_metas_attachment') ) { tc_render_template('post_metas_attachment'); }
+    if ( tc_has('post_metas_button') ) { tc_render_template( 'content/post-metas/post_metas', 'post_metas_button'); }
+    elseif ( tc_has('post_metas_text') ) { tc_render_template('content/post-metas/post_metas_text', 'post_metas_text'); }
+    elseif ( tc_has('post_metas_attachment') ) { tc_render_template('content/post-metas/attachment_post_metas', 'post_metas_attachment'); }
     //do_action( '__headings_content__' );
   ?>
   <?php if ( is_singular() ) : ?>
