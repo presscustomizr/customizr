ed9fb88c0e75c835e630f32293b718cf6482281c
Rocco Aliberti <raliberti84@gmail.com>
2016-04-12 09:36:58 +0200

    Imp: btt-arrow rendered in index

--- core/front/models/class-model-footer.php
+++ core/front/models/class-model-footer.php
@@ -5,8 +5,6 @@ class TC_footer_model_class extends TC_Model {
     $children = array(
       //sticky footer
       array( 'hook' => 'after_render_view_main_container', 'template' => 'footer/footer_push', 'priority' => 100 ),
-      //btt arrow
-      array( 'hook' => '__after_page_wrapper', 'template' => 'footer/btt_arrow')
     );
 
     return $children;
