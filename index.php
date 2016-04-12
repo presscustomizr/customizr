8740c0b804f6521eb3ef5b23b6c1ea2cc4c55deb
Rocco Aliberti <raliberti84@gmail.com>
2016-04-12 09:36:58 +0200

    Imp: btt-arrow rendered in index

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
     </div>
 
     <?php do_action('__after_page_wrapper'); ?>
-
+    <?php if ( tc_has('btt_arrow') ){ tc_render_template('footer/btt_arrow'); }; ?>
     <?php wp_footer() ?>
   </body>
 </html>
