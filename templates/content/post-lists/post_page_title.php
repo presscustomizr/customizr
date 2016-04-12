f60b8f3af27aa94cbb776235c3a537b281d53521
Rocco Aliberti <raliberti84@gmail.com>
2016-04-12 10:59:19 +0200

    Imp: commentbubble, editlink, recently update rendered with tc_has/tc_render_template

--- templates/content/post-lists/post_page_title.php
+++ templates/content/post-lists/post_page_title.php
@@ -8,7 +8,8 @@
  */
 ?>
 <h2 class="<?php tc_echo( 'element_class') ?>" <?php tc_echo('element_attributes') ?>>
-  <?php do_action( '__before_inner_post_page_title__' ) ?>
   <a href="<?php the_permalink() ?>" title="<?php _e( 'Permalink to' , 'customizr' ) ?> <?php esc_attr( strip_tags( get_the_title() ) ) ?>" rel="bookmark"><?php the_title() ?></a>
-  <?php do_action( '__after_inner_post_page_title__' ) ?>
+  <?php if ( tc_has( 'comment_bubble' ) ) tc_render_template( 'modules/comment_bubble', 'comment_bubble' ) ?>
+  <?php if ( tc_has( 'edit_button' ) ) tc_render_template( 'modules/edit_button', 'edit_button' ) ?>
+  <?php if ( tc_has( 'recently_updated' ) ) tc_render_template( 'modules/recently_updated', 'recently_updated' ) ?>
 </h2>
