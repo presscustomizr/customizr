09dc42a510be5eb2d913a4d68310e3d6a7257e46
Rocco Aliberti <raliberti84@gmail.com>
2016-04-13 16:51:24 +0200

    Fix: fix post pagination style (wp_link_pages before and after params)

--- templates/content/singles/post_content.php
+++ templates/content/singles/post_content.php
@@ -14,11 +14,8 @@
   <?php
   the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>' , 'customizr' ) );
   wp_link_pages( array(
-    'before'        => '<div class="btn-toolbar page-links"><div class="btn-group">' . __( 'Pages:' , 'customizr' ),
-    'after'         => '</div></div>',
-    'link_before'   => '<button class="btn btn-small">',
-    'link_after'    => '</button>',
-    'separator'     => '',
+    'before'        => '<div class="pagination pagination-centered">' . __( 'Pages:' , 'customizr' ),
+    'after'         => '</div>',
     )
   );
   ?>
