934cdbf519530df9629469914285be01e80bb2fb
Rocco Aliberti <raliberti84@gmail.com>
2016-04-14 11:37:34 +0200

    Fix: menu button label inside the sidenav must show the "Close" text. The issue was due to the fact that we use just one model for the sidenav toggle button both in the navbar and the sidenav, and the label text was conditioned to the model hook. Since the tc_has/tc_render_template introduction we don't use to display it with the hook, hence we now rely on the model id.

--- core/front/models/header/class-model-sidenav_menu_button.php
+++ core/front/models/header/class-model-sidenav_menu_button.php
@@ -12,7 +12,7 @@ class TC_sidenav_menu_button_model_class extends TC_menu_button_model_class {
 
     array_push( $model[ 'element_class' ], 'sn-toggle' );
     //@todo fix this condition
-    if ( '__sidenav__' == $model['hook'] ) {
+    if ( 'sidenav_menu_button' == $model['id'] ) {
       $_close_message             = __('Close', 'customizr' );
       //button label
       $model[ 'button_label' ]    = $model[ 'button_label' ] ? sprintf( '<span class="menu-label">%s</span>', $_close_message ) : '';
