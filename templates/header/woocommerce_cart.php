b400113d3ebfc84e61a32a1ffa2d75c6a1bfb190
Rocco Aliberti <raliberti84@gmail.com>
2016-04-15 12:10:16 +0200

    Imp: Introduce woocomerce cart model/template the registration is done in the woocommerce plugins compat method uses the new tc_has added param "only_registered"

---
+++ templates/header/woocommerce_cart.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying the Woocommerce Cart in the header
+ */
+?>
+<div class="tc-wc-menu tc-open-on-hover span1">
+ <ul class="tc-wc-header-cart nav tc-hover-menu">
+   <li class="<?php tc_echo( 'current_menu_item_class' ) ?> menu-item">
+     <a class="cart-contents" href="<?php echo esc_url( WC()->cart->get_cart_url() ); ?>" title="<?php _e( 'View your shopping cart', 'customizr' ); ?>">
+       <span class="count btn-link tc-wc-count"><?php echo WC()->cart->get_cart_contents_count() ? WC()->cart->get_cart_contents_count() : '' ?></span>
+     </a>
+    <?php
+    ?>
+    <?php if ( ! tc_get( 'is_checkout_cart' ) ) : //do not display the dropdown in the cart or checkout page ?>
+      <ul class="dropdown-menu">
+       <li>
+         <?php the_widget( 'WC_Widget_Cart', 'title=' ); ?>
+        </li>
+      </ul>
+    <?php endif; ?>
+   </li>
+  </ul>
+</div>
+
