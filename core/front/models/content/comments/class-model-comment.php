4d2ed2f001b09076fa5a736b4a771a0e6448f3cb
Rocco Aliberti <raliberti84@gmail.com>
2016-04-19 14:17:28 +0200

    Fix: comment model, do not sanitize now removed props

--- core/front/models/content/comments/class-model-comment.php
+++ core/front/models/content/comments/class-model-comment.php
@@ -28,17 +28,6 @@ class TC_comment_model_class extends TC_Model {
     return $model;
   }
 
-
-  /**
-  * @override
-  * parse this model properties for rendering
-  */
-  function tc_sanitize_model_properties( $model ) {
-    parent::tc_sanitize_model_properties( $model );
-    foreach ( array('wrapper', 'content', 'avatar', 'reply_btn' ) as $property )
-      $model -> {"comment_{$property}_class"} = $this -> tc_stringify_model_property( "comment_{$property}_class" );
-  }
-
   /**
    * Prepare template for comments
    *
