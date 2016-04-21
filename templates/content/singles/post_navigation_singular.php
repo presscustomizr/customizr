d7f5b2bd2a68c19037eaaf8c0dcaa95e6c5f3510
Rocco Aliberti <raliberti84@gmail.com>
2016-04-21 15:38:53 +0200

    Imp: navigation - display the whole nav-below only if one of the link is available

--- templates/content/singles/post_navigation_singular.php
+++ templates/content/singles/post_navigation_singular.php
@@ -10,6 +10,27 @@ $l_arr        = __( '&larr;', 'customizr' );
 $r_arr        = __( '&rarr;', 'customizr' );
 $prev_arr     = is_rtl() ? $r_arr : $l_arr;
 $next_arr     = is_rtl() ? $l_arr : $r_arr;
+
+/* Generate links */
+$prev_link = get_previous_post_link(
+        '%link', //format
+        '<span class="meta-nav">' . $prev_arr . '</span> %title', //title
+        false, //in_same_term
+        '', //excluded_terms
+        'category'//taxonomy
+    );
+
+$next_link  = get_next_post_link(
+        '%link', //format
+        '%title <span class="meta-nav">' . $next_arr . '</span>', //title
+        false, //in_same_term
+        '', //excluded_terms
+        'category'//taxonomy
+    );
+
+/* If no links are present do not display this */
+if ( null != $prev_link || null != $next_link ) :
+
 ?>
 <nav id="nav-below" class="<?php tc_echo( 'element_class' ) ?>" <?php tc_echo('element_attributes') ?>>
   <hr class="featurette-divider">
@@ -17,26 +38,14 @@ $next_arr     = is_rtl() ? $l_arr : $r_arr;
     <?php  _e( 'Post navigation' , 'customizr' ) ?>
   </h3>
   <ul class="pager">
-    <?php if ( null != $prev_link = get_previous_post_link(
-        '%link', //format
-        '<span class="meta-nav">' . $prev_arr . '</span> %title', //title
-        false, //in_same_term
-        '', //excluded_terms
-        'category'//taxonomy
-        ) ) : ?>
+    <?php if ( null != $prev_link ) : ?>
     <li class="previous">
       <span class="nav-previous">
         <?php echo $prev_link ?>
       </span>
     </li>
   <?php endif; ?>
-  <?php if ( null != $next_link = get_next_post_link(
-        '%link', //format
-        '%title <span class="meta-nav">' . $next_arr . '</span>', //title
-        false, //in_same_term
-        '', //excluded_terms
-        'category'//taxonomy
-        ) ) : ?>
+  <?php if ( null != $next_link ) : ?>
   <li class="next">
     <span class="nav-next">
     <?php echo $next_link ?>
@@ -45,3 +54,4 @@ $next_arr     = is_rtl() ? $l_arr : $r_arr;
   <?php endif ?>
 </ul>
 </nav>
+<?php endif;
