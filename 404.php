f01505412529b0673de2f55e37188b0869f030b4
Rocco Aliberti <raliberti84@gmail.com>
2016-04-22 16:32:08 +0200

    Imp: introduce 404 template

---
+++ 404.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ *
+ * The template for displaying 404 pages (not found)
+ *
+ * @package Customizr
+ * @since Twenty Customizr 3.5
+ */
+?>
+<?php get_header() ?>
+  <?php do_action('__before_main_wrapper'); ?>
+
+    <div id="main-wrapper" class="container" <?php tc_echo('element_attributes', 'main_content') ?>>
+
+      <?php if ( tc_has('breadcrumb') ) { tc_render_template('modules/breadcrumb'); } ?>
+
+      <?php do_action('__before_main_container'); ?>
+      <div class="container" role="main">
+        <div class="<?php tc_echo ('column_content_class', 'main_content' ) ?>">
+          <?php
+            if ( tc_has('left_sidebar') ) { tc_render_template('content/sidebars/left_sidebar', 'left_sidebar'); }
+          ?>
+
+              <?php do_action('__before_content'); ?>
+
+              <div id="content" class="<?php tc_echo( 'article_wrapper_class', 'main_content' ) ?>">
+                <?php tc_render_template('content/singles/404.php'); ?>
+              </div>
+
+              <?php do_action('__after_content'); ?>
+
+            <?php
+            if ( tc_has('right_sidebar') ) { tc_render_template('content/sidebars/right_sidebar', 'right_sidebar'); }
+          ?>
+        </div>
+      </div><!-- .container -->
+
+      <?php do_action('__after_main_container'); ?>
+      <?php if ( tc_has('footer_push') ) { tc_render_template('footer/footer_push', 'footer_push'); } ?>
+
+    </div><!-- #main-wrapper -->
+
+    <?php do_action('__after_main_wrapper'); ?>
+
+<?php get_footer() ?>
