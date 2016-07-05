7bf4317d6f0c802ca3dc13a9790cb1cdcadaa943
nikeo <nicolas@presscustomizr.com>
2016-07-05 18:11:01 +0200

    class-view cleaning

--- core/framework/class-view.php
+++ core/framework/class-view.php
@@ -13,24 +13,18 @@ if ( ! class_exists( 'CZR_cl_View' ) ) :
   class CZR_cl_View {
     public $model;
     function __construct( $model ) {
-      // $keys = array_keys( get_object_vars( $this ) );
+          // $keys = array_keys( get_object_vars( $this ) );
 
-      // foreach ( $keys as $key ) {
-      //   if ( isset( $model[ $key ] ) ) {
-      //     $this->$key = $model[ $key ];
-      //   }
-      // }
-      $this -> model = $model;
+          // foreach ( $keys as $key ) {
+          //   if ( isset( $model[ $key ] ) ) {
+          //     $this->$key = $model[ $key ];
+          //   }
+          // }
+          $this -> model = $model;
 
-      //emit event on view instantiation
-      //Will be listen to by the model and trigger the maybe_hook_view callback
-      do_action( "view_instantiated_{$this -> model -> id}", $this );
-
-      //listens to a view pre-render => and fire the czr_fn_apply_registered_changes_to_instance
-      // => a change might have been registered
-      //view id
-      //view params
-      add_action( 'pre_render_view'             , array( $this, 'czr_fn_apply_registered_changes_to_instance' ), 10, 1 );
+          //emit event on view instantiation
+          //Will be listen to by the model and trigger the maybe_hook_view callback
+          do_action( "view_instantiated_{$this -> model -> id}", $this );
     }
 
 
@@ -42,110 +36,91 @@ if ( ! class_exists( 'CZR_cl_View' ) ) :
     //hook : $model['hook']
     //NOTE : the $this here can be the child class $this.
     public function czr_fn_maybe_render() {
-      //this event is used to check for late deletion or change before actually rendering
-      //will fire czr_fn_apply_registered_changes_to_instance
-      //do_action( 'pre_render_view', $this -> id );
+          //this event is used to check for late deletion or change before actually rendering
+          //will fire czr_fn_apply_registered_changes_to_instance
+          //do_action( 'pre_render_view', $this -> id );
 
-      if ( ! apply_filters( "czr_do_render_view_{$this -> model -> id}", true ) )
-        return;
+          if ( ! apply_filters( "czr_do_render_view_{$this -> model -> id}", true ) )
+            return;
 
-      //allow filtering of the model before rendering (the view's model is passed by reference)
-      do_action_ref_array( 'pre_rendering_view', array(&$this -> model) );
-      do_action_ref_array( "pre_rendering_view_{$this -> model -> id}", array(&$this -> model) );
+          //allow filtering of the model before rendering (the view's model is passed by reference)
+          do_action_ref_array( 'pre_rendering_view', array(&$this -> model) );
+          do_action_ref_array( "pre_rendering_view_{$this -> model -> id}", array(&$this -> model) );
 
-      do_action( "__before_{$this -> model -> id}" );
+          do_action( "__before_{$this -> model -> id}" );
 
-      $czr_fn_print_debug =  ! CZR() -> czr_fn_is_customizing() && is_user_logged_in() && current_user_can( 'edit_theme_options' );
+          $czr_fn_print_debug =  ! CZR() -> czr_fn_is_customizing() && is_user_logged_in() && current_user_can( 'edit_theme_options' );
 
-      ?>
-      <?php
-      if ( $czr_fn_print_debug ) {
-        echo "<!-- HOOK CONTENT HERE : __before_{$this -> model -> id} -->";
+          ?>
+          <?php
+          if ( $czr_fn_print_debug ) {
+            echo "<!-- HOOK CONTENT HERE : __before_{$this -> model -> id} -->";
 
-        /* Maybe merge debug info into the model element attributes */
-        $this -> model -> element_attributes =  join( ' ', array_filter( array(
-            $this -> model -> element_attributes,
-            'data-model_id="'. $this -> model -> id .'"',
-            isset( $this -> model -> template ) ? 'data-template="'. $this -> model -> template .'"' : ''
-        )) );
-        echo "<!-- START RENDERING VIEW ID : {$this -> model -> id} -->";
-      }
+            /* Maybe merge debug info into the model element attributes */
+            $this -> model -> element_attributes =  join( ' ', array_filter( array(
+                $this -> model -> element_attributes,
+                'data-model_id="'. $this -> model -> id .'"',
+                isset( $this -> model -> template ) ? 'data-template="'. $this -> model -> template .'"' : ''
+            )) );
+            echo "<!-- START RENDERING VIEW ID : {$this -> model -> id} -->";
+          }
 
-        $this -> czr_fn_render();
+            $this -> czr_fn_render();
 
-      if ( $czr_fn_print_debug ) {
-        echo "<!-- END OF RENDERING VIEW ID : {$this -> model -> id} -->";
-        echo "<!-- HOOK CONTENT HERE : __after_{$this -> model -> id} -->";
-      }
-      do_action( "__after_{$this -> model -> id}" );
+          if ( $czr_fn_print_debug ) {
+            echo "<!-- END OF RENDERING VIEW ID : {$this -> model -> id} -->";
+            echo "<!-- HOOK CONTENT HERE : __after_{$this -> model -> id} -->";
+          }
+          do_action( "__after_{$this -> model -> id}" );
     }
 
 
 
     //might be overriden in the child view if any
     public function czr_fn_render() {
-      if ( ! empty( $this -> model -> html ) )
-        echo $this -> model -> html;
+          if ( ! empty( $this -> model -> html ) )
+            echo $this -> model -> html;
 
-      if ( ! empty( $this -> model -> template ) ) {
-        //get the filename
-        $_template_file = czr_fn_get_theme_file("templates/{$this -> model -> template}.php" );
+          if ( ! empty( $this -> model -> template ) ) {
+            //get the filename
+            $_template_file = czr_fn_get_theme_file("templates/{$this -> model -> template}.php" );
 
-        if ( false !== $_template_file ) {
-          czr_fn_set_current_model( $this -> model );
-          ob_start();
-            load_template( $_template_file, $require_once = false );
-          $_temp_content = ob_get_contents();
+            if ( false !== $_template_file ) {
+              czr_fn_set_current_model( $this -> model );
+              ob_start();
+                load_template( $_template_file, $require_once = false );
+              $_temp_content = ob_get_contents();
 
-          ob_end_clean();
-          if ( ! empty($_temp_content) )
-            echo $_temp_content;
+              ob_end_clean();
+              if ( ! empty($_temp_content) )
+                echo $_temp_content;
 
-          czr_fn_reset_current_model();
-        }
-      }
+              czr_fn_reset_current_model();
+            }
+          }
 
-      if ( ! empty( $this -> model -> callback ) )
-        CZR() -> helpers -> czr_fn_fire_cb( $this -> model -> callback, $this -> model -> cb_params );
+          if ( ! empty( $this -> model -> callback ) )
+            CZR() -> helpers -> czr_fn_fire_cb( $this -> model -> callback, $this -> model -> cb_params );
     }
 
-
-    //updates the view properties with the requested args
-    //stores the clean and updated args in a view property.
-    //@return void()
-    //called directly sometimes
-    //fired on 'pre_render_view'
-    //fired on czr_fn_change if view is instantiated
-    public function czr_fn_apply_registered_changes_to_instance( $id, $new_params = array() ) {
-      if ( ! CZR() -> collection -> czr_fn_has_registered_change( $id ) )
-        return;
-
-      $new_params = empty($new_params) ? CZR() -> collection -> czr_fn_get_registered_changes( $id ) : $new_params;
-
-      $this -> czr_fn_update_model_instance( $id, $new_params );
-
-      //This event will trigger a removal of the change from the change list
-      //=> czr_deregister_change
-      do_action('registered_changed_applied' , $id);
-    }
 
 
     //at this stage, the view is instantiated
     //@return void()
     private function czr_fn_update_model_instance( $id, $new_params ) {
-      //get current params
-      $current_params = $this -> czr_fn_get_view($id);
-      if ( ! $current_params || ! is_array($current_params) )
-        return;
-      //pre-process new params
-      $new_params = wp_parse_args( $new_params, $current_params );
+          //get current params
+          $current_params = $this -> czr_fn_get_view($id);
+          if ( ! $current_params || ! is_array($current_params) )
+            return;
+          //pre-process new params
+          $new_params = wp_parse_args( $new_params, $current_params );
 
-      //update the modified view properties
-      //=> will automatically trigger the collection update
-      foreach ($new_params as $property => $value) {
-        if ( $value != $current_params[$property] )
-          $current_params['view_instance'] -> czr_fn_set_property( $property, $value );
-      }
+          //update the modified view properties
+          //=> will automatically trigger the collection update
+          foreach ($new_params as $property => $value) {
+            if ( $value != $current_params[$property] )
+              $current_params['view_instance'] -> czr_fn_set_property( $property, $value );
+          }
     }
 
 
@@ -153,7 +128,7 @@ if ( ! class_exists( 'CZR_cl_View' ) ) :
     * PUBLIC HELPERS
     ***********************************************************************************/
     public function czr_fn_get_instance() {
-      return $this;
+          return $this;
     }
 
   }//class
