bbdef578d99e32530b096e9eee243d848dcbd5ea
nikeo <nicolas@presscustomizr.com>
2016-07-06 11:08:53 +0200

    code cleaning

--- core/framework/class-controllers.php
+++ core/framework/class-controllers.php
@@ -12,29 +12,29 @@ if ( ! class_exists( 'CZR_cl_controllers' ) ) :
     static $controllers_instances = array();
 
     function __construct( $args = array() ) {
-      self::$instance =& $this;
-      $this -> controllers = array(
-        'header' => array(
-          'head', 'title', 'logo_wrapper', 'logo', 'sticky_logo', /*'logo_title', */'tagline', 'mobile_tagline', 'favicon', 'menu', 'sidenav', 'navbar_menu', 'navbar_secondary_menu', 'menu_button', 'mobile_menu_button', 'sidenav_menu_button', 'sidenav_navbar_menu_button',
-        ),
-        'content' => array(
-          '404', 'attachment', 'headings', 'no_results', 'page', 'post', 'single_author_info', 'post_list', 'post_metas_button', 'post_metas_text', 'post_metas_attachment','right_sidebar', 'left_sidebar', 'posts_list_headings', 'posts_list_description', 'author_description', 'posts_list_title', 'posts_list_search_title', 'singular_article', 'post_list_title', 'post_navigation_singular', 'post_navigation_posts', 'comments', 'comment_list', 'comment', 'tracepingback', 'author_info', 'singular_headings', 'post_list_standard_thumb', 'post_list_rectangular_thumb', 'post_thumbnail'
-        ),
-        'footer' => array(
-          'btt_arrow', 'footer_btt', 'footer_push', 'footer_widgets_wrapper'
-      //    'widgets', 'colophon', 'back_to_top'
-        ),
-        'modules' => array(
-          'social_block', 'breadcrumb', 'comment_bubble', 'post_list_grid', 'featured_pages', 'main_slider', 'recently_updated', 'edit_button', 'help_block'
-        //   'breadcrumb', 'comment_bubbles', 'featured_pages', 'gallery', 'post_list_grid', 'post_thumbnails', 'slider'
-        ),
-      );
+          self::$instance =& $this;
+          $this -> controllers = array(
+            'header' => array(
+              'head', 'title', 'logo_wrapper', 'logo', 'sticky_logo', /*'logo_title', */'tagline', 'mobile_tagline', 'favicon', 'menu', 'sidenav', 'navbar_menu', 'navbar_secondary_menu', 'menu_button', 'mobile_menu_button', 'sidenav_menu_button', 'sidenav_navbar_menu_button',
+            ),
+            'content' => array(
+              '404', 'attachment', 'headings', 'no_results', 'page', 'post', 'single_author_info', 'post_list', 'post_metas_button', 'post_metas_text', 'post_metas_attachment','right_sidebar', 'left_sidebar', 'posts_list_headings', 'posts_list_description', 'author_description', 'posts_list_title', 'posts_list_search_title', 'singular_article', 'post_list_title', 'post_navigation_singular', 'post_navigation_posts', 'comments', 'comment_list', 'comment', 'tracepingback', 'author_info', 'singular_headings', 'post_list_standard_thumb', 'post_list_rectangular_thumb', 'post_thumbnail'
+            ),
+            'footer' => array(
+              'btt_arrow', 'footer_btt', 'footer_push', 'footer_widgets_wrapper'
+          //    'widgets', 'colophon', 'back_to_top'
+            ),
+            'modules' => array(
+              'social_block', 'breadcrumb', 'comment_bubble', 'post_list_grid', 'featured_pages', 'main_slider', 'recently_updated', 'edit_button', 'help_block'
+            //   'breadcrumb', 'comment_bubbles', 'featured_pages', 'gallery', 'post_list_grid', 'post_thumbnails', 'slider'
+            ),
+          );
 
-      //Store a group controller instance for later uses
-      //takes 2 params
-      //group name (string)
-      //group controller instance (object)
-      add_action( 'group_controller_instantiated', array( $this, 'czr_fn_store_controller_instance'),10, 2);
+          //Store a group controller instance for later uses
+          //takes 2 params
+          //group name (string)
+          //group controller instance (object)
+          add_action( 'group_controller_instantiated', array( $this, 'czr_fn_store_controller_instance'),10, 2);
     }//__construct()
 
 
@@ -55,23 +55,23 @@ if ( ! class_exists( 'CZR_cl_controllers' ) ) :
     //  2) a model array (not instanciated),
     //  3) a string id of the model
     public function czr_fn_is_possible( $model ) {
-      //if the specified param is an id, then get the model from the collection
-      if ( is_string($model) )
-        $model = array('id' => $model );
+          //if the specified param is an id, then get the model from the collection
+          if ( is_string($model) )
+            $model = array('id' => $model );
 
-      //abort with true if still no model at this stage.
-      if ( empty($model) )
-        return true;
+          //abort with true if still no model at this stage.
+          if ( empty($model) )
+            return true;
 
-      //the returned value can be a string or an array( instance, method)
-      $controller_cb = $this -> czr_fn_get_controller( $model );
-      //FOR TEST ONLY
-      //return true;
+          //the returned value can be a string or an array( instance, method)
+          $controller_cb = $this -> czr_fn_get_controller( $model );
+          //FOR TEST ONLY
+          //return true;
 
-      if ( ! empty( $controller_cb ) ) {
-        return apply_filters( 'czr_fn_set_control' , (bool) CZR() -> helpers -> czr_fn_return_cb_result( $controller_cb, $model ) );
-      }
-      return true;
+          if ( ! empty( $controller_cb ) ) {
+            return apply_filters( 'czr_fn_set_control' , (bool) CZR() -> helpers -> czr_fn_return_cb_result( $controller_cb, $model ) );
+          }
+          return true;
     }
 
 
@@ -79,7 +79,7 @@ if ( ! class_exists( 'CZR_cl_controllers' ) ) :
     //@return bool
     //@param array() or object() model
     public function czr_fn_has_controller( $model = array() ) {
-      return ! empty( $this -> czr_fn_build_controller( $model ) );
+          return ! empty( $this -> czr_fn_build_controller( $model ) );
     }
 
 
@@ -87,36 +87,36 @@ if ( ! class_exists( 'CZR_cl_controllers' ) ) :
     //@return a function string or an array( instance, method )
     //@param array() or object() model
     private function czr_fn_get_controller( $model = array() ) {
-      $model = is_object($model) ? (array)$model : $model;
-      $controller_cb = "";
+          $model = is_object($model) ? (array)$model : $model;
+          $controller_cb = "";
 
-      //IS A CONTROLLER SPECIFIED AS PROPERTY OF THIS MODEL ?
-      //and is a valid callback
-      if ( isset($model['controller']) && is_callable( $model['controller'] )) {
-        //a callback can be function or a method of a class
-        $controller_cb =  $model['controller'];
-      }
-      //IS THERE A PRE-DEFINED CONTROLLER FOR THE VIEW ?
-      else {
-        //the default controller match can either be:
-        //a) based on the model id (has the precedence )
-        //b) based on the controller model field, when not a callback
-        $controller_ids   = array_filter( array(
-            $model['id'],
-            ! empty( $model['controller'] ) ? $model['controller'] : '',
-          )
-        );
-        if ( $this -> czr_fn_has_default_controller( $controller_ids ) ) {
-          $controller_cb = $this -> czr_fn_get_default_controller( $controller_ids );
-          //make sure the default controller is well formed
-          //the default controller should look like array( instance, method )
-          if ( empty($controller_cb) ) {
-            do_action( 'czr_dev_notice', 'View : '.$model['id'].'. The default group controller has not been instantiated');
-            return "";
-          }//if
-        }//if has default controller
-      }//else
-      return $controller_cb;
+          //IS A CONTROLLER SPECIFIED AS PROPERTY OF THIS MODEL ?
+          //and is a valid callback
+          if ( isset($model['controller']) && is_callable( $model['controller'] )) {
+            //a callback can be function or a method of a class
+            $controller_cb =  $model['controller'];
+          }
+          //IS THERE A PRE-DEFINED CONTROLLER FOR THE VIEW ?
+          else {
+            //the default controller match can either be:
+            //a) based on the model id (has the precedence )
+            //b) based on the controller model field, when not a callback
+            $controller_ids   = array_filter( array(
+                $model['id'],
+                ! empty( $model['controller'] ) ? $model['controller'] : '',
+              )
+            );
+            if ( $this -> czr_fn_has_default_controller( $controller_ids ) ) {
+              $controller_cb = $this -> czr_fn_get_default_controller( $controller_ids );
+              //make sure the default controller is well formed
+              //the default controller should look like array( instance, method )
+              if ( empty($controller_cb) ) {
+                do_action( 'czr_dev_notice', 'View : '.$model['id'].'. The default group controller has not been instantiated');
+                return "";
+              }//if
+            }//if has default controller
+          }//else
+          return $controller_cb;
     }
 
 
@@ -124,60 +124,60 @@ if ( ! class_exists( 'CZR_cl_controllers' ) ) :
     //@return boolean
     //@walks the controllers setup array until a match is found
     private function czr_fn_has_default_controller( $controller_ids ) {
-      foreach ( $this -> controllers as $group => $views_id )
-        foreach( $controller_ids as $id )
-          if ( in_array($id, $views_id) )
-            return true;
-        //foreach
-      //foreach
-      return false;
+          foreach ( $this -> controllers as $group => $views_id )
+            foreach( $controller_ids as $id )
+              if ( in_array($id, $views_id) )
+                return true;
+            //foreach
+          //foreach
+          return false;
     }
 
 
     //tries to find a default controller group for this method
     //@return array(instance, method) or array() if nothind found
     private function czr_fn_get_default_controller( $controller_ids ) {
-      $controller_cb = false;
-      foreach ( $this -> controllers as $group => $views_id )
-        foreach( $controller_ids as $id )
-          if ( in_array($id, $views_id) ) {
-            $controller_cb = $id;
-            $controller_group = $group;
-            break 2;
-          }//if
-        //foreach
-      //foreach
-      //return here is no match found
-      if ( ! $controller_cb ) {
-        do_action( 'czr_dev_notice', 'View : '.$id.'. No control method found.');
-        return array();
-      }
+          $controller_cb = false;
+          foreach ( $this -> controllers as $group => $views_id )
+            foreach( $controller_ids as $id )
+              if ( in_array($id, $views_id) ) {
+                $controller_cb = $id;
+                $controller_group = $group;
+                break 2;
+              }//if
+            //foreach
+          //foreach
+          //return here is no match found
+          if ( ! $controller_cb ) {
+            do_action( 'czr_dev_notice', 'View : '.$id.'. No control method found.');
+            return array();
+          }
 
 
-      //Is this group controller instantiated ?
-      if ( ! array_key_exists($controller_group, self::$controllers_instances) ) {
-        //this will actually load the class file and instantiate it
-        $_instance = $this -> czr_fn_instantiate_group_controller($controller_group);
-      } else {
-        $_instance = $this -> czr_fn_get_controller_instance($controller_group);
-      }
+          //Is this group controller instantiated ?
+          if ( ! array_key_exists($controller_group, self::$controllers_instances) ) {
+            //this will actually load the class file and instantiate it
+            $_instance = $this -> czr_fn_instantiate_group_controller($controller_group);
+          } else {
+            $_instance = $this -> czr_fn_get_controller_instance($controller_group);
+          }
 
-      //stop here if still nothing is instantiated
-      if ( ! isset( $_instance) || ! is_object( $_instance ) ) {
-        do_action( 'czr_dev_notice', 'View : '.$id.'. The control class for : ' . $controller_group . ' has not been instantiated.' );
-        return array();
-      }
+          //stop here if still nothing is instantiated
+          if ( ! isset( $_instance) || ! is_object( $_instance ) ) {
+            do_action( 'czr_dev_notice', 'View : '.$id.'. The control class for : ' . $controller_group . ' has not been instantiated.' );
+            return array();
+          }
 
-      //build the method name
-      $method_name = "czr_fn_display_view_{$controller_cb}";//ex : czr_fn_display_view_{favicon_control}()
+          //build the method name
+          $method_name = "czr_fn_display_view_{$controller_cb}";//ex : czr_fn_display_view_{favicon_control}()
 
 
-      //make sure we have a class instance and that the requested controller method exists in it
-      if ( method_exists( $_instance , $method_name ) )
-        return array( $_instance, $method_name );
+          //make sure we have a class instance and that the requested controller method exists in it
+          if ( method_exists( $_instance , $method_name ) )
+            return array( $_instance, $method_name );
 
-      do_action( 'czr_dev_notice', 'model : '.$id.'. The method : ' . $method_name . ' has not been found in group controller : '.$controller_group );
-      return array();
+          do_action( 'czr_dev_notice', 'model : '.$id.'. The method : ' . $method_name . ' has not been found in group controller : '.$controller_group );
+          return array();
     }
 
 
@@ -186,17 +186,17 @@ if ( ! class_exists( 'CZR_cl_controllers' ) ) :
     //@param is a string : header, content, footer, modules
     //@return the $instance
     private function czr_fn_instantiate_group_controller( $group ) {
-      $_path  = "controllers/class-controller-{$group}.php";
-      $_class = "CZR_cl_controller_{$group}";
-      $_instance = false;
+          $_path  = "controllers/class-controller-{$group}.php";
+          $_class = "CZR_cl_controller_{$group}";
+          $_instance = false;
 
-      czr_fn_fw_front_require_once( $_path );
+          czr_fn_fw_front_require_once( $_path );
 
-      if ( class_exists($_class) ) {
-        $_instance = new $_class;
-        do_action( 'group_controller_instantiated', $group, $_instance );
-      }
-      return $_instance;
+          if ( class_exists($_class) ) {
+            $_instance = new $_class;
+            do_action( 'group_controller_instantiated', $group, $_instance );
+          }
+          return $_instance;
     }
 
 
@@ -204,21 +204,21 @@ if ( ! class_exists( 'CZR_cl_controllers' ) ) :
     //stores the groupd controller instance in the property : self::$controllers_instances
     //@return void()
     public function czr_fn_store_controller_instance( $group , $_instance ) {
-      $controller_instances = self::$controllers_instances;
-      if ( array_key_exists($group, $controller_instances) )
-        return;
+          $controller_instances = self::$controllers_instances;
+          if ( array_key_exists($group, $controller_instances) )
+            return;
 
-      $controller_instances[$group] = $_instance;
-      self::$controllers_instances = $controller_instances;
+          $controller_instances[$group] = $_instance;
+          self::$controllers_instances = $controller_instances;
     }
 
 
     //get an already instantiated controller group instance
     public function czr_fn_get_controller_instance( $group ) {
-      $controller_instances = self::$controllers_instances;
-      if ( ! array_key_exists($group, $controller_instances) )
-        return;
-      return $controller_instances[$group];
+          $controller_instances = self::$controllers_instances;
+          if ( ! array_key_exists($group, $controller_instances) )
+            return;
+          return $controller_instances[$group];
     }
   }//end of class
 endif;
--- core/front/models/header/class-model-logo.php
+++ core/front/models/header/class-model-logo.php
@@ -12,95 +12,96 @@ class CZR_cl_logo_model_class extends CZR_cl_Model {
   * return model params array()
   */
   function czr_fn_extend_params( $model = array() ) {
-    extract( $this -> czr_fn_get_logo_src_args( $this -> logo_type ) );
+        extract( $this -> czr_fn_get_logo_src_args( $this -> logo_type ) );
 
-    $model[ 'src' ]   = $logo_src;
-    $model[ 'alt' ]   = apply_filters( 'czr_logo_alt', __( 'Back Home', 'customizr' ) ) ;
-    $model[ 'element_class' ] = array( $this -> logo_type );
+        $model[ 'src' ]   = $logo_src;
+        $model[ 'alt' ]   = apply_filters( 'czr_logo_alt', __( 'Back Home', 'customizr' ) ) ;
+        $model[ 'element_class' ] = array( $this -> logo_type );
 
-    //build other attrs
-    $model[ 'element_attributes' ] = trim( sprintf('%1$s %2$s %3$s %4$s',
-        $logo_width ? sprintf( 'width="%1$s"', $logo_width ) : '',
-        $logo_height ? sprintf( 'height="%1$s"', $logo_height ) : '',
-        ( 1 == $logo_resize) ? sprintf( 'style="max-width:%1$spx;max-height:%2$spx"',
-                                apply_filters( 'czr_logo_max_width', 250 ),
-                                apply_filters( 'czr_logo_max_height', 100 )
-                                ) : '',
-        implode(' ' , apply_filters('czr_logo_other_attributes' , ( 0 == czr_fn_get_opt( 'tc_retina_support' ) ) ? array('data-no-retina') : array() ) )
-    ));
+        //build other attrs
+        $model[ 'element_attributes' ] = trim( sprintf('%1$s %2$s %3$s %4$s',
+            $logo_width ? sprintf( 'width="%1$s"', $logo_width ) : '',
+            $logo_height ? sprintf( 'height="%1$s"', $logo_height ) : '',
+            ( 1 == $logo_resize) ? sprintf( 'style="max-width:%1$spx;max-height:%2$spx"',
+                                    apply_filters( 'czr_logo_max_width', 250 ),
+                                    apply_filters( 'czr_logo_max_height', 100 )
+                                    ) : '',
+            implode(' ' , apply_filters('czr_logo_other_attributes' , ( 0 == czr_fn_get_opt( 'tc_retina_support' ) ) ? array('data-no-retina') : array() ) )
+        ));
 
-    return $model;
+        return $model;
   }
 
   function czr_fn_get_logo_src_args( $logo_type ) {
-    $logo_type_sep          = $logo_type ? '_sticky_' : '_';
-    $accepted_formats		= apply_filters( 'czr_logo_img_formats' , array('jpg', 'jpeg', 'png' ,'gif', 'svg', 'svgz' ) );
-    $args                   = array();
-    //check if the logo is a path or is numeric
-    //get src for both cases
-    $_logo_src 			    = '';
-    $_width 				= false;
-    $_height 				= false;
-    $_attachement_id 		= false;
-    $_logo_option  			= esc_attr( czr_fn_get_opt( "tc{$logo_type_sep}logo_upload") );
-    //check if option is an attachement id or a path (for backward compatibility)
-    if ( is_numeric($_logo_option) ) {
-      $_attachement_id 	    = $_logo_option;
-      $_attachment_data 	= apply_filters( "tc{$logo_type_sep}logo_attachment_img" , wp_get_attachment_image_src( $_logo_option , 'full' ) );
-      $_logo_src 			= $_attachment_data[0];
-      $_width 			    = ( isset($_attachment_data[1]) && $_attachment_data[1] > 1 ) ? $_attachment_data[1] : $_width;
-      $_height 			    = ( isset($_attachment_data[2]) && $_attachment_data[2] > 1 ) ? $_attachment_data[2] : $_height;
-    } else { //old treatment
-      //rebuild the logo path : check if the full path is already saved in DB. If not, then rebuild it.
-      $upload_dir 			= wp_upload_dir();
-      $_saved_path 			= esc_url ( czr_fn_get_opt( "tc{$logo_type_sep}logo_upload") );
-      $_logo_src 			= ( false !== strpos( $_saved_path , '/wp-content/' ) ) ? $_saved_path : $upload_dir['baseurl'] . $_saved_path;
-    }
-    //hook + makes ssl compliant
-    $_logo_src    			= apply_filters( "tc{$logo_type_sep}logo_src" , is_ssl() ? str_replace('http://', 'https://', $_logo_src) : $_logo_src ) ;
-    $logo_resize 			= ( $logo_type_sep == '_' ) ? esc_attr( czr_fn_get_opt( 'tc_logo_resize') ) : '';
-    $filetype 				= CZR_cl_utils::$inst -> czr_fn_check_filetype ($_logo_src);
-    if( ! empty($_logo_src) && in_array( $filetype['ext'], $accepted_formats ) )
-      $args 		= array(
-                'logo_src' 				=> $_logo_src,
-                'logo_resize' 			=> $logo_resize,
-                'logo_attachment_id' 	=> $_attachement_id,
-                'logo_width' 			=> $_width,
-                'logo_height' 			=> $_height,
-                'logo_type'             => trim($logo_type_sep,'_')
-      );
+        $logo_type_sep          = $logo_type ? '_sticky_' : '_';
+        $accepted_formats		= apply_filters( 'czr_logo_img_formats' , array('jpg', 'jpeg', 'png' ,'gif', 'svg', 'svgz' ) );
+        $args                   = array();
+        //check if the logo is a path or is numeric
+        //get src for both cases
+        $_logo_src 			    = '';
+        $_width 				= false;
+        $_height 				= false;
+        $_attachement_id 		= false;
+        $_logo_option  			= esc_attr( czr_fn_get_opt( "tc{$logo_type_sep}logo_upload") );
+        //check if option is an attachement id or a path (for backward compatibility)
+        if ( is_numeric($_logo_option) ) {
+              $_attachement_id 	    = $_logo_option;
+              $_attachment_data 	= apply_filters( "tc{$logo_type_sep}logo_attachment_img" , wp_get_attachment_image_src( $_logo_option , 'full' ) );
+              $_logo_src 			= $_attachment_data[0];
+              $_width 			    = ( isset($_attachment_data[1]) && $_attachment_data[1] > 1 ) ? $_attachment_data[1] : $_width;
+              $_height 			    = ( isset($_attachment_data[2]) && $_attachment_data[2] > 1 ) ? $_attachment_data[2] : $_height;
+        } else { //old treatment
+              //rebuild the logo path : check if the full path is already saved in DB. If not, then rebuild it.
+              $upload_dir 			= wp_upload_dir();
+              $_saved_path 			= esc_url ( czr_fn_get_opt( "tc{$logo_type_sep}logo_upload") );
+              $_logo_src 			= ( false !== strpos( $_saved_path , '/wp-content/' ) ) ? $_saved_path : $upload_dir['baseurl'] . $_saved_path;
+        }
+        //hook + makes ssl compliant
+        $_logo_src    			= apply_filters( "tc{$logo_type_sep}logo_src" , is_ssl() ? str_replace('http://', 'https://', $_logo_src) : $_logo_src ) ;
+        $logo_resize 			= ( $logo_type_sep == '_' ) ? esc_attr( czr_fn_get_opt( 'tc_logo_resize') ) : '';
+        $filetype 				= CZR_cl_utils::$inst -> czr_fn_check_filetype ($_logo_src);
+        if( ! empty($_logo_src) && in_array( $filetype['ext'], $accepted_formats ) ) {
+              $args 		= array(
+                        'logo_src' 				=> $_logo_src,
+                        'logo_resize' 			=> $logo_resize,
+                        'logo_attachment_id' 	=> $_attachement_id,
+                        'logo_width' 			=> $_width,
+                        'logo_height' 			=> $_height,
+                        'logo_type'             => trim($logo_type_sep,'_')
+              );
+        }
 
-      return $args;
+        return $args;
   }
 
   /*
   * Custom CSS
   */
   function czr_fn_user_options_style_cb( $_css ) {
-    //logos shrink
-    //fire once
-    static $_fired = false;
-    if ( $_fired ) return $_css;
-    $_fired        = true;
+        //logos shrink
+        //fire once
+        static $_fired = false;
+        if ( $_fired ) return $_css;
+        $_fired        = true;
 
-    //when to print the shrink logo CSS?
-    //1) In customizing as the sticky_header is passed as postMessage
-    //or
-    //2) The sticky header is enabled
-    //and
-    //2.1) the shrink title_logo option is enabled
-    if ( CZR() -> czr_fn_is_customizing() ||
-        ( 0 != esc_attr( czr_fn_get_opt( 'tc_sticky_header') ) && 0 != esc_attr( czr_fn_get_opt( 'tc_sticky_shrink_title_logo') ) ) ) {
-        $_logo_shrink  = implode (';' , apply_filters('czr_logo_shrink_css' , array("height:30px!important","width:auto!important") ) );
-        $_css = sprintf("%s%s",
-            $_css,
-            "
-        .sticky-enabled .tc-shrink-on .site-logo img {
-          {$_logo_shrink}
-        }"
-        );
-    }
-    return $_css;
-    //end logos shrink (fire once)
+        //when to print the shrink logo CSS?
+        //1) In customizing as the sticky_header is passed as postMessage
+        //or
+        //2) The sticky header is enabled
+        //and
+        //2.1) the shrink title_logo option is enabled
+        if ( CZR() -> czr_fn_is_customizing() ||
+            ( 0 != esc_attr( czr_fn_get_opt( 'tc_sticky_header') ) && 0 != esc_attr( czr_fn_get_opt( 'tc_sticky_shrink_title_logo') ) ) ) {
+            $_logo_shrink  = implode (';' , apply_filters('czr_logo_shrink_css' , array("height:30px!important","width:auto!important") ) );
+            $_css = sprintf("%s%s",
+                $_css,
+                "
+            .sticky-enabled .tc-shrink-on .site-logo img {
+              {$_logo_shrink}
+            }"
+            );
+        }
+        return $_css;
+        //end logos shrink (fire once)
   }
 }//end class
