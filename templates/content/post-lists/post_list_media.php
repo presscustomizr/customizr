e154f1f23c025f4207df151e80dc735740567e1b
Rocco Aliberti <rocco@presscustomizr.com>
2016-09-05 11:49:41 +0200

    Imp: thumb popup sketch + better excerpt filter hooking

--- templates/content/post-lists/post_list_media.php
+++ templates/content/post-lists/post_list_media.php
@@ -8,9 +8,14 @@
  * @since Customizr 3.5.0
  */
 ?>
-<section class="tc-thumbnail entry-image__holder <?php czr_fn_echo( 'element_class' ) ?>" <?php czr_fn_echo('element_attributes') ?>>
+<section class="tc-thumbnail entry-image__container <?php czr_fn_echo( 'element_class' ) ?>" <?php czr_fn_echo('element_attributes') ?>>
 <?php if ( czr_fn_get('czr_has_media') ): ?>
   <?php czr_fn_echo('media_content') ?>
+  <?php if ( (bool) ( $original_thumb_url = czr_fn_get( 'original_thumb_url' ) ) ): ?>
+    <div class="post-action">
+      <a href="<?php echo $original_thumb_url ?>" class="expand-img"><i class="icn-expand"></i></a>
+    </div>
+  <?php endif ?>
 <?php else: ?>
   <div class="post-type__icon">
     <icon class="icn-<?php czr_fn_echo('icon_type') ?>"></i>
