a1839ea9046ae8ef662815733eccaa837680432b
Rocco Aliberti <rocco@presscustomizr.com>
2016-09-08 13:36:35 +0200

    Fix: fix no-title header class adding bug

--- core/front/models/content/post-lists/class-model-post_list_header.php
+++ core/front/models/content/post-lists/class-model-post_list_header.php
@@ -5,7 +5,7 @@ class CZR_cl_post_list_header_model_class extends CZR_cl_Model {
   function czr_fn_setup_late_properties() {
     $this -> czr_fn_update( array(
       'has_header_format_icon' => czr_fn_get( 'has_header_format_icon' ),
-      'element_class'          => empty( get_the_title() ) ? '' : 'no-title'
+      'element_class'          => ! empty( get_the_title() ) ? '' : 'no-title'
     ) );
   }
 
