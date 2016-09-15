abace8114dd1c539da22b0e62cff54fd9186784d
Rocco Aliberti <rocco@presscustomizr.com>
2016-09-15 18:55:33 +0200

    Fix: footer widgets - by default all have the same width

--- templates/footer/footer_widgets.php
+++ templates/footer/footer_widgets.php
@@ -15,15 +15,16 @@
     <?php do_action("__before_footer_widgets") ?>
     <?php
       $_footer_widgets  = apply_filters( 'tc_footer_widgets', CZR_cl_init::$instance -> footer_widgets );
+      $class = $_footer_widgets ? 'col-md-' . 12/( count($_footer_widgets) ) : '';
 
-      $class = $_footer_widgets ? 'col-md-' . 12/( count($_footer_widgets) + 1 ) : '';
       foreach ( $_footer_widgets as $key => $area ):
       ?>
-        <div id="<?php echo $key; ?>" class="<?php echo 'footer_one' == $key ? 'col-md-6 about': $class ?> col-xs-12">
+        <div id="<?php echo $key ?>" class="<?php echo $class ?> col-xs-12">
           <?php dynamic_sidebar( $key ) ?>
         </div>  
       <?php
       endforeach;
     ?>
+    <?php do_action("__after_footer_widgets") ?>
   </div>
 </div>
