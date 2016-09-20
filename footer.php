d824495c27a3b1b90b15dc577d78cc72b36b2d63
Rocco Aliberti <rocco@presscustomizr.com>
2016-09-20 10:55:16 +0200

    Imp: add full page search

--- footer.php
+++ footer.php
@@ -9,9 +9,10 @@
  */
 ?>
       <?php czr_fn_render_template('footer'); ?>
-    </div>
+    </div><!-- end #tc-page-wrap -->
 
     <?php do_action('__after_page_wrapper'); ?>
+    <?php if ( czr_fn_has('saerch_full_page') ){ czr_fn_render_template('modules/search_full_page'); }; ?>
     <?php if ( czr_fn_has('btt_arrow') ){ czr_fn_render_template('footer/btt_arrow'); }; ?>
     <?php wp_footer() ?>
   </body>
