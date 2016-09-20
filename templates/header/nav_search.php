78cc93035b5aa086ac42f8fea520314d21d98418
Rocco Aliberti <rocco@presscustomizr.com>
2016-09-20 10:55:16 +0200

    Imp: add full page search

--- templates/header/nav_search.php
+++ templates/header/nav_search.php
@@ -1,16 +1,9 @@
 <?php
 /**
- * The template for displaying the header search form and button
+ * The template for displaying the header search form (mobile) and button
  */
 ?>
 <li class="primary-nav__search" <?php czr_fn_echo('element_attributes') ?>>
   <a class="desktop_search__link"><span class="sr-only">Search</span><i class="icn-search"></i></a>
-  <div class="search-form__container">
-    <form action="http://test.presscustomizr.com/" method="get" class="czr-form search-form">
-      <div class="form-group">
-        <label for="email">Search<i class="icn-search"></i></label>
-        <input id="sidebar-search" class="form-control" name="search" type="text" value="" aria-describedby="search form" title="Search presscustomizr.com for ...">
-       </div>
-    </form>
-  </div>
+  <?php get_search_form() ?>
 </li>
