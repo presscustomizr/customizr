837742700340ec174189055655959d353d5a3955
Rocco Aliberti <rocco@presscustomizr.com>
2016-09-26 14:40:56 +0200

    Imp: be sure to transform only labels of input text types

--- searchform.php
+++ searchform.php
@@ -5,7 +5,7 @@
 ?>
 <div class="search-form__container">
   <form action="<?php echo esc_url(home_url( '/' )); ?>" method="get" class="czr-form search-form">
-    <div class="form-group">
+    <div class="form-group czr-focus">
       <?php $label_id = rand() ?>
       <label class="screen-reader-text" for="s" id="search-<?php echo $label_id ?>"><span><?php _ex( 'Search', 'label', 'customizr') ?></span><i class="icn-search"></i><i class="icn-close"></i></label>
       <input class="form-control" name="s" type="text" value="<?php echo get_search_query() ?>" aria-describedby="search-<?php echo $label_id ?>" title="<?php echo esc_attr_x( 'Search &hellip;', 'title', 'customizr') ?>">
