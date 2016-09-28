e8f7614f9747be6004b4ba7a5fff9194ebbb5359
Rocco Aliberti <rocco@presscustomizr.com>
2016-09-28 18:54:21 +0200

    Imp: small improvements to the related posts

--- templates/content/post-lists/post_list_content.php
+++ templates/content/post-lists/post_list_content.php
@@ -8,28 +8,28 @@
  */
 ?>
 <section class="tc-content entry-content__holder <?php czr_fn_echo('element_class') ?>" <?php czr_fn_echo('element_attributes') ?> >
+  <div class="entry-content__wrapper">
+    <?php do_action( 'before_post_list_entry_content' ) ?>
 
-  <?php do_action( 'before_post_list_entry_content' ) ?>
+    <?php if ( czr_fn_has('post_list_header') ) czr_fn_render_template('content/post-lists/post_list_header') ?>
 
-  <?php if ( czr_fn_has('post_list_header') ) czr_fn_render_template('content/post-lists/post_list_header') ?>
-
-  <div class="<?php czr_fn_echo( 'content_class' ) ?>">
-    <?php
-      czr_fn_echo( 'post_list_content', null, array(
-        __( 'Continue reading <span class="meta-nav">&rarr;</span>' , 'customizr' ),
-        wp_link_pages( array(
-          'before'        => '<div class="post-pagination row"><div class="col-md-12">',
-          'after'         => '</div></div>',
-          'link_before'   => '<span>',
-          'link_after'    => '</span>',
+    <div class="<?php czr_fn_echo( 'content_class' ) ?>">
+      <?php
+        czr_fn_echo( 'post_list_content', null, array(
+          __( 'Continue reading <span class="meta-nav">&rarr;</span>' , 'customizr' ),
+          wp_link_pages( array(
+            'before'        => '<div class="post-pagination row"><div class="col-md-12">',
+            'after'         => '</div></div>',
+            'link_before'   => '<span>',
+            'link_after'    => '</span>',
+            )
           )
-        )
-      ) );
-    ?>
+        ) );
+      ?>
+    </div>
+
+    <?php if ( czr_fn_has('post_list_footer') ) czr_fn_render_template('content/post-lists/post_list_footer') ?>
+
+    <?php do_action( 'after_post_list_entry_content' ) ?>
   </div>
-
-  <?php if ( czr_fn_has('post_list_footer') ) czr_fn_render_template('content/post-lists/post_list_footer') ?>
-
-  <?php do_action( 'after_post_list_entry_content' ) ?>
-
 </section>
