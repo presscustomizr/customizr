8edf2df8636983d37abbdd154ef4802dc2bca8c1
Rocco Aliberti <rocco@presscustomizr.com>
2016-10-10 12:25:08 +0200

    Fix: some header fixes

--- core/front/models/header/class-model-second_menu.php
+++ core/front/models/header/class-model-second_menu.php
@@ -6,7 +6,7 @@ class CZR_cl_second_menu_model_class extends CZR_cl_menu_model_class {
   * @override
   */
   protected function get_element_class() {
-    return array_merge( parent::get_element_class(), array( 'secondary-nav__container' ) );
+    return array_merge( parent::get_element_class(), array( 'secondary-nav__menu-wrapper' ) );
   }
 
   /*
