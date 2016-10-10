a8127f9d3d26ff182f416a9194b55c038f768a51
Rocco Aliberti <rocco@presscustomizr.com>
2016-10-10 12:25:08 +0200

    Fix: some header fixes

--- templates/header/topnav.php
+++ templates/header/topnav.php
@@ -4,12 +4,12 @@
 */
 ?>
 <div class="hidden-tablet secondary-navbar__wrapper row" <?php czr_fn_echo('element_attributes') ?>>
-  <div class="col-md-6">
+  <div class="col-md-9">
     <div class="secondary-nav__container">
       <?php czr_fn_render_template('header/menu', 'secondary_menu' ) ?>
     </div>
   </div>
-  <div class="col-md-6">
+  <div class="col-md-3">
     <div class="secondary-nav__socials">
       <?php czr_fn_render_template('header/header_socials') ?>
     </div>
