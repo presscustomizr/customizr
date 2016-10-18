315fa598df9e2cb5824e3d1f59e670ec2e315a7d
Rocco Aliberti <rocco@presscustomizr.com>
2016-10-18 12:22:38 +0200

    Fix: several small bug fixes

--- core/front/models/header/class-model-second_menu.php
+++ core/front/models/header/class-model-second_menu.php
@@ -1,13 +1,15 @@
 <?php
 class CZR_cl_second_menu_model_class extends CZR_cl_menu_model_class {
   public $theme_location = 'secondary';
-
+  public $menu_id = 'secondary-menu';
   /*
   * @override
   */
   protected function get_element_class() {
-    return array_merge( parent::get_element_class(), array( 'secondary-nav__menu-wrapper' ) );
+    $_menu_position_class =  esc_attr( czr_fn_get_opt( 'tc_second_menu_position') );
+    return array_merge( parent::get_element_class(), array( 'secondary-nav__menu-wrapper', $_menu_position_class ) );
   }
+
 
   /*
   * @override
