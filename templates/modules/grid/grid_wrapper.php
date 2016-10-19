1475cc3061bcece1ef7854164f69c45819bfa25e
Rocco Aliberti <rocco@presscustomizr.com>
2016-10-19 15:58:20 +0200

    Imp: merge grid_item model into grid_wrapper model

--- templates/modules/grid/grid_wrapper.php
+++ templates/modules/grid/grid_wrapper.php
@@ -9,10 +9,10 @@
  */
 
 /* Are we at the start of the loop? in this case print a section wrapper element */
-if ( czr_fn_get( 'is_loop_start' ) ) :
+if ( czr_fn_is_loop_start() ) :
 
 ?>
-<div class="row grid-container__classic" <?php czr_fn_echo('element_attributes') ?>>
+<div class="row grid-container__classic <?php czr_fn_echo('element_class') ?>" <?php czr_fn_echo('element_attributes') ?>>
 <?php
 
 endif;
@@ -24,12 +24,15 @@ endif;
   if ( czr_fn_get( 'is_first_of_row' ) ) :
 
   ?>
-  <section class="row grid grid-cols-<?php czr_fn_echo( 'section_cols' ) ?> <?php czr_fn_echo( 'element_class' ) ?>">
+  <section class="row grid grid-cols-<?php czr_fn_echo( 'section_cols' ) ?>">
   <?php
 
   endif;
 
-    if ( czr_fn_has( 'grid_item' ) ) czr_fn_render_template( 'modules/grid/grid_item', 'grid_item' );
+    if ( czr_fn_has( 'grid_item' ) )
+      czr_fn_render_template( 'modules/grid/grid_item', 'grid_item',
+        czr_fn_get( 'grid_item' )
+      );
 
   /* close the row if the displayed item is the last of row */
   if ( czr_fn_get( 'is_last_of_row' ) ) :
@@ -41,7 +44,7 @@ endif;
   endif;
 
 /* Close se section at the end of the loop */
-if ( czr_fn_get( 'is_loop_end' ) ) : ?>
+if ( czr_fn_is_loop_end() ) : ?>
 </div>
 <?php
 
