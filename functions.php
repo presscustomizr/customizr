d97f3ce4944d3bdfa1eee03124420993b668a2ef
Rocco Aliberti <rocco@presscustomizr.com>
2016-11-03 11:54:32 +0100

    Imp: move functions and functions-test into core

--- functions.php
+++ functions.php
@@ -1,226 +1,57 @@
 <?php
-require_once( get_template_directory() . '/functions-test.php' );
-
-//@todo : breadcrumd czr_fn_echo does not work in the template
-//@todo : remove the
-//Fire
-require_once( get_template_directory() . '/core/init.php' );
-
-//shortcut function to get a Customizr option
-if ( ! function_exists('czr_fn_get_opt') ) {
-  function czr_fn_get_opt( $option_name , $option_group = null, $use_default = true ) {
-    return czr_fn_opt( $option_name , $option_group, $use_default ) ;
-  }
-}
-
-/*
- * @since 3.5.0
- */
-//shortcut function to echo the column content wrapper class
-if ( ! function_exists('czr_fn_column_content_wrapper_class') ) {
-  function czr_fn_column_content_wrapper_class() {
-    return CZR() -> czr_fn_column_content_wrapper_class();
-  }
-}
-
-/*
- * @since 3.5.0
- */
-//shortcut function to echo the article container class
-if ( ! function_exists('czr_fn_article_container_class') ) {
-  function czr_fn_article_container_class() {
-    return CZR() -> czr_fn_article_container_class();
-  }
-}
-
-/*
- * @since 3.5.0
- */
-//shortcut function to get a theme file
-if ( ! function_exists('czr_fn_get_theme_file') ) {
-  function czr_fn_get_theme_file( $path_suffix ) {
-    return CZR() -> czr_fn_get_theme_file( $path_suffix );
-  }
-}
-/*
- * @since 3.5.0
- */
-//shortcut function to get a theme file
-if ( ! function_exists('czr_fn_get_theme_file_url') ) {
-  function czr_fn_get_theme_file_url( $url_suffix ) {
-    return CZR() -> czr_fn_get_theme_file_url( $url_suffix );
-  }
-}
-/*
- * @since 3.5.0
- */
-//shortcut function to require a theme file
-if ( ! function_exists('czr_fn_require_once') ) {
-  function czr_fn_require_once( $path_suffix ) {
-    return CZR() -> czr_fn_require_once( $path_suffix );
-  }
-}
-
-
-/*
- * @since 3.5.0
- */
-//shortcut function to set the current model which will be accessible by the czr_fn_get
-if ( ! function_exists('czr_fn_set_current_model') ) {
-  function czr_fn_set_current_model( $model ) {
-    return CZR() -> czr_fn_set_current_model( $model );
-  }
-}
-
-/*
- * @since 3.5.0
- */
-//shortcut function to reset the current model
-if ( ! function_exists('czr_fn_reset_current_model') ) {
-  function czr_fn_reset_current_model() {
-    return CZR() -> czr_fn_reset_current_model();
-  }
-}
-
-/*
- * @since 3.5.0
- */
-//shortcut function to get a current model property
-if ( ! function_exists('czr_fn_get') ) {
-  function czr_fn_get( $property, $model_id = null, $args = array() ) {
-    return CZR() -> czr_fn_get( $property, $model_id, $args );
-  }
-}
-
-/*
- * @since 3.5.0
- */
-//shortcut function to echo a current model property
-if ( ! function_exists('czr_fn_echo') ) {
-  function czr_fn_echo( $property, $model_id = null, $args = array() ) {
-    return CZR() -> czr_fn_echo( $property, $model_id, $args );
-  }
-}
-
-/*
- * @since 3.5.0
- */
-//shortcut function to instantiate easier
-if ( ! function_exists('czr_fn_new') ) {
-  function czr_fn_new( $_to_load, $_args = array() ) {
-    CZR() -> czr__fn_( $_to_load , $_args );
-    return;
-  }
-}
-
-//shortcut function to instantiate a model + render its template
-//model and template should share the same name
-//some templates are shared by several models => that's when the $_id param is useful
-if ( ! function_exists('czr_fn_render_template') ) {
-  function czr_fn_render_template( $_t, $_id = null, $args = array() ) {
-    if ( ! $_t || empty($_t) )
-        return;
-
-    $_model_id = is_null($_id) ? basename($_t) : $_id;
-
-    if ( czr_fn_is_registered( $_model_id ) ) {
-      $model_instance = czr_fn_get_model_instance( $_model_id );
-
-      //sets the template property on the fly based on what's requested
-      if ( ! czr_fn_get_model_property( $_model_id, 'template') ) {
-        $model_instance -> czr_fn_set_property('template' , $_t );
-      }
-      //update model with the one passed
-      if ( ! empty ( $args ) )
-        $model_instance -> czr_fn_update( $args );
-
-      czr_fn_get_view_instance($_model_id ) -> czr_fn_maybe_render();
-    }
-    else {
-      //$_model_instance = CZR() -> collection -> czr_fn_get_model_instance( $_model_id );
-      czr_fn_register( array( 'id' => $_model_id, 'render' => true, 'template' => $_t, 'args' => $args ) );
-    }
-  }
-}
-
-//shortcut function to render a post list
-if ( ! function_exists('czr_fn_render_list_of_posts') ) {
-  function czr_fn_render_list_of_posts( $_t = null, $_id = null, $args = array() ) {
-    if ( $_t && $_id )
-      czr_fn_render_template( $_t, $_id, $args );
-
-    elseif ( czr_fn_has('post_list_grid') ) {
-      czr_fn_render_template('modules/grid/grid_wrapper', 'post_list_grid');
-    }
-    elseif ( czr_fn_has('post_list') ){
-      czr_fn_render_template('content/post-lists/post_list_alternate', 'post_list');
-    }elseif ( czr_fn_has('post_list_masonry') ) {
-      czr_fn_render_template('content/post-lists/post_list_masonry', 'post_list_masonry');
-    }elseif ( czr_fn_has('post_list_plain') ) {
-      czr_fn_render_template('content/post-lists/post_list_plain', 'post_list_plain');
-    }elseif ( czr_fn_has('post_list_plain_excerpt') ) {
-      czr_fn_render_template('content/post-lists/post_list_plain_excerpt', 'post_list_plain_excerpt');
-    }
-
-  }
-}
-
-//@return boolean
-//states if registered and possible
-//useful is a check has to be done in the template before "instant" registration.
-function czr_fn_has( $_t, $_id = null, $only_registered = false ) {
-  $_model_id = is_null($_id) ? $_t : $_id;
-  if ( CZR() -> collection -> czr_fn_is_registered( $_model_id ) ) {
-    return true;
-  }
-  //if the model is not registered yet, let's test its eligibility by accessing directly its controller boolean if exists
-  elseif ( ! $only_registered ) {
-    return CZR() -> controllers -> czr_fn_is_possible( $_model_id );
-  }
-}
-
-//@return boolean
-//states if registered only
-function czr_fn_is_registered( $_model_id ) {
-  return CZR() -> collection -> czr_fn_is_registered( $_model_id );
-}
-
-//@return model object if exists
-function czr_fn_get_model_instance( $_model_id ) {
-  if ( ! CZR() -> collection -> czr_fn_is_registered( $_model_id ) )
-    return;
-  return CZR() -> collection -> czr_fn_get_model_instance( $_model_id );
-}
-
-//@return model property if exists
-//@param _model_id string
-//@param property name string
-function czr_fn_get_model_property( $_model_id, $_property ) {
-  if ( ! CZR() -> collection -> czr_fn_is_registered( $_model_id ) )
-    return;
-  return CZR() -> collection -> czr_fn_get_model_instance( $_model_id ) -> czr_fn_get_property($_property);
-}
-
-//@return view model object if exists
-function czr_fn_get_view_instance( $_model_id ) {
-  if ( ! isset(CZR() -> collection -> czr_fn_get_model_instance( $_model_id ) -> view_instance) )
-    return;
-  return CZR() -> collection -> czr_fn_get_model_instance( $_model_id ) -> view_instance;
-}
-
-
-function czr_fn_register( $model = array() ) {
-  return CZR() -> collection -> czr_fn_register( $model );
-}
+/**
+*
+* This program is a free software; you can use it and/or modify it under the terms of the GNU
+* General Public License as published by the Free Software Foundation; either version 2 of the License,
+* or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+* even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* You should have received a copy of the GNU General Public License along with this program; if not, write
+* to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*
+* @package    Customizr
+* @subpackage functions
+* @since      1.0
+* @author     Nicolas GUILLAUME <nicolas@presscustomizr.com>
+* @copyright  Copyright (c) 2013-2016, Nicolas GUILLAUME
+* @link       http://presscustomizr.com/customizr
+* @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
 
 
 /**
- * @since 3.5.0
- * @return object CZR Instance
- */
-function CZR() {
-  return CZR___::czr_fn_instance();
-}
+* This is where Customizr starts. This file defines and loads the theme's components :
+* => Constants : CUSTOMIZR_VER, TC_BASE, TC_BASE_CHILD, TC_BASE_URL, TC_BASE_URL_CHILD, THEMENAME, CZR_WEBSITE
+* => Default filtered values : images sizes, skins, featured pages, social networks, widgets, post list layout
+* => Text Domain
+* => Theme supports : editor style, automatic-feed-links, post formats, navigation menu, post-thumbnails, retina support
+* => Plugins compatibility : JetPack, bbPress, qTranslate, WooCommerce and more to come
+* => Default filtered options for the customizer
+* => Customizr theme's hooks API : front end components are rendered with action and filter hooks
+*
+* The method CZR__::czr_fn__() loads the php files and instantiates all theme's classes.
+* All classes files (except the class__.php file which loads the other) are named with the following convention : class-[group]-[class_name].php
+*
+* The theme is entirely built on an extensible filter and action hooks API, which makes customizations easy and safe, without ever needing to modify the core structure.
+* Customizr's code acts like a collection of plugins that can be enabled, disabled or extended.
+*
+* If you're not familiar with the WordPress hooks concept, you might want to read those guides :
+* http://docs.presscustomizr.com/article/26-wordpress-actions-filters-and-hooks-a-guide-for-non-developers
+* https://codex.wordpress.org/Plugin_API
+*/
 
-// Fire Customizr
-CZR();
+//Fire Customizr
+require_once( get_template_directory() . '/core/init.php' );
+
+/**
+* THE BEST AND SAFEST WAY TO EXTEND THE CUSTOMIZR THEME WITH YOUR OWN CUSTOM CODE IS TO CREATE A CHILD THEME.
+* You can add code here but it will be lost on upgrade. If you use a child theme, you are safe!
+*
+* Don't know what a child theme is ? Then you really want to spend 5 minutes learning how to use child themes in WordPress, you won't regret it :) !
+* https://codex.wordpress.org/Child_Themes
+*
+* More informations about how to create a child theme with Customizr : http://docs.presscustomizr.com/article/24-creating-a-child-theme-for-customizr/
+* A good starting point to customize the Customizr theme : http://docs.presscustomizr.com/article/35-how-to-customize-the-customizr-wordpress-theme/
+*/
