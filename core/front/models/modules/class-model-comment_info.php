a8e9c69a8fa68f3054c9710bcd7da46f4bfa80fa
Rocco Aliberti <rocco@presscustomizr.com>
2016-11-04 11:09:53 +0100

    Fix: fix disqus compatibility

--- core/front/models/modules/class-model-comment_info.php
+++ core/front/models/modules/class-model-comment_info.php
@@ -1,5 +1,7 @@
 <?php
 class CZR_comment_info_model_class extends CZR_Model {
+  public $link_attributes = array();
+
   /*
   * @param link (stirng url) the link
   * @param add_anchor (bool) whether or not add an anchor to the link, default true
@@ -7,8 +9,12 @@ class CZR_comment_info_model_class extends CZR_Model {
   function czr_fn_get_comment_info_link( $link, $add_anchor = true ) {
     $link = sprintf( "%s%s",
         is_singular() ? '' : esc_url( $link ),
-        $add_anchor ? apply_filters( 'czr_info_comment_anchor', '#czr-comments-title') : ''
+        $add_anchor ? apply_filters( 'czr_comment_info_anchor', '#czr-comments-title') : ''
     );
     return $link;
+  }
+
+  function czr_fn_get_link_attributes() {
+    return apply_filters( 'czr_comment_info_link_attributes', $this -> link_attributes );
   }
 }
--- templates/modules/comment_info.php
+++ templates/modules/comment_info.php
@@ -4,7 +4,7 @@
  *
  */
 ?>
-<a class="comments__link <?php czr_fn_echo( 'element_class' ) ?>" href="<?php czr_fn_echo( 'comment_info_link', null, array( get_permalink() ) ) ?>" title="<?php echo get_comments_number() ?> <?php _e( 'Comment(s) on', 'customizr') ?> <?php echo esc_attr( strip_tags( get_the_title() ) ) ?>" <?php czr_fn_echo('element_attributes') ?> data-disqus-identifier="javascript:this.page.identifier">
+<a class="comments__link <?php czr_fn_echo( 'element_class' ) ?>" href="<?php czr_fn_echo( 'comment_info_link', null, array( get_permalink() ) ) ?>" title="<?php echo get_comments_number() ?> <?php _e( 'Comment(s) on', 'customizr') ?> <?php echo esc_attr( strip_tags( get_the_title() ) ) ?>" <?php czr_fn_echo('element_attributes') ?> <?php czr_fn_echo('link_attributes') ?>>
   <span>
     <?php echo number_format_i18n( get_comments_number() ) . ' ' . _n( 'comment' , 'comments' , get_comments_number(), 'customizr' ) ?>
   </span>
