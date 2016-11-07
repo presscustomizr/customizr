f607150807404641d3daadd251b3a629da4f14ae
Rocco Aliberti <rocco@presscustomizr.com>
2016-11-07 11:04:47 +0100

    Imp: final merge new php framework

--- _fmk/templates/header/logo.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * The template for displaying the Logo(s) - both standard and sticky one
- */
-?>
-<img src="<?php czr_fn_echo( 'src' ) ?>" alt="<?php czr_fn_echo( 'alt' ) ?>" class="<?php czr_fn_echo( 'element_class') ?>" <?php czr_fn_echo( 'element_attributes' ) ?>>
--- _fmk/templates/modules/recently_updated.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * The template for displaying the recently updated label for posts in single and post lists contexts
- *
- * @package Customizr
- * @since Customizr 3.5.0
- */
-?>
-&nbsp;<span class="tc-update-notice label <?php czr_fn_echo( 'element_class' ) ?>" <?php czr_fn_echo( 'element_attributes' ) ?>><?php czr_fn_echo( 'recently_updated_text' ) ?></span>
