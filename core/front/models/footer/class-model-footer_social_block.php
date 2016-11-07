d7e389e1881951915bd70008f75703ae8f8582d2
Rocco Aliberti <rocco@presscustomizr.com>
2016-11-07 15:00:39 +0100

    Imp: prepare fktest merging

--- core/front/models/footer/class-model-footer_social_block.php
+++
@@ -1,6 +0,0 @@
-<?php
-class CZR_cl_footer_social_block_model_class extends CZR_cl_social_block_model_class {
-  public $element_class    = array('tc-footer-social-links-wrapper');
-  public $element_tag      = 'span';
-  public $where            = 'footer';
-}
