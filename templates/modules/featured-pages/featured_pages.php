64f658a6b08939ecabb9083b819fa7fdae82d7fd
Rocco Aliberti <rocco@presscustomizr.com>
2016-11-17 16:51:47 +0100

    Imp: improve featured pages css

--- templates/modules/featured-pages/featured_pages.php
+++ templates/modules/featured-pages/featured_pages.php
@@ -3,7 +3,7 @@
  * The template for displaying the featured pages wrapper
  */
 ?>
-<div class="container marketing" <?php czr_fn_echo('element_attributes') ?>>
+<div class="container marketing <?php czr_fn_echo('element_class') ?>" <?php czr_fn_echo('element_attributes') ?>>
   <?php
     do_action( '__before_fp' );
     while ( $featured_page = czr_fn_get( 'featured_page' ) ) {
