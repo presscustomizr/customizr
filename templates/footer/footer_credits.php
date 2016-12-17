5e4d0c112b17cc585abe19e9f3ae85b2c6dcaba3
eri-trabiccolo <raliberti84@gmail.com>
2016-12-17 15:26:38 +0100

    Imp: allow footer link to WordPress to be localized

--- templates/footer/footer_credits.php
+++ templates/footer/footer_credits.php
@@ -9,6 +9,6 @@
     <span class="czr-copyright-text">&copy;&nbsp;<?php echo esc_attr( date('Y') ) ?>&nbsp;</span><a class="czr-copyright-link" href="<?php echo esc_url( home_url() ) ?>" title="<?php echo esc_attr( get_bloginfo() ) ?>"><?php echo esc_attr( get_bloginfo() ) ?></a><span class="czr-rights-text">&nbsp;&ndash;&nbsp;<?php _e( 'All rights reserved', 'customizr') ?></span>
   </p>
   <p class="czr-credits">
-    <span class="czr-designer"><span class="czr-designer-text"><?php _e('Designed by', 'customizr') ?>&nbsp;<a class="czr-designer-link" href="<?php echo CZR_WEBSITE ?>" title="Press Customizr">Press Customizr</a></span><span class="czr-wp-powered">&nbsp;&ndash;&nbsp;<span class="czr-wp-powered-text"><?php _e( 'Powered by', 'customizr') ?>&nbsp;</span><a class="czr-wp-powered-link fa fa-wordpress" title="<?php _e( 'Powered by WordPress', 'customizr' ) ?>" href="https://www.wordpress.org" target="_blank"></a></span>
+    <span class="czr-designer"><span class="czr-designer-text"><?php _e('Designed by', 'customizr') ?>&nbsp;<a class="czr-designer-link" href="<?php echo CZR_WEBSITE ?>" title="Press Customizr">Press Customizr</a></span><span class="czr-wp-powered">&nbsp;&ndash;&nbsp;<span class="czr-wp-powered-text"><?php _e( 'Powered by', 'customizr') ?>&nbsp;</span><a class="czr-wp-powered-link fa fa-wordpress" title="<?php _e( 'Powered by WordPress', 'customizr' ) ?>" href="<?php echo esc_url( __( 'https://wordpress.org/', 'customizr' ) ); ?>" target="_blank"></a></span>
   </p>
 </div>
