27e053f13cc39a228524732f3495a308dd8fc6a6
eri-trabiccolo <raliberti84@gmail.com>
2016-12-17 15:25:21 +0100

    Fix: fix customizer deep link for wp 4.6.1+

--- core/utils/class-fire-utils.php
+++ core/utils/class-fire-utils.php
@@ -615,8 +615,9 @@ function czr_fn_is_secondary_menu_enabled() {
 * @param $autofocus(optional) is an array indicating the elements to focus on ( control,section,panel).
 * Ex : array( 'control' => 'tc_front_slider', 'section' => 'frontpage_sec').
 * Wordpress will cycle among autofocus keys focusing the existing element - See wp-admin/customize.php.
-* The actual focused element depends on its type according to this priority scale: control, section, panel.
-* In this sense when specifying a control, additional section and panel could be considered as fall-back.
+* // Following not valid anymore in wp 4.6.1, due to a bug?
+* //The actual focused element depends on its type according to this priority scale: control, section, panel.
+* //In this sense when specifying a control, additional section and panel could be considered as fall-back.
 *
 * @param $control_wrapper(optional) is a string indicating the wrapper to apply to the passed control. By default is "tc_theme_options".
 * Ex: passing $aufocus = array('control' => 'tc_front_slider') will produce the query arg 'autofocus'=>array('control' => 'tc_theme_options[tc_front_slider]'
@@ -632,16 +633,24 @@ function czr_fn_get_customizer_url( $autofocus = null, $control_wrapper = 'tc_th
     if ( is_null($autofocus) )
       return $_customize_url;
 
+    $_ordered_keys = array( 'control', 'section', 'panel');
+
     // $autofocus must contain at least one key among (control,section,panel)
-    if ( ! count( array_intersect( array_keys($autofocus), array( 'control', 'section', 'panel') ) ) )
+    if ( ! count( array_intersect( array_keys($autofocus), $_ordered_keys ) ) )
       return $_customize_url;
 
     // wrap the control in the $control_wrapper if neded
     if ( array_key_exists( 'control', $autofocus ) && ! empty( $autofocus['control'] ) && $control_wrapper ){
       $autofocus['control'] = $control_wrapper . '[' . $autofocus['control'] . ']';
     }
-    // We don't really have to care for not existent autofocus keys, wordpress will stash them when passing the values to the customize js
-    return add_query_arg( array( 'autofocus' => $autofocus ), $_customize_url );
+
+    //Since wp 4.6.1
+    $autofocus = array_merge( array_flip( $_ordered_keys ), $autofocus );
+    if ( false !== $autofocus = reset($autofocus) )
+      // We don't really have to care for not existent autofocus keys, wordpress will stash them when passing the values to the customize js
+      return add_query_arg( array( 'autofocus' => $autofocus ), $_customize_url );
+    else
+      return $_customize_url;
 }
 
 
