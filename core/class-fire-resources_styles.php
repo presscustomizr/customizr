c987274ef923fcb3ec1cfab6a0eba4e033e3d5ee
eri-trabiccolo <raliberti84@gmail.com>
2017-03-16 20:08:33 +0100

    Imp: initial add of a decent color library

--- core/class-fire-resources_styles.php
+++ core/class-fire-resources_styles.php
@@ -29,103 +29,224 @@ if ( ! class_exists( 'CZR_resources_styles' ) ) :
             if ( $skin_color == $def_skin_color )
                 return;
 
-            $skin_dark_color      = czr_fn_sass_darken( $skin_color, '10%' );
-            $skin_darkest_color   = czr_fn_sass_darken( $skin_color, '25%' );
-            $skin_light_color     = czr_fn_sass_lighten( $skin_color, '10%' );
-            $skin_lightest_color  = czr_fn_sass_lighten( $skin_color, '25%' );
+            $skin_dark_color      = czr_fn_sass_darken( $skin_color, '15' );
+            $skin_darkest_color   = czr_fn_sass_darken( $skin_color, '25' );
+            $skin_light_color     = czr_fn_sass_lighten( $skin_color, '15' );
+            $skin_lightest_color  = czr_fn_sass_lighten( $skin_color, '25' );
 
             //LET'S DANCE
             //start computing style
             $styles    = array();
             $glue      = esc_attr( czr_fn_get_opt('tc_minified_skin') ) ? '' : "\n";
 
-            $styles[]  = '::selection, ::moz-selection { background-color: '. $skin_color .' }';
+            $skinner   = array(
+                'skin_color' => array(
+                    'color'  => $skin_color,
+                    'rules'  => array(
+                        //prop => selectors
+                        'color'  => array(
+                            'a',
+                            '.btn-skin:hover',
+                            '.btn-skin.inverted',
+                            '.post-type__icon:hover .icn-format',
+                            '[class*="grid-container__"] .hover .entry-title a'
+                        ),
+                        'border-color' => array(
+                            '.btn-skin',
+                            '.btn-skin:hover',
+                        ),
+                        'background-color' => array(
+                            '.btn-skin',
+                            '.btn-skin.inverted:hover'
+                        )
+                    )
+                ),
 
-            /*Skin color => color Property */
-            $_skin_color_color_prop_selectors = array(
-                'a',
-                '.btn-skin:hover',
-                '.btn-skin.inverted',
-                '.post-type__icon:hover .icn-format',
-                '[class*="grid-container__"] .hover .entry-header .entry-title a'
+                'skin_lightest_color_shade_high' => array(
+                    'color'  => czr_fn_hex2rgba( $skin_lightest_color, 0.2, $array=false, $make_prop_value=true),
+                    'rules'  => array(
+                        'background-color' => array(
+                            '.post-navigation',
+                        )
+                    )
+                ),
+
+
+                'skin_dark_color' => array(
+                    'color'  => $skin_darkest_color,
+                    'rules'  => array(
+                        //prop => selectors
+                        'background-color' => array(
+                            '.flickity-page-dots .dot'
+                        )
+                    )
+                ),
+
+                'skin_darkest_color' => array(
+                    'color'  => $skin_darkest_color,
+                    'rules'  => array(
+                        //prop => selectors
+                        'color'  => array(
+                            '.pagination',
+                            '.btn-skin-darkest:hover',
+                            '.btn-skin-darkest.inverted',
+                            'a:hover',
+                            'a:focus',
+                            'a:active',
+                            '.entry-meta a:not(.btn):hover',
+                            '.grid-container__classic .post-type__icon .icn-format',
+                            '[class*="grid-container__"] .entry-title a:hover',
+                            '.widget-area a:not(.btn):hover',
+                        ),
+                        'border-color' => array(
+                            '.btn-skin-darkest',
+                            '.btn-skin-darkest.inverted',
+                            'input[type=submit]',
+                            '.btn-skin-darkest:hover',
+                            '.btn-skin-darkest.inverted:hover',
+                            'input[type=submit]:hover',
+                        ),
+                        'background-color' => array(
+                            '.btn-skin-darkest',
+                            '.btn-skin-darkest.inverted:hover',
+                            'input[type=submit]',
+                            '.widget-area .widget:not(.widget_shopping_cart) a:not(.btn):before',
+                        )
+                    )
+                ),
+
+                'skin_darkest_color_shade_high' => array(
+                    'color'  => czr_fn_hex2rgba($skin_darkest_color, 0.2, $array=false, $make_prop_value=true),
+                    'rules'  => array(
+                        //prop => selectors
+                        'background-color' => array(
+                            '.btn-skin-darkest-shaded:hover',
+                            '.btn-skin-darkest-shaded.inverted',
+                            '.slider-nav .slider-control',
+                            '.mfp-gallery .slider-control',
+                            '.tc-gallery-nav .slider-control',
+                        )
+                    )
+                ),
+
+                'skin_darkest_color_shade_low' => array(
+                    'color'  => czr_fn_hex2rgba($skin_darkest_color, 0.7, $array=false, $make_prop_value=true),
+                    'rules'  => array(
+                        //prop => selectors
+                        'background-color' => array(
+                            '.btn-skin-darkest-shaded',
+                            '.btn-skin-darkest-shaded.inverted:hover',
+                            '.slider-nav .slider-control:hover',
+                            '.mfp-gallery .slider-control:hover',
+                            '.tc-gallery-nav .slider-control:hover',
+                        )
+                    )
+                )
             );
 
-            $_skin_color_color_prop_selectors = implode( ",{$glue}", apply_filters( 'czr_dynamic_skin_color_color_prop_selectors', $_skin_color_color_prop_selectors ) );
+            //these break the style, let's separate them from the others;
+            $styles[]  = '::-moz-selection { background-color:'. $skin_color .'}';
+            $styles[]  = '::selection { background-color:'. $skin_color .'}';
 
-            $styles[] = $_skin_color_color_prop_selectors ? $_skin_color_color_prop_selectors . '{ color: '. $skin_color .' }' : '';
+            foreach ( $skinner as $skin_color => $params ) {
+                foreach ( $params['rules'] as $prop => $selectors ) {
+                    $_selectors = implode( ",{$glue}",
+                        apply_filters( "czr_dynamic_{$skin_color}_{$prop}_prop_selectors", $selectors ) );
 
-            /* Skin color => border-color Property */
-            $_skin_color_border_color_prop_selectors = array(
-                '.btn-skin',
-                '.btn-skin:hover',
-            );
+                    if ( $_selectors ) {
+                        $styles[] = sprintf( '%1$s{%2$s:%3$s}',
+                                                $_selectors,
+                                                $prop, //property
+                                                $params['color'] //color
+                                    );
+                    }
 
-            $_skin_color_border_color_prop_selectors = implode( ",{$glue}", apply_filters( 'czr_dynamic_skin_color_border_color_prop_selectors', $_skin_color_border_color_prop_selectors ) );
+                }
+            }
 
-            $styles[] = $_skin_color_border_color_prop_selectors ? $_skin_color_border_color_prop_selectors . '{ border-color: '. $skin_color .' }' : '';
+            // $styles[]  = '::selection, ::moz-selection { background-color: '. $skin_color .' }';
 
+            // //Skin color => color Property
+            // $_skin_color_color_prop_selectors = array(
+            //     'a',
+            //     '.btn-skin:hover',
+            //     '.btn-skin.inverted',
+            //     '.post-type__icon:hover .icn-format',
+            //     '[class*="grid-container__"] .hover .entry-header .entry-title a'
+            // );
 
-            /* Skin color => background-color Property */
-            $_skin_color_background_color_prop_selectors = array(
-                '.btn-skin',
-                '.btn-skin.inverted:hover'
-            );
+            // $_skin_color_color_prop_selectors = implode( ",{$glue}", apply_filters( 'czr_dynamic_skin_color_color_prop_selectors', $_skin_color_color_prop_selectors ) );
 
-            $_skin_color_background_color_prop_selectors = implode( ",{$glue}", apply_filters( 'czr_dynamic_skin_color_background_color_prop_selectors', $_skin_color_background_color_prop_selectors ) );
+            // $styles[] = $_skin_color_color_prop_selectors ? $_skin_color_color_prop_selectors . '{ color: '. $skin_color .' }' : '';
 
-            $styles[] = $_skin_color_background_color_prop_selectors ? $_skin_color_background_color_prop_selectors . '{ background-color: '. $skin_color .' }' : '';
+            // // Skin color => border-color Property
+            // $_skin_color_border_color_prop_selectors = array(
+            //     '.btn-skin',
+            //     '.btn-skin:hover',
+            // );
 
+            // $_skin_color_border_color_prop_selectors = implode( ",{$glue}", apply_filters( 'czr_dynamic_skin_color_border_color_prop_selectors', $_skin_color_border_color_prop_selectors ) );
 
-            /* Skin darkest color => color Property */
-            $_skin_darkest_color_color_prop_selectors = array(
-                '.pagination',
-                '.btn-skin-darkest:hover',
-                '.btn-skin-darkest.inverted',
-                'a:hover',
-                'a:focus',
-                'a:active',
-                '.entry-meta a:not(.btn):hover',
-                '.grid-container__classic .post-type__icon .icn-format',
-                '[class*="grid-container__"] .entry-header .entry-title a:hover',
-                '.widget-area a:not(.btn):hover',
-
-            );
-
-            $_skin_darkest_color_color_prop_selectors = implode( ",{$glue}", apply_filters( 'czr_dynamic_skin_darkest_color_color_prop_selectors', $_skin_darkest_color_color_prop_selectors ) );
-
-            $styles[] = $_skin_darkest_color_color_prop_selectors ? $_skin_darkest_color_color_prop_selectors . '{ color: '. $skin_darkest_color .'}' : '';
-
-
-            /* Skin darkest color => border-color Property */
-            $_skin_darkest_color_border_color_prop_selectors = array(
-                '.btn-skin-darkest',
-                '.btn-skin-darkest.inverted',
-                'input[type=submit]',
-                '.btn-skin-darkest:hover',
-                '.btn-skin-darkest.inverted:hover',
-                'input[type=submit]:hover',
-            );
-
-            $_skin_darkest_color_border_color_prop_selectors = implode( ",{$glue}", apply_filters( 'czr_dynamic_skin_darkest_color_border_color_prop_selectors', $_skin_darkest_color_border_color_prop_selectors ) );
-
-            $styles[] = $_skin_darkest_color_border_color_prop_selectors ? $_skin_darkest_color_border_color_prop_selectors . '{ border-color: '. $skin_darkest_color .' }' : '';
-
-            // end computing
-            if ( empty ( $styles ) )
-              return;
+            // $styles[] = $_skin_color_border_color_prop_selectors ? $_skin_color_border_color_prop_selectors . '{ border-color: '. $skin_color .' }' : '';
 
 
-            /* Skin darkest color => background-color Property */
-            $_skin_darkest_color_background_color_prop_selectors = array(
-                '.btn-skin-darkest',
-                '.btn-skin-darkest.inverted:hover',
-                'input[type=submit]',
-                '.widget-area .widget:not(.widget_shopping_cart) a:not(.btn):before',
-            );
+            // // Skin color => background-color Property
+            // $_skin_color_background_color_prop_selectors = array(
+            //     '.btn-skin',
+            //     '.btn-skin.inverted:hover'
+            // );
 
-            $_skin_darkest_color_background_color_prop_selectors = implode( ",{$glue}", apply_filters( 'czr_dynamic_skin_darkest_color_background_color_prop_selectors', $_skin_darkest_color_background_color_prop_selectors ) );
+            // $_skin_color_background_color_prop_selectors = implode( ",{$glue}", apply_filters( 'czr_dynamic_skin_color_background_color_prop_selectors', $_skin_color_background_color_prop_selectors ) );
 
-            $styles[] = $_skin_darkest_color_background_color_prop_selectors ? $_skin_darkest_color_background_color_prop_selectors . '{ background-color: '. $skin_darkest_color .' }' : '';
+            // $styles[] = $_skin_color_background_color_prop_selectors ? $_skin_color_background_color_prop_selectors . '{ background-color: '. $skin_color .' }' : '';
+
+
+            // // Skin darkest color => color Property
+            // $_skin_darkest_color_color_prop_selectors = array(
+            //     '.pagination',
+            //     '.btn-skin-darkest:hover',
+            //     '.btn-skin-darkest.inverted',
+            //     'a:hover',
+            //     'a:focus',
+            //     'a:active',
+            //     '.entry-meta a:not(.btn):hover',
+            //     '.grid-container__classic .post-type__icon .icn-format',
+            //     '[class*="grid-container__"] .entry-header .entry-title a:hover',
+            //     '.widget-area a:not(.btn):hover',
+
+            // );
+
+            // $_skin_darkest_color_color_prop_selectors = implode( ",{$glue}", apply_filters( 'czr_dynamic_skin_darkest_color_color_prop_selectors', $_skin_darkest_color_color_prop_selectors ) );
+
+            // $styles[] = $_skin_darkest_color_color_prop_selectors ? $_skin_darkest_color_color_prop_selectors . '{ color: '. $skin_darkest_color .'}' : '';
+
+
+            // // Skin darkest color => border-color Property
+            // $_skin_darkest_color_border_color_prop_selectors = array(
+            //     '.btn-skin-darkest',
+            //     '.btn-skin-darkest.inverted',
+            //     'input[type=submit]',
+            //     '.btn-skin-darkest:hover',
+            //     '.btn-skin-darkest.inverted:hover',
+            //     'input[type=submit]:hover',
+            // );
+
+            // $_skin_darkest_color_border_color_prop_selectors = implode( ",{$glue}", apply_filters( 'czr_dynamic_skin_darkest_color_border_color_prop_selectors', $_skin_darkest_color_border_color_prop_selectors ) );
+
+            // $styles[] = $_skin_darkest_color_border_color_prop_selectors ? $_skin_darkest_color_border_color_prop_selectors . '{ border-color: '. $skin_darkest_color .' }' : '';
+
+
+            // // Skin darkest color => background-color Property
+            // $_skin_darkest_color_background_color_prop_selectors = array(
+            //     '.btn-skin-darkest',
+            //     '.btn-skin-darkest.inverted:hover',
+            //     'input[type=submit]',
+            //     '.widget-area .widget:not(.widget_shopping_cart) a:not(.btn):before',
+            // );
+
+            // $_skin_darkest_color_background_color_prop_selectors = implode( ",{$glue}", apply_filters( 'czr_dynamic_skin_darkest_color_background_color_prop_selectors', $_skin_darkest_color_background_color_prop_selectors ) );
+
+            // $styles[] = $_skin_darkest_color_background_color_prop_selectors ? $_skin_darkest_color_background_color_prop_selectors . '{ background-color: '. $skin_darkest_color .' }' : '';
 
             // end computing
             if ( empty ( $styles ) )
--- core/utils/class-fire-utils_colors.php
+++ core/utils/class-fire-utils_colors.php
@@ -1,51 +1,212 @@
 <?php
-/* 
-https://gist.github.com/jegtnes/5720178
-*/
 if ( ! function_exists( 'czr_fn_sass_lighten' ) ) {
-    function czr_fn_sass_darken($hex, $percent) {
-        preg_match('/^#?([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/i', $hex, $primary_colors);
-            str_replace('%', '', $percent);
-            $color = "#";
-            for($i = 1; $i <= 3; $i++) {
-                    $primary_colors[$i] = hexdec($primary_colors[$i]);
-                    $primary_colors[$i] = round($primary_colors[$i] * (100-($percent*2))/100);
-                    $color .= str_pad(dechex($primary_colors[$i]), 2, '0', STR_PAD_LEFT);
-            }
-            return $color;
+    function czr_fn_sass_darken($hex, $percent, $make_prop_value = true) {
+        return $make_prop_value ? '#' . czr_fn_darken( czr_fn_rgb2hsl(czr_fn_hex2rgb($hex, true ), true ), $percent ) :
+        czr_fn_darken( czr_fn_rgb2hsl(czr_fn_hex2rgb($hex, true ), true ), $percent );
     }
 }
 
 if ( ! function_exists( 'czr_fn_sass_lighten' ) ) {
-    function czr_fn_sass_lighten($hex, $percent) {
-        preg_match('/^#?([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/i', $hex, $primary_colors);
-        str_replace('%', '', $percent);
-        $color = "#";
-        for($i = 1; $i <= 3; $i++) {
-            $primary_colors[$i] = hexdec($primary_colors[$i]);
-            $primary_colors[$i] = round($primary_colors[$i] * (100+($percent*2))/100);
-            $color .= str_pad(dechex($primary_colors[$i]), 2, '0', STR_PAD_LEFT);
-        }
-        return $color;
+    function czr_fn_sass_lighten($hex, $percent, $make_prop_value = true) {
+        return $make_prop_value ? '#' . czr_fn_lighten( czr_fn_rgb2hsl(czr_fn_hex2rgb($hex, true ), true ), $percent ) :
+        czr_fn_lighten( czr_fn_rgb2hsl(czr_fn_hex2rgb($hex, true ), true ), $percent );
     }
 }
+
 
 /*  Convert hexadecimal to rgb
 /* ------------------------------------ */
 if ( ! function_exists( 'czr_fn_hex2rgb' ) ) {
-    function czr_fn_hex2rgb( $hex, $array=false ) {
+    function czr_fn_hex2rgb( $hex, $array = false, $make_prop_value = false ) {
         $hex = str_replace("#", "", $hex);
+
         if ( strlen($hex) == 3 ) {
             $r = hexdec(substr($hex,0,1).substr($hex,0,1));
             $g = hexdec(substr($hex,1,1).substr($hex,1,1));
             $b = hexdec(substr($hex,2,1).substr($hex,2,1));
-        } else {
+        }
+        else {
             $r = hexdec(substr($hex,0,2));
             $g = hexdec(substr($hex,2,2));
             $b = hexdec(substr($hex,4,2));
         }
+
         $rgb = array( $r, $g, $b );
-        if ( !$array ) { $rgb = implode(",", $rgb); }
+
+        if ( !$array ) {
+            $rgb = implode(",", $rgb);
+            $rgb = $make_prop_value ? "rgb($rgb)" : $rgb;
+        }
+
         return $rgb;
   }
+}
+
+/*  Convert hexadecimal to rgba
+/* ------------------------------------ */
+if ( ! function_exists( 'czr_fn_hex2rgba' ) ) {
+    function czr_fn_hex2rgba( $hex, $alpha = 0.7, $array = false, $make_prop_value = false ) {
+        $rgb = $rgba = czr_fn_hex2rgb($hex, $_array = true);
+        $rgba[]      = $alpha;
+
+        if ( !$array ) {
+            $rgba = implode(",", $rgba);
+            $rgba = $make_prop_value ? "rgba($rgba)" : $rgba;
+        }
+
+        return $rgba;
+  }
+}
+
+/*  Convert  rgb to hexadecimal
+/* ------------------------------------ */
+if ( ! function_exists( 'czr_fn_rgb2hex' ) ) {
+    function czr_fn_rgb2hex( $rgb, $make_prop_value = false ) {
+
+        $rgb = is_array( $rgb ) ? $rgb : explode(',', $rgb);
+        $rgb = is_array( $rgb) ? $rgb : array( $rgb );
+        $rgb = count($rgb) < 3 ? array_pad( $rgb, 3, 255) : $rgb;
+
+        // Convert RGB to HEX
+        $hex[0] = str_pad(dechex($rgb[0]), 2, '0', STR_PAD_LEFT);
+        $hex[1] = str_pad(dechex($rgb[1]), 2, '0', STR_PAD_LEFT);
+        $hex[2] = str_pad(dechex($rgb[2]), 2, '0', STR_PAD_LEFT);
+
+        $hex = implode( '', $hex );
+
+        return $make_prop_value ? $hex : "#{$hex}";
+    }
+}
+
+/*
+* heavily based on
+* phpColors
+*/
+
+/*  Convert rgb to hsl
+/* ------------------------------------ */
+if ( ! function_exists( 'czr_fn_rgb2hsl' ) ) {
+    function czr_fn_rgb2hsl( $rgb, $array=false, $make_prop_value = false ) {
+        $HSL = array();
+
+        $R = ($rgb[0] / 255);
+        $G = ($rgb[1] / 255);
+        $B = ($rgb[2] / 255);
+
+        $min = min($R, $G, $B);
+        $max = max($R, $G, $B);
+        $del_max = $max - $min;
+
+        $L = ($max + $min)/2;
+
+        if ($del_max == 0)
+        {
+            $H = 0;
+            $S = 0;
+        }
+        else
+        {
+            if ( $L < 0.5 ) $S = $del_max / ( $max + $min );
+            else            $S = $del_max / ( 2 - $max - $min );
+            $del_R = ( ( ( $max - $R ) / 6 ) + ( $del_max / 2 ) ) / $del_max;
+            $del_G = ( ( ( $max - $G ) / 6 ) + ( $del_max / 2 ) ) / $del_max;
+            $del_B = ( ( ( $max - $B ) / 6 ) + ( $del_max / 2 ) ) / $del_max;
+            if      ($R == $max) $H = $del_B - $del_G;
+            else if ($G == $max) $H = ( 1 / 3 ) + $del_R - $del_B;
+            else if ($B == $max) $H = ( 2 / 3 ) + $del_G - $del_R;
+            if ($H<0) $H++;
+            if ($H>1) $H--;
+        }
+        $HSL['H'] = ($H*360);
+        $HSL['S'] = $S;
+        $HSL['L'] = $L;
+
+        if ( !$array ) {
+            $HSL = implode(",", $HSL);
+            $HSL = $make_prop_value ? "rgba($rgba)" : $HSL;
+        }
+
+        return $HSL;
+    }
+}
+
+/* Convert hsl to hex
+/* ------------------------------------ */
+if ( ! function_exists( 'czr_fn_hsl2hex' ) ) {
+    function czr_fn_hsl2hex( $hsl = array() ){
+
+        list($H,$S,$L) = array( $hsl['H']/360,$hsl['S'],$hsl['L'] );
+        if( $S == 0 ) {
+            $r = $L * 255;
+            $g = $L * 255;
+            $b = $L * 255;
+        } else {
+            if($L<0.5) {
+                $var_2 = $L*(1+$S);
+            } else {
+                $var_2 = ($L+$S) - ($S*$L);
+            }
+            $var_1 = 2 * $L - $var_2;
+            $r = round(255 * czr_fn_hue2rgb( $var_1, $var_2, $H + (1/3) ));
+            $g = round(255 * czr_fn_hue2rgb( $var_1, $var_2, $H ));
+            $b = round(255 * czr_fn_hue2rgb( $var_1, $var_2, $H - (1/3) ));
+        }
+        // Convert to hex
+        $r = dechex($r);
+        $g = dechex($g);
+        $b = dechex($b);
+
+        // Make sure we get 2 digits for decimals
+        $r = (strlen("".$r)===1) ? "0".$r:$r;
+        $g = (strlen("".$g)===1) ? "0".$g:$g;
+        $b = (strlen("".$b)===1) ? "0".$b:$b;
+        return $r.$g.$b;
+
+    }
+}
+
+/* Convert hue to rgb
+/* ------------------------------------ */
+if ( ! function_exists( 'czr_fn_hue2rgb' ) ) {
+    function czr_fn_hue2rgb( $v1,$v2,$vH ) {
+        if( $vH < 0 ) {
+            $vH += 1;
+        }
+        if( $vH > 1 ) {
+            $vH -= 1;
+        }
+        if( (6*$vH) < 1 ) {
+               return ($v1 + ($v2 - $v1) * 6 * $vH);
+        }
+        if( (2*$vH) < 1 ) {
+            return $v2;
+        }
+        if( (3*$vH) < 2 ) {
+            return ($v1 + ($v2-$v1) * ( (2/3)-$vH ) * 6);
+        }
+        return $v1;
+    }
+}
+
+
+/* Darken hsl
+/* ------------------------------------ */
+if ( ! function_exists( 'czr_fn_darken' ) ) {
+    function czr_fn_darken( $hsl, $amount ){
+        $hsl['L'] = ($hsl['L'] * 100) - $amount;
+        $hsl['L'] = ($hsl['L'] < 0) ? 0:$hsl['L']/100;
+
+        return czr_fn_hsl2hex($hsl);
+    }
+}
+
+/* Lighten hsl
+/* ------------------------------------ */
+if ( ! function_exists( 'czr_fn_lighten' ) ) {
+    function czr_fn_lighten( $hsl, $amount ) {
+        $hsl['L'] = ($hsl['L'] * 100) + $amount;
+        $hsl['L'] = ($hsl['L'] > 100) ? 1:$hsl['L']/100;
+
+        return czr_fn_hsl2hex($hsl);
+    }
 }
