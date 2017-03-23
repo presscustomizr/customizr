11d220d4128cb5d011d16c195594d46f8fe75d81
eri-trabiccolo <raliberti84@gmail.com>
2017-03-23 15:47:19 +0100

    Imp: minor style fixes

--- templates/modules/post_action_button.php
+++ templates/modules/post_action_button.php
@@ -4,6 +4,6 @@
  *
  */
 ?>
-<div class="post-action btn btn-skin-darkest-shaded <?php czr_fn_echo( 'element_class' ) ?>" <?php czr_fn_echo('element_attributes') ?>>
+<div class="post-action btn btn-skin-darkest-shaded inverted" <?php czr_fn_echo('element_attributes') ?>>
   <a href="<?php echo esc_url( czr_fn_get( 'post_action_link' ) ) ?>" class="<?php czr_fn_echo( 'post_action_link_class' ) ?> icn-expand"></a>
 </div>
