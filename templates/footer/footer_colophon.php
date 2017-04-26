5d78502da2e4b29b666c9196a8fa6748f20e4550
eri-trabiccolo <raliberti84@gmail.com>
2017-04-26 16:38:41 +0200

    Fix: add colophon CSS class to the footer colophon

--- templates/footer/footer_colophon.php
+++ templates/footer/footer_colophon.php
@@ -6,7 +6,7 @@
  * @since Customizr 3.5.0
  */
 ?>
-<div id="colophon" class="colophon__row row" <?php czr_fn_echo('element_attributes') ?>>
+<div id="colophon" class="colophon colophon__row row" <?php czr_fn_echo('element_attributes') ?>>
   <div class="col-sm-6">
     <?php if ( czr_fn_has( 'footer_credits' ) ) czr_fn_render_template( 'footer/footer_credits' ) ?>
   </div>
