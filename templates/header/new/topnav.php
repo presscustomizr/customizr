679bfa45f24043735434a22f48188abde6f14e2c
eri-trabiccolo <raliberti84@gmail.com>
2017-05-03 18:56:47 +0200

    Imp: initial topnav styling

---
+++ templates/header/new/topnav.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying the topnav
+*/
+?>
+<div class="secondary-navbar__wrapper row <?php czr_fn_echo('element_class') ?> flex-lg-nowrap justify-content-lg-between align-items-lg-start" <?php czr_fn_echo('element_attributes') ?>>
+  <?php if ( czr_fn_has( 'secondary_menu' ) ) :?>
+    <?php if ( czr_fn_get( 'has_mobile_button' ) ) :?>
+      <div class="hamburger-toggler__wrapper col-12 float-left hidden-lg-up">
+        <?php
+          czr_fn_render_template( 'header/menu_button', array(
+            'model_args' => array(
+              'data_attributes' => 'data-toggle="collapse" data-target="#secondary-nav"',
+              'element_class'   => 'float-right'
+            )
+          ));
+        ?>
+      </div>
+    <?php endif ?>
+    <div class="secondary-nav__container">
+      <nav id="secondary-nav" class="secondary-nav__nav float-left <?php czr_fn_echo('nav_class') ?>">
+        <?php
+          czr_fn_render_template( 'header/menu', array(
+            'model_id' => 'secondary_menu',
+            'model_args' => array(
+              'theme_location' => 'secondary',
+              'element_class' => 'secondary-nav__menu-wrapper',
+              'menu_class'    => czr_fn_get('menu_class'),
+              'menu_id'       => 'secondary-menu'
+            )
+          ));
+        ?>
+      </nav>
+    </div>
+  <?php endif ?>
+  <?php if ( czr_fn_has('header_social_block') ) : ?>
+    <div class="secondary-nav__socials social-links hidden-md-down">
+      <?php czr_fn_render_template( 'modules/common/social_block' ) ?>
+    </div>
+  <?php endif ?>
+</div>
