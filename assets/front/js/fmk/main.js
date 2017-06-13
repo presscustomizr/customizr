1f85d45ef8c9e1fab6e823b12d7a1800c05857df
eri-trabiccolo <raliberti84@gmail.com>
2017-06-13 12:21:20 +0200

    Imp: fp and grid images always js centered

--- assets/front/js/fmk/main.js
+++ assets/front/js/fmk/main.js
@@ -1122,9 +1122,9 @@ var czrapp = czrapp || {};
     * @return {void}
     */
     centerImages : function() {
-      //POST CLASSIC GRID IMAGES
+      //Featured pages and classical grid are always centered
       $('.tc-grid-figure, .widget-front .tc-thumbnail').centerImages( {
-        enableCentering : czrapp.localized.centerAllImg,
+        enableCentering : 1,
         oncustom : ['smartload', 'refresh-height', 'simple_load'],
         zeroTopAdjust: 0,
         enableGoldenRatio : false,
@@ -1256,9 +1256,6 @@ var czrapp = czrapp || {};
                   /* Allow parallax */
                   czrapp.$_body.on( 'czr-flickity-ready.flickity', '.czr-parallax-slider', self._parallax );
 
-                  /* Enable page dots on fly (for the main slider only, for the moment, consider to make it dependend to data-flickity-dots)*/
-                  czrapp.$_body.on( 'czr-flickity-ready.flickity', '[id^="customizr-slider-main"] .carousel-inner', self._slider_dots );
-
                   /* Fire fittext */
                   czrapp.$_body.on( 'czr-flickity-ready.flickity', '[id^="customizr-slider-main"] .carousel-inner', function() {
                     $(this).find( '.carousel-caption .czrs-title' ).czrFitText(
@@ -1329,23 +1326,35 @@ var czrapp = czrapp || {};
                       dragThreshold: 10
                   });
 
-                  $('.carousel-inner', '[id^="customizr-slider-main"]').flickity({
-                      prevNextButtons: false,
-                      pageDots: false,
 
-                      wrapAround: true,
-                      imagesLoaded: true,
-                      //lazyLoad ?
+                  var $_main_slider = $('.carousel-inner', '[id^="customizr-slider-main"]');
+                  if ( $_main_slider.length > 0 ) {
+                        //number of slides
+                        //check if has one single slide
+                        //in this case we don't allow pageDots, nor the slide will be draggable
+                        var _is_single_slide = 1 == $_main_slider.find( '.carousel-cell' ).length;
 
-                      setGallerySize: false,
-                      cellSelector: '.carousel-cell',
+                        $_main_slider.flickity({
+                            prevNextButtons: false,
+                            pageDots: !_is_single_slide,
+                            draggable: !_is_single_slide,
 
-                      dragThreshold: 10,
+                            wrapAround: true,
 
-                      autoPlay: true, // {Number in milliseconds }
+                            imagesLoaded: true,
 
-                      accessibility: false,
-                  });
+                            //lazyLoad ?
+
+                            setGallerySize: false,
+                            cellSelector: '.carousel-cell',
+
+                            dragThreshold: 10,
+
+                            autoPlay: true, // {Number in milliseconds }
+
+                            accessibility: false,
+                        });
+                  }
             },
 
             centerMainSlider : function() {
@@ -1379,6 +1388,7 @@ var czrapp = czrapp || {};
 
                   } , 50);
             },
+
             /*
             * carousel parallax on flickity ready
             * we parallax only the flickity-viewport, so that we don't parallax the carouasel-dots
@@ -1396,19 +1406,6 @@ var czrapp = czrapp || {};
                         );
 
                   $_parallax_carousel.children('.flickity-viewport').czrParallax(_parallax_data);
-
-            },
-
-
-
-            //Enable page dots on fly
-            _slider_dots : function( evt, _flickity ) {
-
-                  if ( $(evt.target).find('.carousel-cell').length > 1 ) {
-                    _flickity.options.pageDots = true;
-                    _flickity._createPageDots();
-                    _flickity.activatePageDots();
-                  }
 
             },
 
