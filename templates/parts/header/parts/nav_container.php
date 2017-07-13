cfd31e3025cb1d9b92a7e5c6d4aa750eacc97822
nikeo <nicolas@presscustomizr.com>
2017-07-13 15:48:56 +0200

    changed the scss file organization

--- templates/parts/header/parts/nav_container.php
+++ templates/parts/header/parts/nav_container.php
@@ -5,17 +5,15 @@
 ?>
 <div class="primary-nav__container <?php czr_fn_echo('element_class') ?>" <?php czr_fn_echo('element_attributes') ?>>
   <div class="primary-nav__wrapper navbar-toggleable-md flex-lg-row justify-content-end">
-     <nav class="collapse navbar-collapse primary-nav__nav col-lg" id="primary-nav">
-      <?php
-
-        if ( czr_fn_is_registered_or_possible('navbar_primary_menu') || czr_fn_is_registered_or_possible( 'navbar_secondary_menu' ) ) {
-          czr_fn_render_template( 'header/parts/menu', array(
-            'model_id'   =>  czr_fn_is_registered_or_possible('navbar_primary_menu') ? 'navbar_primary_menu' : 'navbar_secondary_menu',
-          ) );
-        };
-
-      ?>
-    </nav>
+     <?php if ( czr_fn_is_registered_or_possible('navbar_primary_menu') || czr_fn_is_registered_or_possible( 'navbar_secondary_menu' ) ) : ?>
+         <nav class="collapse navbar-collapse primary-nav__nav col-lg" id="primary-nav">
+          <?php
+              czr_fn_render_template( 'header/parts/menu', array(
+                'model_id'   =>  czr_fn_is_registered_or_possible('navbar_primary_menu') ? 'navbar_primary_menu' : 'navbar_secondary_menu',
+              ) );
+          ?>
+        </nav>
+    <?php endif; ?>
     <?php
       if ( czr_fn_is_registered_or_possible( 'primary_nav_utils' ) ) czr_fn_render_template( 'header/parts/primary_nav_utils' )
     ?>
