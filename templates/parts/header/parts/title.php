fa67b2a6d0f2c5925eacab2fb72141458f9629e4
nikeo <nicolas@presscustomizr.com>
2017-07-20 16:51:38 +0200

    fixed : missing css class align-self-start for site title and logo

--- templates/parts/header/parts/title.php
+++ templates/parts/header/parts/title.php
@@ -3,7 +3,7 @@
  * The template for displaying the site title (with its wrapper)
  */
 ?>
-<h1 class="navbar-brand <?php czr_fn_echo( 'element_class' ) ?>" <?php czr_fn_echo('element_attributes') ?>>
+<h1 class="navbar-brand align-self-start <?php czr_fn_echo( 'element_class' ) ?>" <?php czr_fn_echo('element_attributes') ?>>
   <a class="navbar-brand-sitename <?php czr_fn_echo( 'title_class' ) ?>" href="<?php _e( esc_url( home_url( '/' ) ) ) ?>"  title="<?php esc_attr_e( get_bloginfo( 'name' ) ) ?> | <?php esc_attr_e( get_bloginfo( 'description' ) ) ?>">
     <span><?php esc_attr_e( get_bloginfo( 'name' ) ) ?></span>
   </a>
