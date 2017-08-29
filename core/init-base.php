9b9e0805e9b94ae3327f296b21327e3d97fc1e57
Rocco Aliberti <raliberti84@gmail.com>
2017-08-29 19:29:06 +0200

    Fix: classical - do not filter home/blog when grid and infinite enabled

--- core/init-base.php
+++ core/init-base.php
@@ -613,11 +613,16 @@ if ( ! class_exists( 'CZR_BASE' ) ) :
         function czr_fn_filter_home_blog_posts_by_tax( $query ) {
             // when we have to filter?
             // in home and blog page
-            if (
-              ! $query->is_main_query()
+            if ( ! $query->is_main_query()
               || ! ( ( is_home() && 'posts' == get_option('show_on_front') ) || $query->is_posts_page )
             )
               return;
+
+           //temp: do not filter in classic style when classic grid enabled and infinite scroll enabled in home/blog 
+           if ( ! CZR_IS_MODERN_STYLE &&
+              'grid'== esc_attr( czr_fn_opt( 'tc_post_list_grid' ) ) &&
+               class_exists( 'PC_init_infinite' ) && esc_attr( czr_fn_opt( 'tc_infinite_scroll' ) ) && esc_attr( czr_fn_opt( 'tc_infinite_scroll_in_home' ) ) )
+            return;
 
            // categories
            // we have to ignore sticky posts (do not prepend them)
