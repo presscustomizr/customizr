86eb4a2847f3820003632c886babab3525bcf152
Rocco Aliberti <raliberti84@gmail.com>
2017-09-18 13:49:54 +0200

    Fix: ms - correctly horizontal positioning of the primary navbar menu fixes #1175

--- templates/parts/header/parts/nav_container.php
+++ templates/parts/header/parts/nav_container.php
@@ -6,7 +6,7 @@
 <div class="primary-nav__container <?php czr_fn_echo('element_class') ?>" <?php czr_fn_echo('element_attributes') ?>>
   <div class="primary-nav__wrapper flex-lg-row align-items-center justify-content-end">
      <?php if ( czr_fn_is_registered_or_possible( 'navbar_primary_menu' ) || czr_fn_is_registered_or_possible( 'navbar_secondary_menu' ) ) { ?>
-         <nav class="primary-nav__nav col col-auto" id="primary-nav">
+         <nav class="primary-nav__nav col" id="primary-nav">
           <?php
               czr_fn_render_template( 'header/parts/menu', array(
                 'model_id'   =>  czr_fn_is_registered_or_possible( 'navbar_primary_menu' ) ? 'navbar_primary_menu' : 'navbar_secondary_menu',
