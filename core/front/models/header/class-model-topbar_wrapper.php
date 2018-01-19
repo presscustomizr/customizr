ece705945c2cc4aa85830df96c9f4f3d6f9f629d
Rocco Aliberti <raliberti84@gmail.com>
2018-01-19 17:24:44 +0100

    Imp: add contact_info e topbar_wrapper models + adapt logic to compute the topbar_wrapper visibility class in the header model

---
+++ core/front/models/header/class-model-topbar_wrapper.php
@@ -0,0 +1,66 @@
+<?php
+class CZR_topbar_wrapper_model_class extends CZR_Model {
+
+    public $contact_info_class;
+    public $social_block_class;
+
+
+    /**
+    * @override
+    * fired before the model properties are parsed in the constructor
+    *
+    * return model params array()
+    */
+    function czr_fn_extend_params( $model = array() ) {
+        /*
+        * contact info class
+        * 3 cases:
+        * 1) do not display both in mobile and desktop
+        * 2) display in mobile and not in desktop
+        * 3) display in desktop and not in mobile
+        * 4) display in mobile and desktop
+        */
+        $_contact_info_visibility = esc_attr( czr_fn_opt( 'tc_header_show_contact_info' ) );
+
+        switch ( $_contact_info_visibility ) :
+          case 'desktop' : $_contact_info_class = array( 'hidden-md-down' );
+                           break;
+          case 'mobile'  : $_contact_info_class = array( 'hidden-lg-up' );
+                           break;
+          case 'none'    : $_contact_info_class = array( 'hidden' );
+                           break;
+          default        : $_contact_info_class = array();
+        endswitch;
+
+
+        /*
+        * social block class
+        * 3 cases:
+        * 1) do not display both in mobile and desktop
+        * 2) display in mobile and not in desktop
+        * 3) display in desktop and not in mobile
+        * 4) display in mobile and desktop
+        */
+        $_socials_visibility = esc_attr( czr_fn_opt( 'tc_header_show_socials' ) );
+
+        switch ( $_socials_visibility ) :
+          case 'desktop' : $_social_block_class = array( 'hidden-md-down' );
+                           break;
+          case 'mobile'  : $_social_block_class = array( 'hidden-lg-up' );
+                           break;
+          case 'none'    : $_social_block_class = array( 'hidden' );
+                           break;
+          default        : $_social_block_class = array();
+        endswitch;
+
+
+        $model = array_merge( $model, array(
+            'contact_info_class' => $_contact_info_class,
+            'social_block_class' => $_social_block_class
+        ) );
+
+
+        return parent::czr_fn_extend_params( $model );
+    }
+
+}
