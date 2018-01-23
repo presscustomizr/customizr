7a82e5dbdd5b7324526cfbb13f33c4f60572301c
Rocco Aliberti <raliberti84@gmail.com>
2018-01-23 18:37:03 +0100

    Imp: modern - update the CSS (static and dynamic) according to the new added feature

--- core/class-fire-resources_styles.php
+++ core/class-fire-resources_styles.php
@@ -179,6 +179,9 @@ if ( ! class_exists( 'CZR_resources_styles' ) ) :
                                        '.nav__utils a',
                                        '.nav__utils a:focus',
                                        '.nav__utils a:active',
+                                       '.header-contact__info a',
+                                       '.header-contact__info a:focus',
+                                       '.header-contact__info a:active',
                                        '.czr-overlay a:hover',
                                        '.dropdown-menu',
                                        '.tc-header .navbar-brand-sitename',
@@ -235,6 +238,7 @@ if ( ! class_exists( 'CZR_resources_styles' ) ) :
                                          '.tc-header .socials a:hover',
                                          '.nav__utils a:hover',
                                          '.nav__utils a.current-active',
+                                         '.header-contact__info a:hover',
                                          '.tc-header .czr-form .form-group.in-focus label',
                                          '.czr-overlay .czr-form .form-group.in-focus label'
                                  ),
