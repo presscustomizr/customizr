36975deb0895c6b305963428c0aa0559806209f0
Rocco Aliberti <raliberti84@gmail.com>
2018-01-23 14:37:12 +0100

    Fix: modern - improve underline link hover effect elements targeting mostly to avoid plugin conflicts fixes https://github.com/presscustomizr/customizr/issues/1399 and https://github.com/presscustomizr/customizr/issues/1202

--- core/class-fire-resources_styles.php
+++ core/class-fire-resources_styles.php
@@ -212,7 +212,6 @@ if ( ! class_exists( 'CZR_resources_styles' ) ) :
                                  'background-color' => array(
                                        '.ham__toggler-span-wrapper .line',
                                        '[class*=nav__menu] .nav__title::before',
-                                       '.tc-header .navbar-brand-sitename.czr-underline span::after'
                                  )
                            )
                      ),
@@ -558,7 +557,7 @@ if ( ! class_exists( 'CZR_resources_styles' ) ) :
                                        '.grid-container__classic .post-type__icon:hover',
                                        'button[type=submit]',
                                        'input[type=submit]',
-                                       '.widget-area .widget:not(.widget_shopping_cart) a:not(.btn):before',
+                                       '.widget-area .widget a:not(.btn):before',
                                        "[class*='grid-container__'] .hover .entry-title a::after",
                                        'a.czr-format-link::before',
                                        '.comment-author a::before',
