presscustomizr/customizr

commit 2c41e21ce1940056b842ecbe36082e1f00d7bb42

Imp: classical - co-authors plus plugin integration

inc/_dev/parts/class-content-post.php
@@ -121,30 +121,48 @@ if ( ! class_exists( 'CZR_post' ) ) :
       //check conditional tags : we want to show single post or single custom post types
       if ( ! $this -> czr_fn_single_post_display_controller() || ! apply_filters( 'tc_show_single_post_footer', true ) )
           return;
+
       //@todo check if some conditions below not redundant?
-      if ( ! is_singular() || ! get_the_author_meta( 'description' ) || ! apply_filters( 'tc_show_author_metas_in_post', true ) || ! esc_attr( czr_fn_opt( 'tc_show_author_info' ) ) )
+      if ( ! is_singular() || ! apply_filters( 'tc_show_author_metas_in_post', true ) || ! esc_attr( czr_fn_opt( 'tc_show_author_info' ) ) ) {
         return;
+      }
 
-      $html = sprintf('<footer class="entry-meta">%1$s<div class="author-info"><div class="%2$s">%3$s %4$s</div></div></footer>',
-                   '<hr class="featurette-divider">',
 
-                  apply_filters( 'tc_author_meta_wrapper_class', 'row-fluid' ),
 
-                  sprintf('<div class="%1$s">%2$s</div>',
-                          apply_filters( 'tc_author_meta_avatar_class', 'comment-avatar author-avatar span2'),
-                          get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'tc_author_bio_avatar_size' , 100 ) )
-                    ),
+      $author_id       = get_the_author_meta( 'ID' );
+      $authors_id      = apply_filters( 'tc_post_author_id', array( $author_id ) );
+      $authors_id      = is_array( $authors_id ) ? $authors_id : array( $author_id );
+      //author candidates must have a bio to be displayed
+      $authors_id      = array_filter( $authors_id, 'czr_fn_get_author_meta_description_by_id' );
 
-                  sprintf('<div class="%1$s"><h3>%2$s</h3><p>%3$s</p><div class="author-link">%4$s</div></div>',
-                          apply_filters( 'tc_author_meta_content_class', 'author-description span10' ),
-                          sprintf( __( 'About %s' , 'customizr' ), get_the_author() ),
-                          get_the_author_meta( 'description' ),
-                          sprintf( '<a href="%1$s" rel="author">%2$s</a>',
-                            esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-                            sprintf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>' , 'customizr' ), get_the_author() )
-                          )
-                    )
-      );//end sprintf
+      if ( empty( $authors_id ) ) {
+        return;
+      }
+
+      $html            = '<footer class="entry-meta"><hr class="featurette-divider"><div class="author-info-wrapper">';
+
+      foreach ( $authors_id as $author_id ) {
+        $author_name   = get_the_author_meta( 'display_name', $author_id );
+        $html         .= sprintf('<div class="author-info"><div class="%1$s">%2$s %3$s</div></div>',
+                            apply_filters( 'tc_author_meta_wrapper_class', 'row-fluid' ),
+
+                            sprintf('<div class="%1$s">%2$s</div>',
+                                    apply_filters( 'tc_author_meta_avatar_class', 'comment-avatar author-avatar span2'),
+                                    get_avatar( get_the_author_meta( 'user_email', $author_id ), apply_filters( 'tc_author_bio_avatar_size' , 100 ) )
+                            ),
+                            sprintf('<div class="%1$s"><h3>%2$s</h3><p>%3$s</p><div class="author-link">%4$s</div></div>',
+                                    apply_filters( 'tc_author_meta_content_class', 'author-description span10' ),
+                                    sprintf( __( 'About %s' , 'customizr' ),  ),
+                                    get_the_author_meta( 'description', $author_id ),
+                                    sprintf( '<a href="%1$s" rel="author">%2$s</a>',
+                                      esc_url( get_author_posts_url( $author_id ) ),
+                                      sprintf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>' , 'customizr' ), $author_name )
+                                    )
+                            )
+        );//end sprintf
+      }//end for
+      $html .= '</div></footer>';
+
       echo apply_filters( 'tc_post_footer', $html );
     }
 

inc/_dev/class-fire-plugins_compat.php
@@ -58,6 +58,7 @@ if ( ! class_exists( 'CZR_plugins_compat' ) ) :
       add_theme_support( 'uris' );
       add_theme_support( 'tc-unlimited-featured-pages' );
       add_theme_support( 'learnpress' );
+      add_theme_support( 'coauthors' );
     }
 
 
@@ -141,6 +142,10 @@ if ( ! class_exists( 'CZR_plugins_compat' ) ) :
       /* LearnPress  */
       if ( current_theme_supports( 'learnpress' ) && $this -> czr_fn_is_plugin_active('learnpress/learnpress.php') )
         $this -> czr_fn_set_lp_compat();
+
+      /* Coauthors-Plus */
+      if ( current_theme_supports( 'coauthors' ) && $this -> czr_fn_is_plugin_active('co-authors-plus/co-authors-plus.php') )
+        $this -> czr_fn_set_coauthors_compat();
     }//end of plugin compatibility function
 
 
@@ -1305,6 +1310,28 @@ if ( ! class_exists( 'CZR_plugins_compat' ) ) :
       //disable lp breadcrumb, we'll use our own
       remove_action( 'learn_press_before_main_content', 'learn_press_breadcrumb' );
     }//end lp compat
+
+
+
+    /* same in czr classic */
+    /*
+    * Coauthors-Plus plugin compat hooks
+    */
+    private function czr_fn_set_coauthors_compat() {
+      if ( !function_exists( 'coauthors_ids' )  ) {
+        return;
+      }
+
+      add_filter( 'tc_post_author_id', 'tc_coauthors_post_author_ids' );
+      if ( !function_exists( 'tc_coauthors_post_author_ids' ) ) {
+        function tc_coauthors_post_author_ids() {
+          $author_ids = coauthors_ids( $between = ',', $betweenLast = ',', $before = null, $after = null, $echo = false );
+          return explode(  ',' , $author_ids );
+        }
+      }
+    }
+
+
 
     /* same in czr modern */
     /**

inc/_dev/parts/class-content-post_metas.php
@@ -512,14 +512,38 @@ if ( ! class_exists( 'CZR_post_metas' ) ) :
         * @since Customizr 3.2.6
         */
         private function czr_fn_get_meta_author() {
-            return apply_filters(
-                'tc_author_meta',
-                sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></span>' ,
-                    esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-                    esc_attr( sprintf( __( 'View all posts by %s' , 'customizr' ), get_the_author() ) ),
-                    get_the_author()
-                )
-            );//end filter
+
+            $author_id = null;
+
+            if ( is_single() ) {
+                if ( ! in_the_loop() ) {
+                    global $post;
+                    $author_id = $post->post_author;
+                }
+            }
+
+            $author_id_array = apply_filters( 'tc_post_author_id', array( $author_id ) );
+            $author_id_array = is_array( $author_id_array ) ? $author_id_array : array( $author_id );
+
+            $_html  = '';
+            $_i     = 0;
+
+            foreach ( $author_id_array as $author_id ) {
+                $_i         +=1;
+                $author_name = get_the_author_meta( 'display_name', $author_id );
+
+                if ( ! ( 1 == $_i || count( $author_id ) == $_i ) ) {
+                    $_html  .= ', ';
+                }
+
+                $_html      .= sprintf( '<span class="author vcard author_name"><a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></span>' ,
+                      esc_url( get_author_posts_url( $author_id ) ),
+                      esc_attr( sprintf( __( 'View all posts by %s' , 'customizr' ), $author_name ) ),
+                      $author_name
+                );
+            }
+
+            return apply_filters('tc_author_meta', $_html );
         }
 
 

commit 6e1d8211897bc6b22c3a1d1b5014f352f5710b49

Fix: classical - fix sprintf missing param causing fatal error in single author box typo introduced in https://github.com/presscustomizr/customizr/commit/2c41e21ce1940056b842ecbe36082e1f00d7bb42

inc/_dev/parts/class-content-post.php
@@ -145,7 +145,7 @@ if ( ! class_exists( 'CZR_post' ) ) :
                             ),
                             sprintf('<div class="%1$s"><h3>%2$s</h3><p>%3$s</p><div class="author-link">%4$s</div></div>',
                                     apply_filters( 'tc_author_meta_content_class', 'author-description span10' ),
-                                    sprintf( __( 'About %s' , 'customizr' ),  ),
+                                    sprintf( __( 'About %s' , 'customizr' ), $author_name ),
                                     get_the_author_meta( 'description', $author_id ),
                                     sprintf( '<a href="%1$s" rel="author">%2$s</a>',
                                       esc_url( get_author_posts_url( $author_id ) ),