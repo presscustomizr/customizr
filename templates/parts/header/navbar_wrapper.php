7bda98be28bfd746df38604d3c2b08ff818975b4
Rocco Aliberti <raliberti84@gmail.com>
2018-07-25 11:03:20 +0200

    Add: modern - new option to display the site title next to the logo fixes #1573

--- templates/parts/header/navbar_wrapper.php
+++ templates/parts/header/navbar_wrapper.php
@@ -15,7 +15,6 @@
       <?php czr_fn_render_template(  'header/parts/branding_wrapper', array(
         'model_args' => array(
           'element_class'         => 'col col-auto',
-          'inner_branding_class'  => 'brand_next' == czr_fn_opt( 'tc_header_desktop_tagline' ) ? 'flex-row tagline-aside' : 'flex-column tagline-below'
         )
       ) ) ?>
       <?php czr_fn_render_template(  'header/parts/nav_container', array(
--- templates/parts/header/parts/title.php
+++ templates/parts/header/parts/title.php
@@ -3,7 +3,7 @@
  * The template for displaying the site title (with its wrapper)
  */
 ?>
-<h1 class="navbar-brand align-self-start <?php czr_fn_echo( 'element_class' ) ?>" <?php czr_fn_echo('element_attributes') ?>>
+<h1 class="navbar-brand col-auto <?php czr_fn_echo( 'element_class' ) ?>" <?php czr_fn_echo('element_attributes') ?>>
   <a class="navbar-brand-sitename <?php czr_fn_echo( 'title_class' ) ?>" href="<?php echo esc_url( home_url( '/' ) ) ?>"  title="<?php bloginfo( 'name' ) ?> | <?php echo get_bloginfo( 'description', 'display' ) ?>">
     <span><?php bloginfo( 'name' ) ?></span>
   </a>
