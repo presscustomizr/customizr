740e17601db79c070e653dcaa71e232d731940d2
Rocco Aliberti <raliberti84@gmail.com>
2018-11-06 18:23:29 +0100

    Fix: classical - fix html validation for meta with http-equiv attribute with value X-UA-Compatible: must have the value IE=EDGE fixes #1583

--- inc/_dev/parts/class-header-header_main.php
+++ inc/_dev/parts/class-header-header_main.php
@@ -115,7 +115,7 @@ if ( ! class_exists( 'CZR_header_main' ) ) :
 				?>
 				<head>
 				    <meta charset="<?php bloginfo( 'charset' ); ?>" />
-				    <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE" />
+				    <meta http-equiv="X-UA-Compatible" content="IE=EDGE" />
             <?php if ( ! function_exists( '_wp_render_title_tag' ) ) :?>
 				      <title><?php wp_title( '|' , true, 'right' ); ?></title>
             <?php endif; ?>
