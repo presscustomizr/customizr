c1996dba520318b351bf510a78692a2631b6fa38
nikeo <nicolas@presscustomizr.com>
2019-04-09 10:38:42 +0200

    fixed : make sure a default option using 'nimblecheck' type is turned into a boolean fixes https://github.com/presscustomizr/customizr/issues/1732

--- core/core-functions.php
+++ core/core-functions.php
@@ -598,7 +598,8 @@ function czr_fn_generate_default_options( $map, $option_group = null ) {
       $option_name = $key;
       //write default option in array
       if( isset($options['default']) )
-        $defaults[$option_name] = ( 'checkbox' == $options['type'] ) ? (bool) $options['default'] : $options['default'];
+        // added check on 'nimblecheck' to fix https://github.com/presscustomizr/customizr/issues/1732
+        $defaults[$option_name] = in_array( $options['type'], array( 'checkbox', 'nimblecheck' ) ) ? (bool)$options['default'] : $options['default'];
       else
         $defaults[$option_name] = null;
     }//end foreach
