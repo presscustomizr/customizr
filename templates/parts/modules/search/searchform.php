02088d38b82d15a50b94b8687bcbc116ca1e9cb3
nikeo <nicolas@presscustomizr.com>
2020-01-30 16:53:07 +0100

    fixed : text on search button might not be visible on hover when footer is dark. fixes #1798 fixed : replace "Search" text by an icon on search button. fixes #1800

--- templates/parts/modules/search/searchform.php
+++ templates/parts/modules/search/searchform.php
@@ -8,10 +8,10 @@
     <div class="form-group czr-focus">
       <?php $sf_id = uniqid() ?>
       <label for="s-<?php echo $sf_id ?>" id="lsearch-<?php echo $sf_id ?>">
-          <span class="screen-reader-text"><?php _ex( 'Search', 'label', 'customizr') ?></span>
-          <input id="s-<?php echo $sf_id ?>" class="form-control czr-search-field" name="s" type="search" value="<?php echo get_search_query() ?>" aria-describedby="lsearch-<?php echo $sf_id ?>" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'title', 'customizr') ?>">
-        </label>
-        <input type="submit" class="search-submit" value="Search">
+        <span class="screen-reader-text"><?php _ex( 'Search', 'label', 'customizr') ?></span>
+        <input id="s-<?php echo $sf_id ?>" class="form-control czr-search-field" name="s" type="search" value="<?php echo get_search_query() ?>" aria-describedby="lsearch-<?php echo $sf_id ?>" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'title', 'customizr') ?>">
+      </label>
+      <button type="submit" class="button"><i class="icn-search"></i><span class="screen-reader-text"><?php echo esc_attr_x( 'Search &hellip;', 'title', 'customizr') ?></span></button>
     </div>
   </form>
 </div>
