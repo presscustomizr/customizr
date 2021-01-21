921c76321e78f8d92309085ae10d6c30b2eb160d
nikeo <nicolas@presscustomizr.com>
2021-01-21 14:46:25 +0100

    fixed : Conflict with OptimizePress3 first stone for #1887

--- assets/front/js/_parts/_parts_classical/_various_former_hardcoded.part.js
+++ assets/front/js/_parts/_parts_classical/_various_former_hardcoded.part.js
@@ -17,11 +17,15 @@
           //PLUGIN COMPAT
           //Optimize Press
           if ( pluginCompatParams.optimizepress_compat && pluginCompatParams.optimizepress_compat.remove_fancybox_loading ) {
-                  var opjq = opjq || 'undefined';
-                  if ( ! _.isUndefined( opjq ) ) {
-                      opjq(document).ready( function() {
-                          opjq('#fancybox-loading').remove();
-                      } );
+                var opjq = window.opjq || 'undefined';
+                if ( !_.isUndefined( opjq ) && _.isFunction(opjq) ) {
+                    try {
+                        opjq(document).ready( function() {
+                            opjq('#fancybox-loading').remove();
+                        });
+                    } catch (error) {
+                        console.log('Optimize Press => error', error );
+                    } 
                 }
           }
     };
