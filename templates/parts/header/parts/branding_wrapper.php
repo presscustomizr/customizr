e9f8200b79e089ee6acbb59c4903b6d67c52382e
nikeo <nicolas@presscustomizr.com>
2021-03-30 17:00:16 +0200

    fixed : [header] when centered on desktop, the site title stays left aligned. fixes #1893

--- templates/parts/header/parts/branding_wrapper.php
+++ templates/parts/header/parts/branding_wrapper.php
@@ -1,4 +1,11 @@
 <?php
+$header_layouts = esc_attr( czr_fn_opt( 'tc_header_layout' ) );
+$is_logo_centered = 'centered' === $header_layouts;
+$branding_row_classes = ['branding-row', 'd-flex', 'flex-row','align-items-center'];
+if ( !$is_logo_centered ) {
+  $branding_row_classes[] = 'align-self-start';
+}
+
 /**
  * The template for displaying the branding wrapper
  * July 2017 : no specific model for this template. The 'inner_branding_class' property => is added to the default model when invoking the czr_fn_render_template
@@ -6,7 +13,7 @@
 ?>
 <div class="branding__container <?php czr_fn_echo('element_class') ?>" <?php czr_fn_echo('element_attributes') ?>>
   <div class="branding align-items-center flex-column <?php czr_fn_echo( 'inner_branding_class' ); ?>">
-    <div class="branding-row d-flex align-self-start flex-row align-items-center">
+    <div class="<?php echo implode(' ', $branding_row_classes); ?>">
       <?php
         if ( czr_fn_is_registered_or_possible('logo_wrapper') ) {
           czr_fn_render_template( 'header/parts/logo_wrapper' );
